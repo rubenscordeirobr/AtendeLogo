6d21035dda976d413c94a8fd7ac109125a73e9cf
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-01-27 09:52:08 +0100

    Add ActivityType and AuthenticationType enums for activity entities

---
+++ src/AtendeLogo.Domain/Enums/ActivityType.cs
@@ -0,0 +1,13 @@
+﻿namespace AtendeLogo.Domain.Enums;
+
+public enum ActivityType
+{
+    Create = 1,
+    Update = 2,
+    Delete = 3,
+    Read = 4,
+    Authenticate = 5,
+    SuccessfulAuthentication = 6,
+    Logout = 6,
+    FailedAuthentication = 7,
+}
---
+++ src/AtendeLogo.Domain/Enums/AuthenticationType.cs
@@ -0,0 +1,13 @@
+﻿namespace AtendeLogo.Domain.Enums;
+ 
+public enum AuthenticationType
+{
+    Unknown = 0,
+    System = 1,         
+    Email_Password = 2,    
+    Google = 3,          
+    Facebook = 4,        
+    Microsoft = 5,        
+    WhatsApp = 6,      
+    SMS = 7               
+}
