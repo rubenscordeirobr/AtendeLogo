6331a525188ed190c5497bd834f243c9bb9cec60
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-01-27 09:40:11 +0100

    Add Address and Tenant classes.

---
+++ src/AtendeLogo.Domain/Entities/Identities/Address.cs
@@ -0,0 +1,35 @@
+﻿using AtendeLogo.Domain.Domain;
+
+namespace AtendeLogo.Domain.Entities.Identities;
+
+public class Address : EntityBase, IAddress
+{
+    public string Street { get; private set; }
+    public string Number { get; private set; }
+    public string Complement { get; private set; }
+    public string Neighborhood { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+    public string Country { get; private set; }
+    public string ZipCode { get; private set; }
+
+    public Address(
+        string street,
+        string number,
+        string complement,
+        string neighborhood,
+        string city,
+        string state,
+        string country,
+        string zipCode)
+    {
+        Street = street;
+        Number = number;
+        Complement = complement;
+        Neighborhood = neighborhood;
+        City = city;
+        State = state;
+        Country = country;
+        ZipCode = zipCode;
+    }
+}
---
+++ src/AtendeLogo.Domain/Entities/Identities/Tenant.cs
@@ -0,0 +1,57 @@
+﻿using AtendeLogo.Domain.Domain;
+
+namespace AtendeLogo.Domain.Entities.Identities;
+
+public class Tenant : EntityBase, ITenant
+{
+    public string Name { get; private set; }
+    public string FiscalCode { get; private set; }
+    public string ContactEmail { get; private set; }
+    public string ContactPhone { get; private set; }
+    public string Country { get; private set; }
+    public string DefaultLanguage { get; private set; }
+    public string DefaultCurrency { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public Address? Address { get; private set; }
+    public Guid AddressId { get; private set; }
+    public BusinessType BusinessType { get; private set; }
+    public TenantState TenantState { get; private set; }
+    public TentantStatus TenantStatus { get; private set; }
+    public TenantType TenantType { get; private set; }
+
+    public Tenant(
+        string name,
+        string fiscalCode,
+        string contactEmail,
+        string contactPhone,
+        string country,
+        string defaultLanguage,
+        string defaultCurrency,
+        string phoneNumber,
+        BusinessType businessType,
+        TenantState tenantState,
+        TentantStatus tenantStatus,
+        TenantType tenantType)
+    {
+        Name = name;
+        FiscalCode = fiscalCode;
+        ContactEmail = contactEmail;
+        ContactPhone = contactPhone;
+        Country = country;
+        DefaultLanguage = defaultLanguage;
+        DefaultCurrency = defaultCurrency;
+        PhoneNumber = phoneNumber;
+        BusinessType = businessType;
+        TenantState = tenantState;
+        TenantStatus = tenantStatus;
+        TenantType = tenantType;
+    }
+
+    public void SetAddress(Address address)
+    {
+        Address = address;
+        AddressId = address.Id;
+    }
+
+    IAddress? ITenant.Address => Address;
+}
