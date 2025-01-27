00a5cfb798094c758d0ce401ab3c59e7190fd66d
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-01-27 09:19:48 +0100

    Add Error record and Result class to AtendeLogo.Domain

---
+++ src/AtendeLogo.Common/Error.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace AtendeLogo.Domain;
+
+[SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords")]
+public record Error(string Code, string Message)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Erro.NullValue", "The value is null");
+
+    public override string ToString()
+        => $"{Code}: {Message}";
+
+    public static implicit operator string(Error error)
+        => error.ToString();
+}
---
+++ src/AtendeLogo.Common/Result.cs
@@ -0,0 +1,50 @@
+﻿namespace AtendeLogo.Domain;
+
+public class Result<T> where T : notnull
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error? Error { get; }
+    public T? Value { get; }
+
+    internal Result(Error? error)
+    {
+        Error = error ?? throw new ArgumentNullException(nameof(error));
+        IsSuccess = false;
+    }
+
+    internal Result(T value)
+    {
+        IsSuccess = true;
+        Value = value;
+    }
+
+    public Result<TConvert> AsFailure<TConvert>() where TConvert : notnull
+    {
+        if (IsSuccess || Error is null)
+            throw new InvalidOperationException("Only failed results can be converted");
+
+        return Result.Failure<TConvert>(Error);
+    }
+
+    public T GetValue()
+    {
+        if (!IsSuccess || Value is null)
+            throw new InvalidOperationException("Cannot get value from failed result");
+        return Value;
+    }
+}
+
+public static class Result
+{
+    public static Result<T> Success<T>(T value)
+        where T : notnull
+        => new(value);
+
+    public static Result<T> Failure<T>(string code, string errorMessage)
+        where T : notnull
+        => new(new Error(code, errorMessage));
+    public static Result<T> Failure<T>(Error error)
+        where T : notnull
+        => new(error);
+}
