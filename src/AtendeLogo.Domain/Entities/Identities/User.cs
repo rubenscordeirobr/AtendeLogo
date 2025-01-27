a62a94e8741ec649e2c450dbbc72715915d3e152
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-01-27 09:41:11 +0100

    Add User and UserSession classes to domain entities.

---
+++ src/AtendeLogo.Domain/Entities/Identities/User.cs
@@ -0,0 +1,36 @@
+﻿using AtendeLogo.Domain.Domain;
+using AtendeLogo.Domain.Domain.Interfaces;
+
+namespace AtendeLogo.Domain.Entities.Identities;
+
+public abstract class User : EntityBase, IUser, IEntityDeleted
+{
+    public string Name { get; protected set; }
+    public string Password { get; protected set; }
+    public string Email { get; protected set; }
+    public string PhoneNumber { get; protected set; }
+    public UserState UserState { get; protected set; }
+    public UserStatus UserStatus { get; protected set; }
+    public PasswordStrength PasswordStrength { get; protected set; }
+    public UserSession? DeletedSession { get; protected set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public Guid? DeletedSessionId { get; set; }
+
+    public User(string name,
+        string email,
+        string phoneNumber,
+        Password password,
+        UserState userState,
+        UserStatus userStatus)
+    {
+        Name = name;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        UserState = userState;
+        UserStatus = userStatus;
+        Password = password.Value;
+        PasswordStrength = password.Strength;
+    }
+
+}
---
+++ src/AtendeLogo.Domain/Entities/Identities/UserSession.cs
@@ -0,0 +1,56 @@
+﻿using AtendeLogo.Domain.Domain;
+
+namespace AtendeLogo.Domain.Entities.Identities;
+public class UserSession : EntityBase, IUserSession
+{
+    public Guid ClientSessionId { get; private set; }
+    public DateTime SessionStart { get; private set; }
+    public DateTime? SessionEnd { get; private set; }
+    public DateTime? LastActivity { get; private set; }
+    public string IPAddress { get; private set; }
+    public string ApplicationName { get; private set; }
+    public string UserAgent { get; private set; }
+    public string Language { get; private set; }
+    public bool IsActive { get; private set; }
+    public GeoLocation? GeoLocation { get; private set; }
+    public Guid UserId { get; private set; }
+    public User User { get; private set; }
+    public Guid? TenantId { get; private set; }
+    public Tenant? Tenant { get; private set; }
+    public AuthenticationType AuthenticationType { get; private set; }
+
+    public UserSession(
+        AuthenticationType authenticationType,
+        Guid clientSessionId,
+        DateTime sessionStart,
+        DateTime? sessionEnd,
+        DateTime? lastActivity,
+        string ipAddress,
+        string applicationName,
+        GeoLocation? geoLocation,
+        Guid userId,
+        User user,
+        Guid? tenantId,
+        Tenant? tenant,
+        string userAgent,
+        string language,
+        bool isActive)
+    {
+        AuthenticationType = authenticationType;
+        ClientSessionId = clientSessionId;
+        SessionStart = sessionStart;
+        SessionEnd = sessionEnd;
+        LastActivity = lastActivity;
+        IPAddress = ipAddress;
+        ApplicationName = applicationName;
+        GeoLocation = geoLocation;
+        UserId = userId;
+        User = user;
+        TenantId = tenantId;
+        Tenant = tenant;
+        UserAgent = userAgent;
+        Language = language;
+        IsActive = isActive;
+    }
+    
+}
