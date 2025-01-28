30333a27fca50ac1bf86947955158d2efa909146
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-01-28 12:57:09 +0100

    Add TimeZoneOffset record to ValueObjects

---
+++ src/AtendeLogo.SharedKernel/ValueObjects/TimeZoneOffset.cs
@@ -0,0 +1,40 @@
+﻿namespace AtendeLogo.Shared.ValueObjects;
+
+public sealed record TimeZoneOffset
+{
+    public string Offset { get; }
+    public string Location { get; }
+    public TimeSpan OffsetTimeSpan { get; }
+
+    private TimeZoneOffset(TimeSpan offsetTimeSpan, string offset, string location)
+    {
+        OffsetTimeSpan = offsetTimeSpan;
+        Offset = offset;
+        Location = location;
+    }
+
+    public static Result<TimeZoneOffset> Create(string offset, string location)
+    {
+        if (string.IsNullOrWhiteSpace(offset))
+        {
+            return Result.ValidationFailure<TimeZoneOffset>(
+                "TimeZone.OffsetEmpty",
+                "Offset cannot be empty.");
+        }
+
+        if (string.IsNullOrWhiteSpace(location))
+        {
+            return Result.ValidationFailure<TimeZoneOffset>(
+                "TimeZine.LocationEmpty",
+                "Location cannot be empty.");
+        }
+
+        if (!TimeSpan.TryParse(offset, out var offsetTimeSpan))
+        {
+            return Result.ValidationFailure<TimeZoneOffset>(
+                "TimeZone.InvalidOffset",
+                $"Invalid offset. {offset}");
+        }
+        return Result.Success(new TimeZoneOffset(offsetTimeSpan, offset, location));
+    }
+}
