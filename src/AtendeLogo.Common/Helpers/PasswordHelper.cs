e638a80e457ab8a8450e1e0bd76c5d585d709457
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-02 16:12:31 +0100

    Refactor PasswordHelper and add new methods GenerateRandomPassword,

--- src/AtendeLogo.Common/Helpers/PasswordHelper.cs
+++ src/AtendeLogo.Common/Helpers/PasswordHelper.cs
@@ -1,8 +1,8 @@
-﻿using AtendeLogo.Domain.Enums;
+﻿using AtendeLogo.Common.Enums;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace AtendeLogo.Domain.Helpers;
+namespace AtendeLogo.Common.Helpers;
 public static class PasswordHelper
 {
     private static bool ContainsUpperCase(string input)
@@ -17,9 +17,7 @@ public static class PasswordHelper
         if (string.IsNullOrWhiteSpace(password))
             return PasswordStrength.Weak;
 
-        int score = 0;
-
-
+        var score = 0;
         if (password.Length >= 8) score++;
         if (password.Length >= 12) score++;
 
@@ -37,15 +35,53 @@ public static class PasswordHelper
 
     public static string HashPassword(string password, string salt)
     {
-        using var sha256 = SHA256.Create();
-        byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes($"{salt}|{password}"));
-
-        return $"{Convert.ToBase64String(hashBytes)}";
+        var passwordCombined = $"{password}::{salt}";
+        return HashHelper.CreateSha256Hash(passwordCombined);
     }
-
-    public static bool VerifyPassword(string inputPassword, string passwordHash, string salt)
+     
+    public static bool VerifyPassword(
+        string inputPassword, 
+        string passwordHash, 
+        string salt)
     {
         var inputPasswordHash = HashPassword(inputPassword, salt);
         return passwordHash == inputPasswordHash;
+    }
+
+    public static string GenerateRandomPassword(int length = 12)
+    {
+        if (length < 4)
+            throw new ArgumentException("Password length must be at least 4", nameof(length));
+
+        // Define allowed character sets
+        const string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string lowercase = "abcdefghijklmnopqrstuvwxyz";
+        const string digits = "0123456789";
+        const string special = "!@#$%^&*()-_=+[]{};:,.<>?";
+
+        var passwordChars = new char[length];
+         
+        passwordChars[0] = uppercase[RandomNumberGenerator.GetInt32(0, uppercase.Length)];
+        passwordChars[1] = lowercase[RandomNumberGenerator.GetInt32(0, lowercase.Length)];
+        passwordChars[2] = digits[RandomNumberGenerator.GetInt32(0, digits.Length)];
+        passwordChars[3] = special[RandomNumberGenerator.GetInt32(0, special.Length)];
+
+        string allChars = uppercase + lowercase + digits + special;
+        for (int i = 4; i < length; i++)
+        {
+            passwordChars[i] = allChars[RandomNumberGenerator.GetInt32(0, allChars.Length)];
+        }
+
+        Shuffle(passwordChars);
+
+        return new string(passwordChars);
+    }
+    private static void Shuffle(char[] array)
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = RandomNumberGenerator.GetInt32(0, i + 1);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
     }
 }
