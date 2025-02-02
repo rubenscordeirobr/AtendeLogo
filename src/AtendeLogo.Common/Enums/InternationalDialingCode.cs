ed72cf778a3dca795e817df5b987dd98c7ecf30b
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-02 16:07:42 +0100

    Add InternationalDialingCode enum

---
+++ src/AtendeLogo.Common/Enums/InternationalDialingCode.cs
@@ -0,0 +1,36 @@
+﻿using AtendeLogo.Common.Attributes;
+
+namespace AtendeLogo.Common.Enums;
+
+public enum InternationalDialingCode
+{
+    Unknown = 0,
+
+    // North America
+    [SharedInternationalDialingCode]
+    UnitedStatesOrCanada = 1,
+    Mexico = 52,
+     
+    // South America
+    Argentina = 54,
+    Bolivia = 591,
+    Brazil = 55,
+    Chile = 56,
+    Colombia = 57,
+    Ecuador = 593,
+    Guyana = 592,
+    Paraguay = 595,
+    Peru = 51,
+    Suriname = 597,
+    Uruguay = 598,
+    Venezuela = 58,
+
+    // Europe
+    Italy = 39,
+    France = 33,
+    Germany = 49,
+    Spain = 34,
+    Portugal = 351,
+    UnitedKingdom = 44
+
+}
