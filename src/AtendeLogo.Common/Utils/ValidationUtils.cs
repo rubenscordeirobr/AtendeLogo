0434ec01be35c658c28199565d0e6172a45b5269
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-03 09:39:53 +0100

    Add ValidationUtils with tests

---
+++ src/AtendeLogo.Common/Utils/ValidationUtils.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace AtendeLogo.Common.Utils;
+
+public static class ValidationUtils
+{
+    private static readonly Regex HexRegex = new(@"^[a-fA-F0-9]+$", RegexOptions.Compiled);
+
+    public static bool IsPhoneNumberValid(string? phoneNumber)
+    {
+        return PhoneNumberUtils.IsFullPhoneNumberValid(phoneNumber);
+    }
+    public static bool IsSha256(string? value)
+    {
+        if (value == null)
+            return false;
+
+        if (value.Length != 64)
+            return false;
+
+        return IsHex(value);
+    }
+
+    private static bool IsHex(string value)
+    {
+        return HexRegex.IsMatch(value);
+    }
+}
