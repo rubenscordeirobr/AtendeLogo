e1eb2257a11a79c5f36fd96c87a0fe7b46cc94f4
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-03 07:17:20 +0100

    Add DictionaryExtensions with the tests

---
+++ src/AtendeLogo.Common/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿public static class DictionaryExtensions
+{
+    public static TValue GetOrAdd<TKey, TValue>(
+        this Dictionary<TKey, TValue> dictionary,
+        TKey key,
+        Func<TValue> valueFactory)
+        where TKey : notnull
+    {
+        if (dictionary.TryGetValue(key, out var value))
+        {
+            return value;
+        }
+        value = valueFactory();
+        dictionary.Add(key, value);
+        return value;
+    }
+}
---
+++ tests/AtendeLogo.Common.UnitTests/Extensions/DictionaryExtensionsTest.cs
@@ -0,0 +1,50 @@
+﻿namespace AtendeLogo.Common.UnitTests.Extensions;
+
+public class DictionaryExtensionsTest
+{
+    [Fact]
+    public void GetOrAdd_KeyExists_ReturnsExistingValue()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int> { { "key1", 1 } };
+
+        // Act
+        var result = dictionary.GetOrAdd("key1", () => 2);
+
+        // Assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void GetOrAdd_KeyDoesNotExist_AddsAndReturnsNewValue()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int>();
+
+        // Act
+        var result = dictionary.GetOrAdd("key1", () => 2);
+
+        // Assert
+        Assert.Equal(2, result);
+        Assert.Equal(2, dictionary["key1"]);
+    }
+
+    [Fact]
+    public void GetOrAdd_ValueFactoryIsCalledOnce()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int>();
+        var callCount = 0;
+
+        // Act
+        var result = dictionary.GetOrAdd("key1", () =>
+        {
+            callCount++;
+            return 2;
+        });
+
+        // Assert
+        Assert.Equal(1, callCount);
+        Assert.Equal(2, result);
+    }
+}
