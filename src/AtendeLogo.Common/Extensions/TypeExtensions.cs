4b535aed9ee9ef3377866b896a0a4bb863f894fc
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-03 07:27:56 +0100

    Add TypeExtensions with tests

---
+++ src/AtendeLogo.Common/Extensions/TypeExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace AtendeLogo.Common.Extensions;
+
+public static class TypeExtensions
+{
+    public static bool IsSubclassOf<T>(this Type type)
+    {
+        return type.IsSubclassOf(typeof(T));
+    }
+
+    public static bool IsSubclassOfOrEquals<T>(this Type type)
+    {
+        return type.IsSubclassOfOrEquals(typeof(T));
+    }
+    public static bool IsSubclassOfOrEquals(this Type type, Type otherType)
+    {
+        return type.IsSubclassOf(otherType) || type == otherType;
+    }
+
+    public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
+    {
+        if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+        {
+            return true;
+        }
+        if (givenType.GetInterfaces().Any(it => it.IsGenericType && it.GetGenericTypeDefinition() == genericType))
+        {
+            return true;
+        }
+        if (givenType.BaseType is null)
+        {
+            return false;
+        }
+        return givenType.BaseType.IsAssignableToGenericType(genericType);
+    }
+
+    public static IEnumerable<PropertyInfo> GetDeclaredProperties(
+        this Type type,
+        bool isIgnoreNotMappedAtribute = true)
+    {
+        var properties = type.GetProperties()
+            .Where(x => x.DeclaringType == type);
+
+        return (isIgnoreNotMappedAtribute)
+            ? properties.Where(property => property.GetCustomAttribute<NotMappedAttribute>() == null)
+            : properties;
+    }
+    
+    public static IEnumerable<PropertyInfo> GetDeclaredPropertiesOfType<T>(
+        this Type type, bool isIgnoreNotMappedAtribute = true)
+    {
+        return GetDeclaredPropertiesOfType(type, typeof(T), isIgnoreNotMappedAtribute);
+    }
+
+    public static IEnumerable<PropertyInfo> GetDeclaredPropertiesOfType(
+        this Type type,
+        Type propertyType,
+        bool isIgnoreNotMappedAtribute = true)
+    {
+        var properties =  type.GetProperties()
+            .Where(x => x.DeclaringType == type)
+            .Where(x => x.PropertyType.IsSubclassOfOrEquals(propertyType));
+
+        return (isIgnoreNotMappedAtribute)
+            ? properties.Where(property => property.GetCustomAttribute<NotMappedAttribute>() == null)
+            : properties;
+    }
+}
---
+++ tests/AtendeLogo.Common.UnitTests/Extensions/TypeExtensionsTest.cs
@@ -0,0 +1,121 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AtendeLogo.Common.UnitTests.Extensions;
+
+public class TypeExtensionsTest
+{
+    public static IEnumerable<object[]> IsSubclassOfTestData
+        => new List<object[]>
+        {
+            new object[] { typeof(DerivedClass), typeof(BaseClass), true },
+            new object[] { typeof(BaseClass), typeof(DerivedClass), false },
+            new object[] { typeof(object), typeof(BaseClass), false },
+            new object[] { typeof(DerivedClass), typeof(object), true },
+        };
+
+    [Theory]
+    [MemberData(nameof(IsSubclassOfTestData))]
+    public void IsSubclassOf_ShouldReturnExpectedResult(
+        Type type,
+        Type baseType,
+        bool expectedResult)
+    {
+        type.IsSubclassOf(baseType).Should().Be(expectedResult);
+    }
+
+    public static IEnumerable<object[]> IsSubclassOfOrEqualsTestData 
+        => new List<object[]>
+        {
+            new object[] { typeof(DerivedClass), typeof(BaseClass), true },
+            new object[] { typeof(BaseClass), typeof(DerivedClass), false },
+            new object[] { typeof(BaseClass), typeof(BaseClass), true },
+            new object[] { typeof(object), typeof(BaseClass), false },
+            new object[] { typeof(DerivedClass), typeof(object), true },
+        };
+
+    [Theory]
+    [MemberData(nameof(IsSubclassOfOrEqualsTestData))]
+    public void IsSubclassOfOrEquals_ShouldReturnExpectedResult(
+        Type type,
+        Type otherType,
+        bool expectedResult)
+    {
+        type.IsSubclassOfOrEquals(otherType).Should().Be(expectedResult);
+    }
+
+    public static IEnumerable<object[]> IsAssignableToGenericTypeTestData
+        => new List<object[]>
+        {
+            new object[] { typeof(List<string>), typeof(IEnumerable<>), true },
+            new object[] { typeof(List<string>), typeof(IList<>), true },
+            new object[] { typeof(List<string>), typeof(ICollection<>), true },
+            new object[] { typeof(string), typeof(IEnumerable<>), true },
+            new object[] { typeof(DerivedClass), typeof(IEnumerable<>), false },
+        };
+
+    [Theory]
+    [MemberData(nameof(IsAssignableToGenericTypeTestData))]
+    public void IsAssignableToGenericType_ShouldReturnExpectedResult(
+        Type givenType,
+        Type genericType,
+        bool expectedResult)
+    {
+        givenType.IsAssignableToGenericType(genericType).Should().Be(expectedResult);
+    }
+
+    public static IEnumerable<object[]> GetDeclaredPropertiesTestData
+        => new List<object[]>
+        {
+            new object[] { typeof(TestClass), true, new[] { "Property1", "Property2" } },
+            new object[] { typeof(TestClass), false, new[] { "Property1", "Property2", "NotMappedProperty" } },
+        };
+
+    [Theory]
+    [MemberData(nameof(GetDeclaredPropertiesTestData))]
+    public void GetDeclaredProperties_ShouldReturnExpectedResult(
+        Type type,
+        bool isIgnoreNotMappedAtribute,
+        string[] expectedProperties)
+    {
+        var properties = type.GetDeclaredProperties(isIgnoreNotMappedAtribute)
+            .Select(p => p.Name)
+            .ToArray();
+
+        properties.Should().BeEquivalentTo(expectedProperties);
+    }
+
+    public static IEnumerable<object[]> GetDeclaredPropertiesOfTypeTestData
+        => new List<object[]>
+        {
+            new object[] { typeof(TestClass), typeof(string), true, new[] { "Property1" } },
+            new object[] { typeof(TestClass), typeof(int), true, new[] { "Property2" } },
+            new object[] { typeof(TestClass), typeof(string), false, new[] { "Property1", "NotMappedProperty" } },
+        };
+
+    [Theory]
+    [MemberData(nameof(GetDeclaredPropertiesOfTypeTestData))]
+    public void GetDeclaredPropertiesOfType_ShouldReturnExpectedResult(
+        Type type,
+        Type propertyType,
+        bool isIgnoreNotMappedAtribute,
+        string[] expectedProperties)
+    {
+        var properties = type.GetDeclaredPropertiesOfType(propertyType, isIgnoreNotMappedAtribute)
+            .Select(p => p.Name)
+            .ToArray();
+
+        properties.Should().BeEquivalentTo(expectedProperties);
+    }
+
+    private class BaseClass { }
+    private class DerivedClass : BaseClass { }
+
+    private class TestClass
+    {
+        public string? Property1 { get; set; }
+        public int Property2 { get; set; }
+
+        [NotMapped]
+        public string? NotMappedProperty { get; set; }
+    }
+}
