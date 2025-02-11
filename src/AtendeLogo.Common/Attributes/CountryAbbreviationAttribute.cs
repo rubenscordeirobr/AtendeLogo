2733a380ed824b6576ec48df90d990085c67fcc1
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-11 17:31:07 +0100

    Add Country attributes and enum for country representation

---
+++ src/AtendeLogo.Common/Attributes/CountryAbbreviationAttribute.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AtendeLogo.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class CountryAbbreviationAttribute : Attribute
+{
+    public string Abbreviation { get; }
+
+    public CountryAbbreviationAttribute(string abbreviation)
+    {
+        Abbreviation = abbreviation;
+    }
+}
---
+++ src/AtendeLogo.Common/Attributes/CountryNumericCodeAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace AtendeLogo.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class CountryNumericCodeAttribute : Attribute
+{
+    public int NumericCode { get; }
+
+    public CountryNumericCodeAttribute(int numericCode)
+    {
+        NumericCode = numericCode;
+    }
+}
