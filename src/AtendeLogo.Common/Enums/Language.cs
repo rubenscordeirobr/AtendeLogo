55dc5dc8a29cb17ac799bec84af503ab38f74276
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-11 17:32:05 +0100

    Update Language enum to include Default and correct English spelling

--- src/AtendeLogo.Common/Enums/Language.cs
+++ src/AtendeLogo.Common/Enums/Language.cs
@@ -2,10 +2,11 @@
 
 public enum Language
 {
-    Portuguese = 1,
-    English = 2,
-    Spanish = 3,
-    French = 4,
-    German = 5,
-    Italian = 6
+    Default ,
+    Portuguese ,
+    Englishk,
+    Spanish,
+    French,
+    German,
+    Italian
 }
