81180389c20cba37a93687a1cbe9c9d350c50423
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-11 19:25:11 +0100

    Add RequestHeaderInfo record with predefined instances for Unknown and System

---
+++ src/AtendeLogo.Common/Infos/RequestHeaderInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace AtendeLogo.Common.Infos;
+public partial record RequestHeaderInfo(
+    string IpAddress,
+    string UserAgent,
+    string ApplicationName);
+
+public partial record RequestHeaderInfo
+{
+    public static RequestHeaderInfo Unknown
+        => new(IpAddress: "Unknown",
+               UserAgent: "Unknown",
+               ApplicationName: "Unknown");
+
+    public static RequestHeaderInfo System
+        => new(IpAddress: "System",
+               UserAgent: "System",
+               ApplicationName: "System");
+}
