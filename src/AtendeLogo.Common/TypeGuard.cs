3e620e801b1787d71020d86ec593ddde49ffcb5d
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-11 19:19:53 +0100

    Add TypeGuard class with type validation methods and corresponding unit tests

---
+++ src/AtendeLogo.Common/TypeGuard.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.CompilerServices;
+using AtendeLogo.Common.Extensions;
+
+namespace AtendeLogo.Common;
+
+public class TypeGuard
+{
+    public static void MustBeNotGeneric(Type type,
+          [CallerArgumentExpression("type")] string? paramName = "")
+    {
+        if (type.IsGenericType)
+            throw new InvalidOperationException($"{paramName}: Type{type.Name} is a generic type");
+    }
+
+    public static void MustBeConcrete(Type type,
+        [CallerArgumentExpression("type")] string? paramName = "")
+    {
+        if (!type.IsConcrete())
+            throw new InvalidOperationException($"{paramName}: Type{type.Name} must be a concrete type");
+    }
+
+    public static void TypeMustBeAssinableFrom(Type type, Type other)
+    {
+        if (!other.IsAssignableFrom(type))
+            throw new InvalidOperationException($"{type.Name} must be a subclass of {other.Name}");
+
+    }
+
+    public static void TypeMustubclassOfOrEquals(Type type, Type other)
+    {
+        if (!type.IsSubclassOfOrEquals(other))
+            throw new InvalidOperationException($"{type.Name} must be a subclass of {other.Name}");
+
+    }
+
+    public static void TypeMustBeInterface(Type type, Type other)
+    {
+        if (!other.IsInterface)
+            throw new InvalidOperationException($"{other.Name} must be an interface");
+    }
+
+    public static void TypeMustImplementInterface(Type type, Type interfaceType)
+    {
+        if (type.ImplementsGenericInterfaceDefinition(interfaceType))
+        {
+            return;
+        }
+
+        if (!interfaceType.IsAssignableFrom(type))
+        {
+            throw new InvalidOperationException($"{type.Name} must implement {interfaceType.Name}");
+        }
+    }
+}
