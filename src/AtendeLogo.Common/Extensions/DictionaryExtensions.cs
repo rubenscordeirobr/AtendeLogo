e48db592c799c4659e204478bf03b60936f16c1d
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-11 17:46:11 +0100

    Add extension methods for Dictionary to support add, update, and thread-safe operations

--- src/AtendeLogo.Common/Extensions/DictionaryExtensions.cs
+++ src/AtendeLogo.Common/Extensions/DictionaryExtensions.cs
@@ -1,4 +1,6 @@
-﻿public static class DictionaryExtensions
+﻿using System.Collections;
+
+public static class DictionaryExtensions
 {
     public static TValue GetOrAdd<TKey, TValue>(
         this Dictionary<TKey, TValue> dictionary,
@@ -13,5 +15,74 @@
         value = valueFactory();
         dictionary.Add(key, value);
         return value;
+    }
+
+    public static void AddOrUpdate<TKey, TValue>(
+        this Dictionary<TKey, TValue> dictionary,
+        TKey key,
+        TValue value)
+        where TKey : notnull
+    {
+        if (dictionary.ContainsKey(key))
+        {
+            dictionary[key] = value;
+        }
+        else
+        {
+            dictionary.Add(key, value);
+        }
+    }
+
+    public static void AddRangeOrUpdate<TKey, TValue>(
+        this Dictionary<TKey, TValue> dictionary,
+        IEnumerable<TKey> keys,
+        IEnumerable<TValue> values)
+        where TKey : notnull
+    {
+        if (keys.Count() != values.Count())
+        {
+            throw new ArgumentException("Keys and values must have the same length.");
+        }
+
+        for (int i = 0; i < keys.Count(); i++)
+        {
+            dictionary.AddOrUpdate(keys.ElementAt(i), values.ElementAt(i));
+        }
+    }
+
+    public static TValue GetOrAddThreadSafe<TKey, TValue>(
+       this Dictionary<TKey, TValue> dictionary,
+       TKey key,
+       Func<TValue> valueFactory)
+       where TKey : notnull
+    {
+        lock (((IDictionary)dictionary).SyncRoot)
+        {
+            return GetOrAdd(dictionary, key, valueFactory);
+        }
+    }
+
+    public static void AddOrUpdateThreadSafe<TKey, TValue>(
+        this Dictionary<TKey, TValue> dictionary,
+        TKey key,
+        TValue value)
+        where TKey : notnull
+    {
+        lock(((IDictionary)dictionary).SyncRoot)
+        {
+             AddOrUpdate(dictionary, key, value);
+        }
+    }
+
+    public static void AddRangeOrUpdateThreadSafe<TKey, TValue>(
+        this Dictionary<TKey, TValue> dictionary,
+        IEnumerable<TKey> keys,
+        IEnumerable<TValue> values)
+        where TKey : notnull
+    {
+        lock (((IDictionary)dictionary).SyncRoot)
+        {
+            AddRangeOrUpdate(dictionary, keys, values);
+        }
     }
 }
