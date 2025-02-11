b6a6d75037128565d598f74ca1021a9a778b4e60
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-11 18:45:24 +0100

    Add MD5 hash generation methods in HashHelper with tests

--- src/AtendeLogo.Common/Helpers/HashHelper.cs
+++ src/AtendeLogo.Common/Helpers/HashHelper.cs
@@ -1,25 +1,36 @@
-﻿
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace AtendeLogo.Common.Helpers;
 
 public class HashHelper
 {
-    public static string CreateSha256Hash(string input)
+    public static string GenerateSha256HashFromString(string input)
     {
-        return CreateSha256Hash(Encoding.UTF8.GetBytes(input));
+        return GenerateSha256Hash(Encoding.UTF8.GetBytes(input));
     }
 
     public static string CreateSha256Hash(Guid firstSystemSession_Id)
     {
-        return CreateSha256Hash(firstSystemSession_Id.ToByteArray());
+        return GenerateSha256Hash(firstSystemSession_Id.ToByteArray());
     }
 
-    public static string CreateSha256Hash(byte[] bytes)
+    public static string GenerateSha256Hash(byte[] bytes)
     {
         using var sha256 = SHA256.Create();
         var hashBytes = sha256.ComputeHash(bytes);
         return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    }
+
+    public static Guid CreateMd5GuidHash(string input)
+    {
+        return GenerateMd5GuidHash(Encoding.UTF8.GetBytes(input));
+    }
+
+    public static Guid GenerateMd5GuidHash(byte[] bytes)
+    {
+        using var md5 = MD5.Create();
+        var hashBytes = md5.ComputeHash(bytes);
+        return new Guid(hashBytes);
     }
 }
