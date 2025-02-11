62a2431d959703831edc01d4913bdb90cd2f8834
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-11 18:04:23 +0100

    Add PropertyInfoExtensions with GetPropertyPath method with tests

---
+++ src/AtendeLogo.Common/Extensions/PropertyInfoExtesions.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace AtendeLogo.Common.Extensions;
+
+public static class PropertyInfoExtesions
+{
+    public static string GetPropertyPath(this PropertyInfo propertyInfo)
+    {
+        return $"{propertyInfo.DeclaringType?.GetQualifiedTypeName()}::{propertyInfo.Name}";
+    }
+}
---
+++ tests/AtendeLogo.Common.UnitTests/Extensions/PropertyInfoExtesionsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace AtendeLogo.Common.UnitTests.Extensions;
+public class PropertyInfoExtesionsTests
+{
+    private class NestedPropertyTestClass
+    {
+        public string? TestProperty { get; set; }
+    }
+
+    [Theory]
+    [InlineData(typeof(NestedPropertyTestClass), nameof(NestedPropertyTestClass.TestProperty), "AtendeLogo.Common.UnitTests.Extensions.PropertyInfoExtesionsTests+NestedTestClass::TestProperty")]
+    [InlineData(typeof(TestPropertyPathClass), nameof(TestPropertyPathClass.TestProperty), "AtendeLogo.Common.UnitTests.Extensions.TestClass::TestProperty")]
+    public void GetPropertyPath_ShouldReturnCorrectPath(Type type, string propertyName, string result)
+    {
+        // Arrange
+        var propertyInfo = type.GetProperty(propertyName);
+
+        // Act
+        var propertyPath = propertyInfo?.GetPropertyPath();
+
+        // Assert
+        propertyPath.Should().Be(result);
+    }
+}
+
+internal class TestPropertyPathClass
+{
+    public string? TestProperty { get; set; }
+}
