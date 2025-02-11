3e0757898a52f8a3bb20c2115b3fe2d0e41e7525
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-11 18:33:19 +0100

    Add DateTimeExtensions with IsExpired method and corresponding unit tests

---
+++ src/AtendeLogo.Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AtendeLogo.Common.Extensions;
+public static class DateTimeExtensions
+{
+    public static bool IsExpired(
+        this DateTime dateTime,
+        TimeSpan expirationTime)
+    {
+        return dateTime.Add(expirationTime) < DateTime.UtcNow;
+    }
+}
