70311df888be86e3c47e069961fe67625a6c1ab5
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 15:49:41 +0100

    Add extension method to mark entities as deleted with soft delete logic

---
+++ src/AtendeLogo.Domain/Extensions/EntityDeletedExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace AtendeLogo.Domain.Extensions;
+
+public static class EntityDeletedExtensions
+{
+    public static void MarkAsDeleted(
+        this ISoftDeletableEntity entity,
+        IUserSession userSession)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+        ArgumentNullException.ThrowIfNull(userSession);
+
+        if (userSession.IsAnonymous())
+        {
+            throw new InvalidOperationException("Cannot delete entity with anonymous session");
+        }
+
+        if (userSession.IsTenantUser() && entity is IEntityTenant entityTenant)
+        {
+            if (entityTenant.Tenant_Id != userSession.Tenant_Id)
+            {
+                throw new InvalidOperationException("Cannot delete entity from another tenant");
+            }
+        }
+
+        var entityType = entity.GetType();
+        var properties = entityType.GetPropertiesFromInterface<ISoftDeletableEntity>();
+
+        properties[nameof(ISoftDeletableEntity.DeletedAt)]
+            .SetValue(entity, DateTime.UtcNow);
+
+        properties[nameof(ISoftDeletableEntity.DeletedSession_Id)]
+            .SetValue(entity, userSession.Id);
+
+        properties[nameof(ISoftDeletableEntity.IsDeleted)]
+            .SetValue(entity, true);
+
+    }
+}
