305e5991c16bd6cf5bc401e3b8808c3690adb394
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 18:34:03 +0100

    Add AnonymousConstants

---
+++ src/AtendeLogo.SharedKernel/Contantes/AnonymousConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace AtendeLogo.Shared.Contantes;
+
+public static class AnonymousConstants
+{
+    private static string? _clientSystemSessionToken;
+
+    public const string AnonymousEmail = "anonymous@atendelogo.com";
+    public const string AnonymousName = "Anonymous";
+
+    public static readonly Guid AnonymousSystemSession_Id = new Guid("49AD1FD6-0385-40DF-9F0F-538B61065442");
+    public static readonly Guid AnonymousUser_Id = new Guid("FB8A6865-A999-0000-8BB7-F4D70ACF1232");
+
+    public static string ClientAnymousSystemSessionToken
+        => _clientSystemSessionToken ??= HashHelper.CreateSha256Hash(AnonymousConstants.AnonymousSystemSession_Id);
+  
+}
--- src/AtendeLogo.SharedKernel/Contantes/SecurityConstants.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AtendeLogo.Shared.Contantes;
-
-public static class SecurityConstants
-{
-    public static readonly Guid AnonymousUserId = new Guid("FB8A6865-A999-4464-8BB7-F4D70ACF1232");
-}
