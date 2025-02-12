cb212b3bd5edfd65c902f824da253eb1f263e6e5
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 19:39:12 +0100

    Add IUserSessionRepository interface for managing user session operations

---
+++ src/AtendeLogo.Application/Contracts/Persistence/Identity/IUserSessionRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AtendeLogo.Application.Contracts.Persistence.Identity;
+
+public interface IUserSessionRepository : IRepositoryBase<UserSession>
+{
+    
+    Task<UserSession?> GetByClientTokenAsync(string sessionToken);
+
+    //Task CreateAsync(UserSession userSession);
+    //Task UpdateAsync(UserSession userSession);
+}
