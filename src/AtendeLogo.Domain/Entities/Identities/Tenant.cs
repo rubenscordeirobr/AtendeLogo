d9279ae630ba2c652df7caea67d695c560728e61
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 15:48:34 +0100

    Refactor Tenant class to use sealed modifier, enhance properties, and implement soft delete and event aggregation

--- src/AtendeLogo.Domain/Entities/Identities/Tenant.cs
+++ src/AtendeLogo.Domain/Entities/Identities/Tenant.cs
@@ -1,57 +1,121 @@
-﻿using AtendeLogo.Domain.Domain;
+﻿using AtendeLogo.Domain.Entities.Shared;
 
 namespace AtendeLogo.Domain.Entities.Identities;
 
-public class Tenant : EntityBase, ITenant
+public sealed class Tenant : EntityBase, ITenant, ISoftDeletableEntity, IEventAggregate
 {
+    private readonly List<IDomainEvent> _events = new();
+
     public string Name { get; private set; }
     public string FiscalCode { get; private set; }
-    public string ContactEmail { get; private set; }
-    public string ContactPhone { get; private set; }
-    public string Country { get; private set; }
-    public string DefaultLanguage { get; private set; }
-    public string DefaultCurrency { get; private set; }
-    public string PhoneNumber { get; private set; }
-    public Address? Address { get; private set; }
-    public Guid AddressId { get; private set; }
+    public string Email { get; private set; }
     public BusinessType BusinessType { get; private set; }
+    public Country Country { get; private set; }
+    public Currency Currency { get; private set; }
+    public Language Language { get; private set; }
     public TenantState TenantState { get; private set; }
     public TenantStatus TenantStatus { get; private set; }
     public TenantType TenantType { get; private set; }
+    public PhoneNumber PhoneNumber { get; private set; }
+
+    public TimeZoneOffset TimeZoneOffset { get; private set; }
+
+    public Guid? Owner_Id { get; private set; }
+    public TenantUser? Owner { get; private set; }
+
+    public Guid? Address_Id { get; private set; }
+    public Address? Address { get; private set; }
+
+    public List<TenantUser> Users { get; private set; } = [];
+    public List<UserSession> Sessions { get; private set; } = [];
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+    private Tenant(
+        string name,
+        string fiscalCode,
+        string email,
+        BusinessType businessType,
+        Country country,
+        Currency currency,
+        Language language,
+        TenantState tenantState,
+        TenantStatus tenantStatus,
+        TenantType tenantType,
+        PhoneNumber phoneNumber)
+        : this(name, fiscalCode, email, businessType, country, currency,
+              language, tenantState, tenantStatus, tenantType, phoneNumber, TimeZoneOffset.Default)
+    {
+
+    }
 
     public Tenant(
         string name,
         string fiscalCode,
-        string contactEmail,
-        string contactPhone,
-        string country,
-        string defaultLanguage,
-        string defaultCurrency,
-        string phoneNumber,
+        string email,
         BusinessType businessType,
+        Country country,
+        Currency currency,
+        Language language,
         TenantState tenantState,
         TenantStatus tenantStatus,
-        TenantType tenantType)
+        TenantType tenantType,
+        PhoneNumber phoneNumber,
+        TimeZoneOffset timeZoneOffset)
     {
         Name = name;
+        Email = email;
         FiscalCode = fiscalCode;
-        ContactEmail = contactEmail;
-        ContactPhone = contactPhone;
-        Country = country;
-        DefaultLanguage = defaultLanguage;
-        DefaultCurrency = defaultCurrency;
-        PhoneNumber = phoneNumber;
         BusinessType = businessType;
+        Country = country;
+        Currency = currency;
+        Language = language;
         TenantState = tenantState;
         TenantStatus = tenantStatus;
         TenantType = tenantType;
+        PhoneNumber = phoneNumber;
+        TimeZoneOffset = timeZoneOffset;
     }
 
     public void SetAddress(Address address)
     {
         Address = address;
-        AddressId = address.Id;
+        Address_Id = address.Id;
     }
 
-    IAddress? ITenant.Address => Address;
+    public void SetCreateOwner(TenantUser tenantUser)
+    {
+        Guard.MustBeEmpty(Owner_Id);
+        Guard.NotEmpty(tenantUser.Id);
+
+        Owner = tenantUser;
+        Owner_Id = tenantUser.Id;
+
+        _events.Add(new TenantCreatedEvent(this, tenantUser));
+    }
+
+    public void SetTimeZoneOffset(TimeZoneOffset timeZoneOffset)
+    {
+        if (!TimeZoneOffset.Equals(timeZoneOffset))
+            return;
+
+        var previousOffset = TimeZoneOffset;
+        TimeZoneOffset = timeZoneOffset;
+
+        var @event = new TenantTimeZoneOffsetChangedEvent(
+            Tenant: this,
+            TimeZoneOffsetOld: previousOffset,
+            TimeZoneOffsetNew: timeZoneOffset);
+
+        _events.Add(@event);
+    }
+
+    #region IEntityDeleted, IOrderableEntity, IDomainEventAggregate
+    public bool IsDeleted { get; private set; }
+    public DateTime? DeletedAt { get; private set; }
+    public Guid? DeletedSession_Id { get; private set; }
+
+    IReadOnlyList<IDomainEvent> IEventAggregate.DomainEvents
+        => _events;
+
+    #endregion
 }
