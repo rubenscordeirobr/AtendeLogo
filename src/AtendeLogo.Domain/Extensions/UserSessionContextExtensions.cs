bca39bc4aa6d4450ec69f8c6e0517d77f66cd660
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 15:50:09 +0100

    Add extension methods for IUserSession to check anonymous and tenant user status

---
+++ src/AtendeLogo.Domain/Extensions/UserSessionContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace AtendeLogo.Domain.Extensions;
+
+public static class UserSessionContextExtensions
+{
+    public static bool IsAnonymous(this IUserSession userSession)
+    {
+        ArgumentNullException.ThrowIfNull(userSession);
+
+        return userSession.User_Id == AnonymousConstants.AnonymousUser_Id ||
+            userSession.AuthenticationType == AuthenticationType.Anonymous;
+    }
+
+    public static bool IsTenantUser(this IUserSession userSession)
+    {
+        ArgumentNullException.ThrowIfNull(userSession);
+
+        return userSession.Tenant_Id.HasValue
+            && userSession.Tenant_Id != Guid.Empty;
+    }
+}
