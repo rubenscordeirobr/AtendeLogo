e21b0e0192ed9a2bc5a60b26ec87cd72e0e8e27d
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 19:39:20 +0100

    Add ITenantRepository interface for tenant management operations

---
+++ src/AtendeLogo.Application/Contracts/Persistence/Identity/ITenantRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace AtendeLogo.Application.Contracts.Persistence.Identity;
+
+public interface ITenantRepository : IRepositoryBase<Tenant>
+{
+    Task<Tenant?> GetByNameAsync(string name, CancellationToken cancellationToken);
+    Task<bool> EmailExistsAsync(string email, CancellationToken token);
+    Task<bool> EmailExistsAsync(Guid currentTenant_Id, string email, CancellationToken token);
+}
