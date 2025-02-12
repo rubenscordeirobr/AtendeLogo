ea04768daac3205729f30546e019e810fb6b3483
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 19:38:20 +0100

    Add IUnitOfWork interface for managing transactions and entity operations

---
+++ src/AtendeLogo.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using AtendeLogo.Application.Common;
+using AtendeLogo.Domain.Primitives;
+
+namespace AtendeLogo.Application.Contracts.Persistence;
+
+public interface IUnitOfWork : IAsyncDisposable
+{
+    //transaction
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+    Task<SaveChangesResult> CommitAsync(CancellationToken cancellationToken = default);
+    Task RollbackAsync(Exception exception, CancellationToken cancellationToken = default);
+
+    //commands
+    public void Add<TEntity>(TEntity entity)
+        where TEntity : EntityBase;
+
+    public void Update<TEntity>(TEntity entity)
+        where TEntity : EntityBase;
+
+    public void Delete<TEntity>(TEntity entity)
+        where TEntity : EntityBase;
+
+    public void Attach<TEntity>(TEntity entity)
+        where TEntity : EntityBase;
+
+    Task<SaveChangesResult> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
