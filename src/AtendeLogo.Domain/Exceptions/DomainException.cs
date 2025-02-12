3905c938e03e3d0d8fe2af8449b62ab6656f3ea5
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 18:30:46 +0100

    Add custom exceptions for domain errors and security violations

---
+++ src/AtendeLogo.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace AtendeLogo.Domain.Exceptions;
+public class DomainException: Exception
+{
+ 
+    public DomainException(string message) 
+        : base(message)
+    {
+    }
+}
---
+++ src/AtendeLogo.Domain/Exceptions/DomainSecurityException.cs
@@ -0,0 +1,9 @@
+﻿namespace AtendeLogo.Domain.Exceptions;
+
+public class DomainSecurityException : DomainException
+{
+    public DomainSecurityException(string message) 
+        : base(message)
+    {
+    }
+}
