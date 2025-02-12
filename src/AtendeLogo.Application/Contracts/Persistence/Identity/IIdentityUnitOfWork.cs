69ebe233392b362a99fd2fb278636858ab9f120a
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 19:39:30 +0100

    Add IIdentityUnitOfWork interface for identity-related repository management

---
+++ src/AtendeLogo.Application/Contracts/Persistence/Identity/IIdentityUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace AtendeLogo.Application.Contracts.Persistence.Identity;
+
+public interface IIdentityUnitOfWork: IUnitOfWork
+{
+    IUserSessionRepository UserSessionRepository { get; }
+    IAdminUserRepository AdminUserRepository { get; }
+
+    ISystemUserRepository SystemUserRepository { get; }
+
+    ITenantUserRepository TenantUserRepository { get; }
+
+    ITenantRepository TenantRepository { get; }
+   
+}
