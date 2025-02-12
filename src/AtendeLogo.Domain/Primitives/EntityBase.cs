4fc2f80b636e23f6d3634c02f87a017e1b0465ff
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 18:31:45 +0100

    Refactor EntityBase to improve session handling and equality checks

--- src/AtendeLogo.Domain/Primitives/EntityBase.cs
+++ src/AtendeLogo.Domain/Primitives/EntityBase.cs
@@ -4,31 +4,59 @@ public abstract class EntityBase
     public Guid Id { get; protected set; }
     public DateTime CreatedAt { get; protected set; }
     public DateTime LastUpdatedAt { get; protected set; }
-    public virtual Guid CreatedSession { get; protected set; }
-    public Guid LastUpdatedSession { get; protected set; }
-
-    public virtual void SetCreateDate(Guid sessionId)
+    public Guid CreatedSession_Id { get; protected set; }
+    public Guid LastUpdatedSession_Id { get; protected set; }
+  
+    public virtual void SetCreateSession(Guid session_Id)
     {
-        if (Id != Guid.Empty)
+        if (Id != Guid.Empty )
         {
             throw new InvalidOperationException("Cannot set create date for existing entity");
         }
 
-        CreatedAt = DateTime.UtcNow;
+        CreatedAt = default;
         LastUpdatedAt = CreatedAt;
-        CreatedSession = sessionId;
-        LastUpdatedSession = sessionId;
+        CreatedSession_Id = session_Id;
+        LastUpdatedSession_Id = session_Id;
     }
 
-    public void UpdateLastUpdateDate(Guid sessionId)
+    public void SetUpdateSession(Guid sessionId)
     {
         LastUpdatedAt = DateTime.UtcNow;
-        LastUpdatedSession = sessionId;
+        LastUpdatedSession_Id = sessionId;
     }
 
     public override string ToString()
     {
+        if(Id == default)
+        {
+            return $"{GetType().Name}: Id={Id}";
+        }
         return $"{GetType().Name}: Id={Id}, CreatedAt={CreatedAt}, LastUpdatedAt={LastUpdatedAt}";
     }
-}
 
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj is not EntityBase other ||
+            GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        if (Id == default || other.Id == default)
+        {
+            return false;
+        }
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return (GetType().ToString() + Id).GetHashCode();
+    }
+}
