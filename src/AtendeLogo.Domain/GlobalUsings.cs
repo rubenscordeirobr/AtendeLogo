1037bfd89da7c3315e0573a69824270c676dc97e
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 18:32:19 +0100

    Add global usings

---
+++ src/AtendeLogo.Domain/GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿global using System;
+global using AtendeLogo.Common;
+global using AtendeLogo.Common.Enums;
+global using AtendeLogo.Common.Extensions;
+global using AtendeLogo.Domain.Entities.Identities.Events;
+global using AtendeLogo.Domain.Primitives;
+global using AtendeLogo.Domain.Primitives.Contracts;
+global using AtendeLogo.Shared.Contantes;
+global using AtendeLogo.Shared.Enums;
+global using AtendeLogo.Shared.Interfaces.Commom;
+global using AtendeLogo.Shared.Interfaces.Identities;
+global using AtendeLogo.Shared.Interfaces.Shared;
+global using AtendeLogo.Shared.ValueObjects;
