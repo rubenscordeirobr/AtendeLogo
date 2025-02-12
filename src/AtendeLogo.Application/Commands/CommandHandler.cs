c4361f950c8f607d1d2be191902d6e351340a5c8
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 19:15:10 +0100

    Add abstract command handler class for processing commands

---
+++ src/AtendeLogo.Application/Commands/CommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using AtendeLogo.Application.Contracts.Handlers;
+
+namespace AtendeLogo.Application.Commands;
+
+public abstract class CommandHandler<TRequest, TResponse>
+    : ICommandHandler<TRequest, TResponse>
+    where TRequest : ICommandRequest<TResponse>
+    where TResponse : IResponse
+{
+    public Task<Result<TResponse>> RunAsync(
+        TRequest command,
+        CancellationToken cancellationToken = default)
+    {
+        return HandleAsync(command, cancellationToken);
+    }
+
+    protected abstract Task<Result<TResponse>> HandleAsync(
+        TRequest command,
+        CancellationToken cancellationToken);
+}
