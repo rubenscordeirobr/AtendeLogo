d3824a7b90339342e3927660fe1f00332422cb19
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 19:38:07 +0100

    Add IRepositoryBase interface for generic repository operations

---
+++ src/AtendeLogo.Application/Contracts/Persistence/IRepositoryBase.cs
@@ -0,0 +1,34 @@
+﻿using AtendeLogo.Domain.Primitives;
+using System.Linq.Expressions;
+
+namespace AtendeLogo.Application.Contracts.Persistence;
+public interface IRepositoryBase<TEntity> where TEntity : EntityBase
+{
+    //queries
+    Task<TEntity?> GetByIdAsync(
+        Guid id, 
+        CancellationToken cancellationToken = default);
+
+    Task<TEntity?> FindAsync(
+        Expression<Func<TEntity, bool>> predicate, 
+        CancellationToken cancellationToken = default);
+   
+    Task<IEnumerable<TEntity>> FindAllAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken = default);
+   
+    Task<IEnumerable<TEntity>> GetAllAsync(
+        CancellationToken cancellationToken = default);
+  
+    Task<bool> AnyAsync(
+        Expression<Func<TEntity, bool>> predicate, 
+        CancellationToken cancellationToken = default);
+
+    Task<TEntity> RefreshAsync(
+        TEntity entity,
+        CancellationToken cancellationToken = default);
+
+    Task<TEntity?> TryRefreshAsync(
+        TEntity entity, 
+        CancellationToken cancellationToken = default);
+}
