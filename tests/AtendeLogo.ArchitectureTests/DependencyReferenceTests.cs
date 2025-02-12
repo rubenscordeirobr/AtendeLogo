1b1a01dce39aae0bcc628537c8f8e74741b81010
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-12 19:08:04 +0100

    Add architecture tests for dependency references and entity rules

---
+++ tests/AtendeLogo.ArchitectureTests/DependencyReferenceTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace AtendeLogo.ArchitectureTests;
+
+public class DependencyReferenceTests
+{
+    private Assembly _commomAssembly;
+    private Assembly _domainAssembly;
+    private Assembly _applicationAssembly;
+    private Assembly _useCasesAssembly;
+    private Assembly _infrastructureAssembly;
+    private Assembly _persistanceAssembly;
+    private Assembly _persistanceActivitiesAssembly;
+
+    private Assembly _useCasesSharedAssembly;
+    private Assembly _sharedKernelAssembly;
+
+    public DependencyReferenceTests()
+    {
+
+        _commomAssembly = typeof(Common.Guard).Assembly;
+        _domainAssembly = typeof(Domain.Primitives.EntityBase).Assembly;
+
+        _applicationAssembly = typeof(Application.ApplicationConfigurationsExtensions).Assembly;
+
+        _useCasesAssembly = typeof(UseCases.UseCasesConfigurationExtensions).Assembly;
+        _infrastructureAssembly = typeof(Infrastructure.InfrastructureConfigurationExtensions).Assembly;
+        _persistanceAssembly = typeof(Persistence.PersistenceConfigurationExtensions).Assembly;
+        _persistanceActivitiesAssembly = typeof(Persistence.Activities.ActivitiesPersistenceConfigurationExtensions).Assembly;
+
+        _sharedKernelAssembly = typeof(Shared.ValueObjects.ValueObjectBase).Assembly;
+        _useCasesSharedAssembly = typeof(UseCases.Resources.ValidationMessages).Assembly;
+
+    }
+
+    [Fact]
+    public void CommomAssembly_ShouldNotHaveDependencies()
+    {
+        var result = Types.InAssembly(_commomAssembly)
+            .ShouldNot()
+            .HaveDependencyOnAny(
+                _useCasesSharedAssembly.GetName().Name,
+                _domainAssembly.GetName().Name,
+                _applicationAssembly.GetName().Name,
+                _infrastructureAssembly.GetName().Name,
+                _persistanceAssembly.GetName().Name,
+                _persistanceActivitiesAssembly.GetName().Name)
+            .GetResult().IsSuccessful
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void UseSharedKernelAssembly_ShouldNotHaveDependencies()
+    {
+        Types.InAssembly(_sharedKernelAssembly)
+            .ShouldNot()
+            .HaveDependencyOnAny(
+                _useCasesSharedAssembly.GetName().Name,
+                _domainAssembly.GetName().Name,
+                _applicationAssembly.GetName().Name,
+                _infrastructureAssembly.GetName().Name,
+                _persistanceAssembly.GetName().Name,
+                _persistanceActivitiesAssembly.GetName().Name)
+            .GetResult()
+            .IsSuccessful
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void UseCasesSharedAssembly_ShouldNotHaveDependencies()
+    {
+        Types.InAssembly(_useCasesSharedAssembly)
+           .ShouldNot()
+           .HaveDependencyOnAny(
+               _domainAssembly.GetName().Name,
+               _applicationAssembly.GetName().Name,
+               _infrastructureAssembly.GetName().Name,
+               _persistanceAssembly.GetName().Name,
+               _persistanceActivitiesAssembly.GetName().Name)
+           .GetResult()
+           .IsSuccessful
+                .Should()
+                .BeTrue();
+    }
+
+    [Fact]
+    public void ApplicationdAssembly_ShouldNotHaveDependencies()
+    {
+        Types.InAssembly(_applicationAssembly)
+             .ShouldNot()
+             .HaveDependencyOnAny(
+                 _infrastructureAssembly.GetName().Name,
+                 _useCasesAssembly.GetName().Name,
+                 _persistanceAssembly.GetName().Name,
+                 _persistanceActivitiesAssembly.GetName().Name)
+             .GetResult()
+             .IsSuccessful
+                .Should()
+                .BeTrue();
+    }
+
+    [Fact]
+    public void UserCasesAssembly_ShouldNotHaveDependencies()
+    {
+        Types.InAssembly(_applicationAssembly)
+             .ShouldNot()
+             .HaveDependencyOnAny(
+                 _infrastructureAssembly.GetName().Name,
+                 _persistanceAssembly.GetName().Name,
+                 _persistanceActivitiesAssembly.GetName().Name)
+             .GetResult()
+             .IsSuccessful
+                .Should()
+                .BeTrue();
+     }
+}
---
+++ tests/AtendeLogo.ArchitectureTests/EntityTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using AtendeLogo.Common.Extensions;
+using AtendeLogo.Domain.Primitives;
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace AtendeLogo.ArchitectureTests;
+
+public class EntityTests
+{
+    [Fact]
+    public void Entities_ShouldBeSealed()
+    {
+        var assembly = typeof(EntityBase).Assembly;
+
+        Types.InAssembly(assembly)
+            .That()
+            .Inherit(typeof(EntityBase))
+            .And()
+            .AreNotAbstract()
+            .Should()
+            .BeSealed()
+            .GetResult()
+            .IsSuccessful
+                .Should()
+                .BeTrue();
+    }
+
+    [Fact]
+    public void Entities_PublicPropertiesShouldHavePrivateOrProtectedSetters()
+    {
+        var assembly = typeof(EntityBase).Assembly;
+
+        var publicProperties = Types.InAssembly(assembly)
+              .That()
+              .Inherit(typeof(EntityBase))
+              .GetTypes()
+              .SelectMany(type => type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+              .Where(prop => prop.GetMethod?.IsPublic == true && prop.SetMethod != null);
+
+        var propertiesWithPublicSetters = publicProperties
+            .Where(prop => prop.SetMethod?.IsPublic == true);
+
+        string getBecauseMessage() 
+            => $"These properties should have private or protected setters:{Environment.NewLine}" +
+               $"{string.Join(Environment.NewLine, propertiesWithPublicSetters.Select(p => p.GetPropertyPath()))}";
+
+        propertiesWithPublicSetters.Should()
+            .BeEmpty(getBecauseMessage());
+    }
+}
