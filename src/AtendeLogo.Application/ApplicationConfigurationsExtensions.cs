f8b798d91fe321537118cd155f94aad4b270f452
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-13 11:37:49 +0100

    Add ApplicationConfigurationsExtensions for configuring application services and handlers

---
+++ src/AtendeLogo.Application/ApplicationConfigurationsExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using AtendeLogo.Application.Mediatores;
+using AtendeLogo.Application.Registrars;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AtendeLogo.Application;
+
+public static class ApplicationConfigurationsExtensions
+{
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        services.AddScoped<IRequestMediator, RequestMediator>();
+        services.AddScoped<IEventMediator, EventMediator>();
+
+        services.AddApplicationHandlersFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationHandlersFromAssembly(
+       this IServiceCollection services,
+       Assembly assembly)
+    {
+        var serviceRegistrar = new ApplicationHandlerRegistrar(services);
+        serviceRegistrar.RegisterFromAssembly(assembly);
+        return services;
+    }
+
+    internal static IServiceCollection AddApplicationHandlersFromTypes(
+       this IServiceCollection services,
+       Type[] handlerTypes)
+    {
+        var serviceRegistrar = new ApplicationHandlerRegistrar(services);
+        serviceRegistrar.RegisterHandlerFromTypes(handlerTypes);
+        return services;
+    }
+}
