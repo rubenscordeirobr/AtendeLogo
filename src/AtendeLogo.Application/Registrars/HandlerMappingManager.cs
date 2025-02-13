5f79430d3cc758bc6d55f7dc17c7a72f8ea5c376
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-13 09:33:31 +0100

    Add ApplicationHandlerRegistrar for mapping handler classes and domain event processing, including implementation classes for dependency injection.

---
+++ src/AtendeLogo.Application/Registrars/HandlerMappingManager.cs
@@ -0,0 +1,31 @@
+﻿namespace AtendeLogo.Application.Registrars;
+
+internal static class HandlerMappingManager
+{
+   private readonly static HandlerMappings _domainEventHandlerMappings = new(HandlerKind.DomainEventHandler);
+   private readonly static HandlerMappings _domainEventPreProcessorHandlerMappings = new(HandlerKind.PreProcessorHandler);
+
+    internal static void MapperDomainEventHandler(
+        Type domainEventType,
+        Type handlerType)
+    {
+        _domainEventHandlerMappings.MapperEventHandler(domainEventType, handlerType);
+    }
+
+    internal static void MapperDomainEventPreProcessorHandler(
+        Type domainEventType,
+        Type handlerType)
+    {
+        _domainEventPreProcessorHandlerMappings.MapperEventHandler(domainEventType, handlerType);
+    }
+
+    internal static IReadOnlyList<Type> GetDomainEventHandlers(Type eventType)
+    {
+        return _domainEventHandlerMappings.GetHandlerTypes(eventType);
+    }
+
+    internal static IReadOnlyList<Type> GetDomainEventPreProcessorHandlers(Type eventType)
+    {
+        return _domainEventPreProcessorHandlerMappings.GetHandlerTypes(eventType);
+    }
+}
