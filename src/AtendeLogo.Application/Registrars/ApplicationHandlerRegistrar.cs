413d24f1808194520200b50d957b39fd2545aad0
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-13 09:33:31 +0100

    Add ApplicationHandlerRegistrar for mapping handler classes and domain event processing, including implementation classes for dependency injection.

---
+++ src/AtendeLogo.Application/Registrars/ApplicationHandlerRegistrar.cs
@@ -0,0 +1,209 @@
+﻿using System.Reflection;
+using AtendeLogo.Application.Contracts.Handlers;
+using AtendeLogo.Common.Extensions;
+using AtendeLogo.Common.Helpers;
+using AtendeLogo.Domain.Primitives;
+using AtendeLogo.Domain.Primitives.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace AtendeLogo.Application.Registrars;
+
+internal class ApplicationHandlerRegistrar
+{
+    private readonly IServiceCollection _services;
+
+    internal ApplicationHandlerRegistrar(
+        IServiceCollection services)
+    {
+        _services = services;
+    }
+
+    internal void RegisterFromAssembly(Assembly assembly)
+    {
+        var assemblyTypes = assembly.GetTypes();
+        RegisterTypes(assemblyTypes);
+    }
+
+    private void RegisterTypes(Type[] types)
+    {
+        RegisterRequestHandlers(types);
+        RegisterEventHandlers(types);
+    }
+
+    private void RegisterRequestHandlers(Type[] assemblyTypes)
+    {
+        var handlerDefinitionType = typeof(IRequestHandler<,>);
+        var handlersTypesMapped = TypeHelper.FindTypesImplementingInterface(assemblyTypes, handlerDefinitionType);
+        
+        foreach (var (handlerType, handlerInterfaceType) in handlersTypesMapped)
+        {
+            CheckIfHasSubclassIn(handlerType, assemblyTypes);
+            CheckIfClass(handlerType);
+
+            var requestType = handlerInterfaceType.GetGenericArguments().First();
+            var responseType = handlerInterfaceType.GetGenericArguments().Last();
+
+            CheckIfNotGenericType(requestType);
+            CheckIfNotGenericType(responseType);
+
+            TypeGuard.MustBeNotGeneric(requestType);
+            TypeGuard.MustBeNotGeneric(responseType);
+
+            TypeGuard.MustBeConcrete(responseType);
+            TypeGuard.MustBeConcrete(requestType);
+
+            var serviceType = typeof(IRequestHandler<,>)
+                .MakeGenericType(requestType, responseType);
+
+            _services.TryAddTransient(serviceType, handlerType);
+        }
+    }
+
+    private void RegisterEventHandlers(Type[] assemblyTypes)
+    {
+        RegisterEventHandlers(
+            assemblyTypes,
+            typeof(IDomainEventHandler<>),
+            HandlerKind.DomainEventHandler,
+            HandlerMappingManager.MapperDomainEventHandler);
+
+        RegisterEventHandlers(
+            assemblyTypes,
+            typeof(IPreProcessorHandler<>),
+            HandlerKind.PreProcessorHandler,
+            HandlerMappingManager.MapperDomainEventPreProcessorHandler);
+    }
+
+    private void RegisterEventHandlers(Type[] assemblyTypes,
+                                       Type handlerDefinition,
+                                       HandlerKind eventHandlerType,
+                                       Action<Type, Type> mapperEventHadler)
+    {
+        var handlersTypesMapped = TypeHelper.FindTypesImplementingInterface(assemblyTypes, handlerDefinition);
+
+        foreach (var (handlerType, handlerInterfaceType) in handlersTypesMapped)
+        {
+            CheckIfHasSubclassIn(handlerType, assemblyTypes);
+            CheckIfClass(handlerType);
+
+            var domainEventType = handlerInterfaceType.GetGenericArguments().First();
+
+            ValidateDomainEventType(domainEventType);
+            ValidateHandlerType(handlerType, eventHandlerType);
+
+            _services.TryAddTransient(handlerType, handlerType);
+
+            mapperEventHadler.Invoke(domainEventType, handlerType);
+        }
+    }
+
+    private void CheckIfClass(Type handlerType)
+    {
+        if (!handlerType.IsClass)
+        {
+            var message = $"Handler {handlerType.Name} is not a class";
+            throw new InvalidOperationException(message);
+        }
+    }
+
+    private void CheckIfHasSubclassIn(Type handlerType, Type[] assemblyTypes)
+    {
+        if (handlerType.IsSealed)
+            return;
+
+        var subClasse = assemblyTypes.FirstOrDefault(x => x.IsSubclassOf(handlerType));
+        if (subClasse is not null)
+        {
+            var message = $"Handler {handlerType.Name} has a subclass  {subClasse} in the assembly" +
+                          "Please, make sure that the handler is not being used as a base class for another or set the handler as abstract class";
+            throw new InvalidOperationException(message);
+        }
+    }
+
+    private void CheckIfNotGenericType(Type type)
+    {
+        if (type.IsGenericType)
+        {
+            var message = $"Type {type.Name} is a generic type";
+            throw new InvalidOperationException(message);
+        }
+    }
+
+    private void ValidateDomainEventType(Type eventHandlerType)
+    {
+        if (eventHandlerType.IsGenericType)
+        {
+            if (eventHandlerType.GetGenericArguments().Count() > 1)
+            {
+                var message = $"Event {eventHandlerType.Name} has more than one generic argument";
+                throw new NotSupportedException(message);
+            }
+
+            if (!eventHandlerType.ImplementsGenericInterfaceDefinition(typeof(IEntityStateChangedEvent<>)))
+            {
+                var mensagem = $"The generic event {eventHandlerType.Name} does not implement IEntityStateChangedEvent<>";
+                throw new NotSupportedException(mensagem);
+            }
+        }
+    }
+
+    private void ValidateHandlerType(Type handlerType, HandlerKind eventHandlerType)
+    {
+        if (!handlerType.IsClass)
+        {
+            var message = $"Handler {handlerType.Name} is not a class";
+            throw new InvalidOperationException(message);
+        }
+
+        if (!handlerType.IsGenericType)
+        {
+            return;
+        }
+
+        if (handlerType.GetGenericArguments().Count() > 1)
+        {
+            var message = $"Handler {handlerType.Name} has more than one generic argument";
+            throw new NotSupportedException(message);
+        }
+
+        var genericArguments = handlerType.GetGenericArguments().First();
+        if (!genericArguments.IsAssignableTo(typeof(IDomainEvent)) &&
+            !genericArguments.IsSubclassOfOrEquals(typeof(EntityBase)))
+        {
+            var message = $"The generic handler {handlerType.Name} does not implement IDomainEvent";
+            throw new NotSupportedException(message);
+        }
+        if (genericArguments.IsGenericType)
+        {
+            throw new Exception($"The generic handler {handlerType.Name} has a generic argument");
+        }
+
+        if (genericArguments.IsSubclassOfOrEquals(typeof(EntityBase)))
+        {
+            if (eventHandlerType == HandlerKind.DomainEventHandler)
+            {
+                if (!handlerType.ImplementsGenericInterfaceDefinition(typeof(IEntityStateChangedEventHandler<>)))
+                {
+                    var message = $"The generic handler {handlerType.Name} does not implement IEntityStateChangedEventHandler<>";
+                    throw new NotSupportedException(message);
+                }
+            }
+            else if (eventHandlerType == HandlerKind.PreProcessorHandler)
+            {
+                if (!handlerType.ImplementsGenericInterfaceDefinition(typeof(IEntityStateChangedEventPreProcessorHandler<>)))
+                {
+                    var message = $"The generic handler {handlerType.Name} does not implement IEntityStateChangedEventPreProcessor<>";
+                    throw new NotSupportedException(message);
+                }
+            }
+        }
+    }
+}
+
+internal enum HandlerKind
+{
+    RequestHandler,
+    DomainEventHandler,
+    PreProcessorHandler
+}
---
+++ tests/AtendeLogo.Application.UnitTests/Registrars/ApplicationHandlerRegistrarTests.cs
@@ -0,0 +1,131 @@
+﻿using AtendeLogo.Application.Commands;
+using AtendeLogo.Application.Contracts.Events;
+using AtendeLogo.Application.Contracts.Handlers;
+using AtendeLogo.Application.Exceptions;
+using AtendeLogo.Application.Queries;
+using AtendeLogo.Common;
+using AtendeLogo.Domain.Primitives.Contracts;
+using AtendeLogo.Shared.Contracts;
+using Microsoft.Extensions.Hosting;
+
+namespace AtendeLogo.Application.UnitTests.Registrars;
+
+public class ApplicationHandlerRegistrarTests
+{
+    private Type[] GetHandlerTypes()
+    {
+        return
+        [
+            typeof(MockCommandHandler),
+            typeof(MockSingleQueryHandler),
+            typeof(MockCollectionQueryHandler),
+            typeof(MockEventHandler)
+        ];
+    }
+
+    [Fact]
+    public void ApplicationHandlerRegistrar_ShouldRegisterHandlersCorrectly()
+    {
+        // Arrange
+        var handlerTypes = GetHandlerTypes();
+
+        var builder = Host.CreateApplicationBuilder();
+        builder.Services.AddApplicationHandlersFromTypes(handlerTypes);
+
+        var app = builder.Build();
+        var serviceProvider = app.Services;
+
+        // Act
+        var commandHandler = serviceProvider.GetService<IRequestHandler<MockCommandRequest, MockResponse>>();
+        var singleQueryHandler = serviceProvider.GetService<IRequestHandler<MockSingleQueryRequest, MockResponse>>();
+        var collectionQueryHandler = serviceProvider.GetService<IRequestHandler<MockCollectionQueryRequest, MockResponse>>();
+
+        // Assert
+        commandHandler.Should().NotBeNull();
+        singleQueryHandler.Should().NotBeNull();
+        collectionQueryHandler.Should().NotBeNull();
+
+        commandHandler.Should().BeOfType<MockCommandHandler>();
+        singleQueryHandler.Should().BeOfType<MockSingleQueryHandler>();
+        collectionQueryHandler.Should().BeOfType<MockCollectionQueryHandler>();
+
+        app.Dispose();
+    }
+
+    [Fact]
+    public void ApplicationHandlerRegistrar_ShouldThrowWhenHandlerAlreadyRegistered()
+    {
+        // Arrange
+        Type[] dupliecatedHandlerTypes = [typeof(MockCommandHandler), typeof(MockCommandHandlerDuplicated)];
+        var builder = Host.CreateApplicationBuilder();
+
+        //Act
+        Action act = () => builder.Services.AddApplicationHandlersFromTypes(dupliecatedHandlerTypes);
+
+        // Assert
+        act.Should().Throw<HandlerRegistrationAlreadyExistsException>();
+    }
+
+    public record MockResponse : IResponse
+    {
+
+    }
+    public record MockCommandRequest : ICommandRequest<MockResponse>
+    {
+        public Guid ClientRequestId { get; } = Guid.NewGuid();
+    }
+     
+    public record MockSingleQueryRequest : IQueryRequest<MockResponse>
+    {
+    }
+
+    public record MockCollectionQueryRequest : IQueryRequest<MockResponse>
+    {
+    }
+
+
+    public class MockCommandHandler : CommandHandler<MockCommandRequest, MockResponse>
+    {
+        protected override Task<Result<MockResponse>> HandleAsync(MockCommandRequest command, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Result.Success(new MockResponse()));
+        }
+    }
+
+    public class MockCommandHandlerDuplicated : CommandHandler<MockCommandRequest, MockResponse>
+    {
+        protected override Task<Result<MockResponse>> HandleAsync(MockCommandRequest command, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Result.Success(new MockResponse()));
+        }
+    }
+
+    public class MockSingleQueryHandler : SingleResultQueryHandler<MockSingleQueryRequest, MockResponse>
+    {
+        public override Task<Result<MockResponse>> HandleAsync(MockSingleQueryRequest request, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Result.Success(new MockResponse()));
+        }
+    }
+
+    public class MockCollectionQueryHandler : CollectionQueryHandler<MockCollectionQueryRequest, MockResponse>
+    {
+        public override Task<IReadOnlyList<MockResponse>> HandleAsync(MockCollectionQueryRequest request, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult<IReadOnlyList<MockResponse>>(new List<MockResponse>());
+        }
+    }
+
+    public record MockDomainEvent : IDomainEvent
+    {
+    }
+
+    public class MockEventHandler : IDomainEventHandler<MockDomainEvent>
+    {
+        public Task HandleAsync(MockDomainEvent domainEvent)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
+
