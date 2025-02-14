7ed35f9b2250f67e6de733b9c58c7c9ab2a9870f
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-14 10:57:24 +0100

    Add Email model for email message representation

---
+++ src/AtendeLogo.Application/Models/Email.cs
@@ -0,0 +1,19 @@
+﻿namespace AtendeLogo.Application.Models;
+
+public record Email
+{
+    public required string To { get; init; }
+    public required string Subject { get; init; }
+    public required string Body { get; init; }
+    public required string From { get; init; }
+    public string? FromName { get; init; }
+
+    public Email(string to, string subject, string body, string from, string fromName)
+    {
+        To = to;
+        Subject = subject;
+        Body = body;
+        From = from;
+        FromName = fromName;
+    }
+}
