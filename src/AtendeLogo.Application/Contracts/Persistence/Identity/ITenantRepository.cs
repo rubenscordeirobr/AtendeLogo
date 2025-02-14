9e7b8d50a7795fd0ccebfe66e59ffc26bdeb05f1
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-14 10:57:38 +0100

    Add methods to check for existing fiscal codes in ITenantRepository

--- src/AtendeLogo.Application/Contracts/Persistence/Identity/ITenantRepository.cs
+++ src/AtendeLogo.Application/Contracts/Persistence/Identity/ITenantRepository.cs
@@ -5,4 +5,6 @@ public interface ITenantRepository : IRepositoryBase<Tenant>
     Task<Tenant?> GetByNameAsync(string name, CancellationToken cancellationToken);
     Task<bool> EmailExistsAsync(string email, CancellationToken token);
     Task<bool> EmailExistsAsync(Guid currentTenant_Id, string email, CancellationToken token);
+    Task<bool> FiscalCodeExistsAsync( string fiscalCode, CancellationToken token);
+    Task<bool> FiscalCodeExistsAsync(Guid currentTenant_Id, string fiscalCode, CancellationToken token);
 }
