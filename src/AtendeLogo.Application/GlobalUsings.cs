198db1005687e5cddf4d16fa3784c5c48579496c
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-14 10:58:09 +0100

    Add project references and global usings

---
+++ src/AtendeLogo.Application/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using AtendeLogo.Application.Contracts.Events;
+global using AtendeLogo.Application.Contracts.Mediators;
+global using AtendeLogo.Common;
+global using AtendeLogo.Domain.Entities.Identities;
+global using AtendeLogo.Shared.Enums;
+global using AtendeLogo.Shared.Contracts;
