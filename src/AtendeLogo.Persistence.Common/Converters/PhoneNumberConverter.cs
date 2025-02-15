778982f02fb04126028c6549a894fbb47abd34f0
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:49:08 +0100

    Add PhoneNumberConverter for converting PhoneNumber value objects to strings

---
+++ src/AtendeLogo.Persistence.Common/Converters/PhoneNumberConverter.cs
@@ -0,0 +1,15 @@
+﻿using AtendeLogo.Shared.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace AtendeLogo.Persistence.Common.Converters;
+
+internal class PhoneNumberConverter : ValueConverter<PhoneNumber, string>
+{
+    private PhoneNumberConverter() : base(
+        phoneNumber => phoneNumber.Number,
+        numberString => new PhoneNumber(numberString) )
+    {
+    }
+
+    internal static PhoneNumberConverter Instance { get; } = new();
+}
