f14c64e227c0ff1513136ef23415d1252e1ec2b1
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 16:29:14 +0100

    Add UserSessionRepository class for managing user session data

---
+++ src/AtendeLogo.Persistence.Identity/Repositories/UserSessionRepository.cs
@@ -0,0 +1,18 @@
+﻿using AtendeLogo.Application.Contracts.Persistence.Identity;
+using AtendeLogo.Persistence.Common;
+using AtendeLogo.Persistence.Common.Enums;
+
+namespace AtendeLogo.Persistence.Identity.Repositories;
+internal class UserSessionRepository : RepositoryBase<UserSession>, IUserSessionRepository
+{
+    public UserSessionRepository(
+        IdentityDbContext dbContext, 
+        IUserSessionService userSessionService,
+        TrackingOption trackingOption = TrackingOption.NoTracking)
+        : base(dbContext, userSessionService, trackingOption)
+    {
+    }
+
+    public Task<UserSession?> GetByClientTokenAsync(string sessionToken)
+        => FindAsync(x => x.ClientSessionToken == sessionToken);
+}
