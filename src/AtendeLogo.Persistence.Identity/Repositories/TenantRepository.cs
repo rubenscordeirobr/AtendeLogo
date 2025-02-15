aaebc17b9b16759952ec33548e4edd0dff5f9d03
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 16:29:27 +0100

    Add TenantRepository class for managing Tenant entity data and existence checks

---
+++ src/AtendeLogo.Persistence.Identity/Repositories/TenantRepository.cs
@@ -0,0 +1,49 @@
+﻿using AtendeLogo.Application.Contracts.Persistence.Identity;
+using AtendeLogo.Persistence.Common;
+using AtendeLogo.Persistence.Common.Enums;
+
+namespace AtendeLogo.Persistence.Identity.Repositories;
+
+internal class TenantRepository : RepositoryBase<Tenant>, ITenantRepository
+{
+    public TenantRepository(
+        IdentityDbContext dbContext,
+        IUserSessionService userSessionService,
+        TrackingOption trackingOption = TrackingOption.NoTracking)
+        : base(dbContext, userSessionService, trackingOption)
+    {
+    }
+
+    public Task<Tenant?> GetByNameAsync(
+        string name,
+        CancellationToken cancellationToken)
+        => FindAsync(x => x.Name == name, cancellationToken);
+
+    public Task<bool> EmailExistsAsync(
+        string email,
+        CancellationToken token)
+    {
+        return AnyAsync(x => x.Email == email, token);
+    }
+
+    public Task<bool> EmailExistsAsync(
+        Guid currentTenant_Id,
+        string email, CancellationToken token)
+    {
+        return AnyAsync(x => x.Email == email && x.Id != currentTenant_Id, token);
+    }
+
+    public Task<bool> FiscalCodeExistsAsync(
+        string fiscalCode,
+        CancellationToken token)
+    {
+        return AnyAsync(x => x.FiscalCode == fiscalCode, token);
+    }
+
+    public Task<bool> FiscalCodeExistsAsync(
+        Guid currentTenant_Id,
+        string fiscalCode, CancellationToken token)
+    {
+        return AnyAsync(x => x.FiscalCode == fiscalCode && x.Id != currentTenant_Id, token);
+    }
+}
