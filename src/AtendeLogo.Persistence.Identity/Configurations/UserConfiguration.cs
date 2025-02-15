070164d45e031c256ec06a7b8f9bd588e336460f
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 16:27:39 +0100

    Add UserConfiguration class for User entity with validation and indexing

---
+++ src/AtendeLogo.Persistence.Identity/Configurations/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace AtendeLogo.Persistence.Identity.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.NameMaxLength);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.EmailMaxLength);
+         
+        builder.HasIndex(x => x.Email)
+            .IsUnique();
+ 
+    }
+}
