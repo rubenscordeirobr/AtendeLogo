2a116a37e95917edd5aad24dca52aa52f487f1ff
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:48:40 +0100

    Add NullableGuidValueConverter for handling nullable Guid conversions

---
+++ src/AtendeLogo.Persistence.Common/Converters/NullableGuidValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace AtendeLogo.Persistence.Common.Converters;
+
+public class NullableGuidValueConverter : ValueConverter<Guid?, Guid?>
+{
+    private NullableGuidValueConverter() : base(
+        guid => Convert(guid),
+        guid => Convert(guid))
+    {
+    }
+
+    internal static Guid? Convert(Guid? value)
+    {
+        return value is null || value == default
+            ? null
+            : value;
+    }
+
+    public static NullableGuidValueConverter Instance { get; } = new();
+}
