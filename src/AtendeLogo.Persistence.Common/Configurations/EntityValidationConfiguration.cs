d44e9b8c598435f60259276264910fbf2bf2b5fa
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:47:12 +0100

    Add EntityValidationConfiguration for property validation and max length checks

---
+++ src/AtendeLogo.Persistence.Common/Configurations/EntityValidationConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using AtendeLogo.Persistence.Common.Exceptions;
+using AtendeLogo.Shared.ValueObjects;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AtendeLogo.Persistence.Common.Configurations;
+
+public static class EntityValidationConfiguration
+{
+    private const string DiscriminatorPropertyName = "Discriminator";
+
+    internal static void Validate<TContext>(
+        this IMutableEntityType mutableEntityType )
+        where TContext : DbContext
+    {
+        foreach (var mutableProperty in mutableEntityType.GetProperties())
+        {
+            mutableProperty
+                .CheckMaxLength()
+                .CheckEnums<TContext>();
+        }
+    }
+
+    private static IMutableProperty CheckMaxLength(
+       this IMutableProperty mutableProperty )
+    {
+        if (!mutableProperty.IsPropertyString())
+        {
+            return mutableProperty;
+        }
+
+        if (mutableProperty.Name != DiscriminatorPropertyName &&
+            mutableProperty.GetMaxLength() is null)
+        {
+            var declaringTypeQualifiedName = mutableProperty.PropertyInfo?.DeclaringType?.GetQualifiedName() ?? "Unknown";
+
+            var errorMessage = $"Entity {declaringTypeQualifiedName} property {mutableProperty.Name} must have a max length defined" +
+                              $"Set the max length in EntityBuilderConfiguration using HasMaxLength method";
+           
+            throw new MaxLengthNotDefinedException(errorMessage);
+      
+        }
+        return mutableProperty;
+    }
+
+    private static bool IsPropertyString(this IMutableProperty mutableProperty)
+    {
+        if (mutableProperty.ClrType != typeof(string))
+        {
+            return false;
+        }
+
+        var declaringType = mutableProperty.PropertyInfo?.DeclaringType;
+        if (declaringType == null)
+        {
+            return false;
+        }
+
+        var isDeclaredInValueObject = declaringType
+            .IsSubclassOf<ValueObjectBase>() == true;
+
+        if (isDeclaredInValueObject)
+        {
+            return false;
+        }
+        return true;
+    }
+    }
+
