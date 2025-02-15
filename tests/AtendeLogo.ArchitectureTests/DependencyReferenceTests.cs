badf8e51196fb585f09e74598c016a2803645d12
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 16:26:23 +0100

    Refactor DependencyReferenceTests and update project references in ArchitectureTests

--- tests/AtendeLogo.ArchitectureTests/DependencyReferenceTests.cs
+++ tests/AtendeLogo.ArchitectureTests/DependencyReferenceTests.cs
@@ -6,13 +6,13 @@ namespace AtendeLogo.ArchitectureTests;
 
 public class DependencyReferenceTests
 {
-    private Assembly _commomAssembly;
+    private Assembly _commonAssembly;
     private Assembly _domainAssembly;
     private Assembly _applicationAssembly;
     private Assembly _useCasesAssembly;
     private Assembly _infrastructureAssembly;
-    private Assembly _persistanceAssembly;
-    private Assembly _persistanceActivitiesAssembly;
+    private Assembly _identityPersistanceAssembly;
+    private Assembly _activityPersistanceAssembly;
 
     private Assembly _useCasesSharedAssembly;
     private Assembly _sharedKernelAssembly;
@@ -20,33 +20,32 @@ public class DependencyReferenceTests
     public DependencyReferenceTests()
     {
 
-        _commomAssembly = typeof(Common.Guard).Assembly;
+        _commonAssembly = typeof(Common.Guard).Assembly;
         _domainAssembly = typeof(Domain.Primitives.EntityBase).Assembly;
 
-        _applicationAssembly = typeof(Application.ApplicationConfigurationsExtensions).Assembly;
+        _applicationAssembly = typeof(Application.ApplicationServiceConfiguration).Assembly;
 
-        _useCasesAssembly = typeof(UseCases.UseCasesConfigurationExtensions).Assembly;
-        _infrastructureAssembly = typeof(Infrastructure.InfrastructureConfigurationExtensions).Assembly;
-        _persistanceAssembly = typeof(Persistence.PersistenceConfigurationExtensions).Assembly;
-        _persistanceActivitiesAssembly = typeof(Persistence.Activities.ActivitiesPersistenceConfigurationExtensions).Assembly;
+        _useCasesAssembly = typeof(UseCases.UseCasesServiceConfiguration).Assembly;
+        _infrastructureAssembly = typeof(Infrastructure.InfrastructureServiceConfiguration).Assembly;
+        _identityPersistanceAssembly = typeof(Persistence.Identity.IdentityPersistenceServiceConfiguration).Assembly;
+        _activityPersistanceAssembly = typeof(Persistence.Activity.ActivitiyPersistenceServiceConfiguration).Assembly;
 
         _sharedKernelAssembly = typeof(Shared.ValueObjects.ValueObjectBase).Assembly;
         _useCasesSharedAssembly = typeof(UseCases.Resources.ValidationMessages).Assembly;
-
     }
 
     [Fact]
-    public void CommomAssembly_ShouldNotHaveDependencies()
+    public void CommonAssembly_ShouldNotHaveDependencies()
     {
-        var result = Types.InAssembly(_commomAssembly)
+        var result = Types.InAssembly(_commonAssembly)
             .ShouldNot()
             .HaveDependencyOnAny(
                 _useCasesSharedAssembly.GetName().Name,
                 _domainAssembly.GetName().Name,
                 _applicationAssembly.GetName().Name,
                 _infrastructureAssembly.GetName().Name,
-                _persistanceAssembly.GetName().Name,
-                _persistanceActivitiesAssembly.GetName().Name)
+                _identityPersistanceAssembly.GetName().Name,
+                _activityPersistanceAssembly.GetName().Name)
             .GetResult().IsSuccessful
             .Should()
             .BeTrue();
@@ -62,8 +61,8 @@ public class DependencyReferenceTests
                 _domainAssembly.GetName().Name,
                 _applicationAssembly.GetName().Name,
                 _infrastructureAssembly.GetName().Name,
-                _persistanceAssembly.GetName().Name,
-                _persistanceActivitiesAssembly.GetName().Name)
+                _identityPersistanceAssembly.GetName().Name,
+                _activityPersistanceAssembly.GetName().Name)
             .GetResult()
             .IsSuccessful
             .Should()
@@ -79,8 +78,8 @@ public class DependencyReferenceTests
                _domainAssembly.GetName().Name,
                _applicationAssembly.GetName().Name,
                _infrastructureAssembly.GetName().Name,
-               _persistanceAssembly.GetName().Name,
-               _persistanceActivitiesAssembly.GetName().Name)
+               _identityPersistanceAssembly.GetName().Name,
+               _activityPersistanceAssembly.GetName().Name)
            .GetResult()
            .IsSuccessful
                 .Should()
@@ -95,8 +94,8 @@ public class DependencyReferenceTests
              .HaveDependencyOnAny(
                  _infrastructureAssembly.GetName().Name,
                  _useCasesAssembly.GetName().Name,
-                 _persistanceAssembly.GetName().Name,
-                 _persistanceActivitiesAssembly.GetName().Name)
+                 _identityPersistanceAssembly.GetName().Name,
+                 _activityPersistanceAssembly.GetName().Name)
              .GetResult()
              .IsSuccessful
                 .Should()
@@ -110,8 +109,8 @@ public class DependencyReferenceTests
              .ShouldNot()
              .HaveDependencyOnAny(
                  _infrastructureAssembly.GetName().Name,
-                 _persistanceAssembly.GetName().Name,
-                 _persistanceActivitiesAssembly.GetName().Name)
+                 _identityPersistanceAssembly.GetName().Name,
+                 _activityPersistanceAssembly.GetName().Name)
              .GetResult()
              .IsSuccessful
                 .Should()
