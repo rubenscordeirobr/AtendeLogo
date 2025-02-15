02e76ae095e7686c789f22301d0c4af3b6123c9c
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 16:28:28 +0100

    Add UserSessionConfiguration class for UserSession entity with validation and indexing

---
+++ src/AtendeLogo.Persistence.Identity/Configurations/UserSessionConfiguration.cs
@@ -0,0 +1,49 @@
+﻿namespace AtendeLogo.Persistence.Identity.Configurations;
+
+public class UserSessionConfiguration : IEntityTypeConfiguration<UserSession>
+{
+    public void Configure(EntityTypeBuilder<UserSession> builder)
+    {
+        builder.Property(x => x.ClientSessionToken)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.TokenMaxLength);
+
+        builder.HasIndex(x => x.ClientSessionToken)
+            .IsUnique();
+
+        builder.Property(x => x.AuthToken)
+            .HasMaxLength(ValidationConstants.TokenMaxLength);
+
+        builder.HasIndex(x => x.AuthToken);
+            
+        builder.Property(x => x.ApplicationName)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.NameMaxLength);
+
+        builder.Property(x => x.IpAddress)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.IpAddressMaxLength);
+
+        builder.Property(x => x.UserAgent)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.UserAgentMaxLength);
+
+        builder.Property(x => x.StartedAt)
+            .HasDefaultValueSql("now()")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.LastActivity)
+            .HasDefaultValueSql("now()")
+            .ValueGeneratedOnAdd();
+         
+        builder.HasOne(x => x.User)
+           .WithMany(x => x.Sessions)
+           .HasForeignKey(x => x.User_Id)
+           .IsRequired(false);
+
+        builder.HasOne(x => x.Tenant)
+           .WithMany(x => x.Sessions)
+           .HasForeignKey(x => x.Tenant_Id)
+           .IsRequired(false);
+    }
+}
