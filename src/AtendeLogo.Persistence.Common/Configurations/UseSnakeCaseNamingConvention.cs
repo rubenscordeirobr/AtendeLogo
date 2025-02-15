4480899da310c04443535de3519dc33ff041abff
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:07:43 +0100

    Add UseSnakeCaseNamingConvention extension for entity naming conventions

---
+++ src/AtendeLogo.Persistence.Common/Configurations/UseSnakeCaseNamingConvention.cs
@@ -0,0 +1,52 @@
+﻿using AtendeLogo.Common.Utils;
+
+namespace AtendeLogo.Persistence.Common.Configurations;
+
+public static class UseSnakeCaseNamingConventionExtensions
+{
+    public static void UseSnakeCaseNamingConvention(this ModelBuilder modelBuilder)
+    {
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            var tableName = CaseUtils.ToSnakeCase(entity.GetTableName() ?? string.Empty);
+            entity.SetTableName(tableName);
+
+            foreach (var property in entity.GetProperties())
+            {
+                var columnName = property.GetColumnName();
+                if (!string.IsNullOrWhiteSpace(columnName))
+                {
+                    property.SetColumnName(CaseUtils.ToSnakeCase(columnName));
+                }
+            }
+
+            foreach (var index in entity.GetIndexes())
+            {
+                var indexName = index.GetDatabaseName();
+                if (!string.IsNullOrWhiteSpace(indexName))
+                {
+                    index.SetDatabaseName(CaseUtils.ToSnakeCase(indexName));
+                }
+            }
+
+            foreach (var key in entity.GetKeys())
+            {
+                var keyName = key.GetName();
+                if (!string.IsNullOrWhiteSpace(keyName))
+                {
+                    key.SetName(CaseUtils.ToSnakeCase(keyName));
+                }
+            }
+
+            foreach (var foreignKey in entity.GetForeignKeys())
+            {
+                var foreignKeyName = foreignKey.GetConstraintName();
+                if (!string.IsNullOrWhiteSpace(foreignKeyName))
+                {
+                    foreignKey.SetConstraintName(CaseUtils.ToSnakeCase(foreignKeyName));
+                }
+            }
+        }
+    }
+
+}
