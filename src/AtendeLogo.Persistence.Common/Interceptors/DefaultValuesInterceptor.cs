ae046764936af4b5270ef5172752859cb572bea5
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:51:14 +0100

    Add DefaultValuesInterceptor to handle default value parameters in database commands

---
+++ src/AtendeLogo.Persistence.Common/Interceptors/DefaultValuesInterceptor.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace AtendeLogo.Persistence.Common.Interceptors;
+
+public class DefaultValuesInterceptor : DbCommandInterceptor
+{
+    public override InterceptionResult<DbDataReader> ReaderExecuting(
+       DbCommand command,
+       CommandEventData eventData,
+       InterceptionResult<DbDataReader> result)
+    {
+        ProcessDefaulValueParameters(command);
+        return base.ReaderExecuting(command, eventData, result);
+    }
+
+    public override InterceptionResult<int> NonQueryExecuting(
+        DbCommand command,
+        CommandEventData eventData,
+        InterceptionResult<int> result)
+    {
+        ProcessDefaulValueParameters(command);
+        return base.NonQueryExecuting(command, eventData, result);
+    }
+
+    public override InterceptionResult<object> ScalarExecuting(
+        DbCommand command,
+        CommandEventData eventData,
+        InterceptionResult<object> result)
+    {
+        ProcessDefaulValueParameters(command);
+        return base.ScalarExecuting(command, eventData, result);
+    }
+
+    private void ProcessDefaulValueParameters(DbCommand command)
+    {
+        foreach (DbParameter parameter in command.Parameters)
+        {
+            if (parameter.Value is DateTime dt && dt == default)
+            {
+                parameter.Value = DBNull.Value;
+            }
+
+            if(parameter.Value is Guid guid && guid == default)
+            {
+                parameter.Value = DBNull.Value;
+            }
+        }
+    }
+}
+
