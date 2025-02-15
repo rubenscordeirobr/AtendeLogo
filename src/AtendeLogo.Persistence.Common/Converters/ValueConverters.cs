6ee9e22478bdac82683ffa0849200e7adcb9961a
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:49:23 +0100

    Add ValueConverters class for centralized access to various converters

---
+++ src/AtendeLogo.Persistence.Common/Converters/ValueConverters.cs
@@ -0,0 +1,10 @@
+﻿namespace AtendeLogo.Persistence.Common.Converters;
+
+internal static class ValueConverters
+{
+    internal static PhoneNumberConverter PhoneNumberConverter => PhoneNumberConverter.Instance;
+    internal static NullableUtcDateTimeConverter NullableUtcDateTimeConverter => NullableUtcDateTimeConverter.Instance;
+    internal static UtcDateTimeConverter UtcDateTimeConverter => UtcDateTimeConverter.Instance;
+    internal static NullableGuidValueConverter EmptyNullableGuidConverter => NullableGuidValueConverter.Instance;
+
+}
