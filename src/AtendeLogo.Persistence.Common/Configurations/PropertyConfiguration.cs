b3ff42c0ae6cdfb55ebe984fc77ee047c39fa693
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:05:49 +0100

    Add PropertyConfiguration for entity property settings

---
+++ src/AtendeLogo.Persistence.Common/Configurations/PropertyConfiguration.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using AtendeLogo.Persistence.Common.Converters;
+using AtendeLogo.Persistence.Common.Validations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AtendeLogo.Persistence.Common.Configurations;
+
+internal static class PropertyConfiguration
+{
+    internal static IMutableEntityType ConfigureProperties(this IMutableEntityType mutableEntityType)
+    {
+        foreach (var mutableProperty in mutableEntityType.GetProperties())
+        {
+            mutableProperty
+                .ConfigureCollation()
+                .ConfigureValueConverter();
+        }
+        return mutableEntityType;
+    }
+
+    internal static IMutableProperty ConfigureCollation(this IMutableProperty mutableProperty)
+    {
+        if (mutableProperty.ClrType == typeof(string))
+        {
+            if (mutableProperty.GetCollation() == null)
+            {
+                mutableProperty.SetCollation("case_accent_insensitive");
+            }
+        }
+        return mutableProperty;
+    }
+
+    internal static IMutableProperty ConfigureValueConverter
+        (this IMutableProperty mutableProperty)
+    {
+        if (mutableProperty.ClrType == typeof(DateTime))
+        {
+            mutableProperty.SetValueConverter(ValueConverters.UtcDateTimeConverter);
+        }
+
+        if (mutableProperty.ClrType == typeof(DateTime?))
+        {
+            mutableProperty.SetValueConverter(ValueConverters.NullableUtcDateTimeConverter);
+        }
+        if (mutableProperty.ClrType == typeof(Guid?))
+        {
+            mutableProperty.SetValueConverter(ValueConverters.EmptyNullableGuidConverter);
+        }
+
+        if (RequiresNotEmptyGuidValidation(mutableProperty.PropertyInfo))
+        {
+            var propertyType = mutableProperty.ClrType;
+            if (mutableProperty.ClrType == typeof(Guid))
+            {
+                mutableProperty.SetValueConverter(new NotEmptyGuidValidation(mutableProperty.PropertyInfo));
+            }
+        }
+        return mutableProperty;
+    }
+
+    private static bool RequiresNotEmptyGuidValidation(
+        [NotNullWhen(true)] PropertyInfo? propertyInfo)
+    {
+        if (propertyInfo is null)
+            return false;
+
+        var isGuidType = propertyInfo.PropertyType == typeof(Guid);
+        if (!isGuidType)
+            return false;
+
+        if (propertyInfo.DeclaringType == typeof(EntityBase) &&
+            propertyInfo.Name == nameof(EntityBase.Id))
+            return false;
+
+        return true;
+    }
+}
