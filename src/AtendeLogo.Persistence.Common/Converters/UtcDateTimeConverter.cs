761a9c89bdff5cab11c229e38d16d3ceab49e15e
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:48:51 +0100

    Add NullableUtcDateTimeConverter and UtcDateTimeConverter for UTC date-time handling

---
+++ src/AtendeLogo.Persistence.Common/Converters/UtcDateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace AtendeLogo.Persistence.Common.Converters;
+
+internal class UtcDateTimeConverter : ValueConverter<DateTime, DateTime>
+{
+    private UtcDateTimeConverter() : base(
+        v => ConverterUtcDateTime(v),
+        v => ConverterUtcDateTime(v))
+    {
+    }
+     
+    private static DateTime ConverterUtcDateTime(DateTime dateTime)
+    {
+        if(dateTime == default)
+        {
+            return DateTime.SpecifyKind(default, DateTimeKind.Utc);
+        }
+        return dateTime.Kind == DateTimeKind.Utc 
+            ? dateTime 
+            : DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+    }
+
+    internal static UtcDateTimeConverter Instance { get; } = new UtcDateTimeConverter();
+}
+
