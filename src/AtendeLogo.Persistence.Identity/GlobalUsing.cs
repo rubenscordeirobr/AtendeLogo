1a1ae783c6ebdeab749905bcaa55b1868ad8d609
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 16:27:23 +0100

    Refactor project references in AtendeLogo.Persistence.Identity and add global using

---
+++ src/AtendeLogo.Persistence.Identity/GlobalUsing.cs
@@ -0,0 +1,9 @@
+﻿global using AtendeLogo.Application.Contracts.Services;
+global using AtendeLogo.Common.Enums;
+global using AtendeLogo.Common.Helpers;
+global using AtendeLogo.Domain.Entities.Identities;
+global using AtendeLogo.Shared.Contantes;
+global using AtendeLogo.Shared.Enums;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
