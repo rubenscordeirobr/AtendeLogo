ec9e75a018679ac3b56c9f189d46d1f56c92c3f9
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 14:58:29 +0100

    Add EFExtensions for applying tracking options and checking entity changes

---
+++ src/AtendeLogo.Persistence.Common/Extensions/EFExtensions.cs
@@ -0,0 +1,29 @@
+﻿using AtendeLogo.Persistence.Common.Enums;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace AtendeLogo.Persistence.Common.Extensions;
+
+public static class EFExtensions
+{
+    public static IQueryable<TEntity> ApplyTracking<TEntity>(
+         this IQueryable<TEntity> dbSet,
+         TrackingOption tracking)
+         where TEntity : EntityBase
+    {
+        return tracking == TrackingOption.Tracking
+            ? dbSet
+            : dbSet.AsNoTracking();
+    }
+
+    public static bool HasChanges(this EntityEntry entry)
+    {
+        return entry.State switch
+        {
+            EntityState.Added => true,
+            EntityState.Modified => true,
+            EntityState.Deleted => true,
+            _ => false
+        };
+    }
+}
+
