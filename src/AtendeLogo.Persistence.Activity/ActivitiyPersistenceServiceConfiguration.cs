703201f33e584549d8f8523133e9e7bf0f4ccefa
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 18:00:53 +0100

    Add Activity persistence project and repository implementation

---
+++ src/AtendeLogo.Persistence.Activity/ActivitiyPersistenceServiceConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using AtendeLogo.Application.Contracts.Persistence.Activities;
+using AtendeLogo.Persistence.Activity.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace AtendeLogo.Persistence.Activity;
+
+public static class ActivitiyPersistenceServiceConfiguration
+{
+    public static IServiceCollection AddActivityPersistenceServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSingleton<IMongoClient>(provider =>
+        {
+            var connectionString = configuration.GetConnectionString("ActivityMongoDb");
+            return new MongoClient(connectionString);
+        });
+
+        services.AddScoped(provider =>
+        {
+            var client = provider.GetRequiredService<IMongoClient>();
+            return client.GetDatabase("activityDB");
+        });
+
+        services.AddScoped<IActivityRepository, ActivityRepository>();
+
+        return services;
+    }
+}
