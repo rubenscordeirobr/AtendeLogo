92425f444866fd37cc8beebfc1d1beba882f13be
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-15 15:00:22 +0100

    Add unit tests for MaxLength validation in ModelBuilder configuration

---
+++ tests/AtendeLogo.Persistence.UnitTests/Common/MaxLengthValidationModelBuilderConfigurationTests.cs
@@ -0,0 +1,52 @@
+﻿using AtendeLogo.Domain.Primitives;
+using AtendeLogo.Persistence.Common.Configurations;
+using AtendeLogo.Persistence.Common.Exceptions;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AtendeLogo.Persistence.UnitTests.Common;
+
+public class MaxLengthValidationModelBuilderConfigurationTests
+
+{
+    [Fact]
+    public void EntityBuilderConfiguration_ShouldTrowMaxLengthNotDefinedException()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<MaxLengthTestDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb")
+            .Options;
+
+        // Act
+        Action act = () =>
+        {
+            using var context = new MaxLengthTestDbContext(options);
+            context.Model.FindEntityType(typeof(MaxLengthTestEntity));
+        };
+
+        // Assert
+        act.Should()
+            .Throw<MaxLengthNotDefinedException>();
+
+    }
+
+    public class MaxLengthTestDbContext : DbContext
+    {
+        public DbSet<MaxLengthTestEntity> MaxLengthTestEntities { get; set; }
+
+        public MaxLengthTestDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ConfigureModelDefaultConfiguration<MaxLengthTestDbContext>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class MaxLengthTestEntity : EntityBase
+    {
+        public required string Name { get; set; }
+    }
+}
