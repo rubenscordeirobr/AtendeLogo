d575ad072eea7ca88a3d631692676b44996228d3
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-16 16:51:35 +0100

    Add EntityDeletedEventHandler with unit tests

---
+++ src/AtendeLogo.UseCases/Activities/Events/EntityDeletedEventHandler.cs
@@ -0,0 +1,69 @@
+﻿using AtendeLogo.Domain.Entities.Activities;
+using System.Dynamic;
+using System.Text.Json;
+using AtendeLogo.Domain.Primitives;
+using AtendeLogo.Common.Extensions;
+using AtendeLogo.Application.Contracts.Persistence.Activities;
+
+namespace AtendeLogo.UseCases.Activities.Events;
+
+public sealed class EntityDeletedEventHandler<TEntity> : IEntityDeletedEventHandler<TEntity>
+    where TEntity : EntityBase
+{
+    private readonly IActivityRepository _activityRepository;
+    private readonly IUserSessionService _userSessionService;
+    private readonly ILogger<EntityDeletedEventHandler<TEntity>> _logger;
+
+    public EntityDeletedEventHandler(
+        IActivityRepository activityRepository,
+        IUserSessionService userSessionService,
+        ILogger<EntityDeletedEventHandler<TEntity>> logger )
+    {
+        _activityRepository = activityRepository;
+        _userSessionService = userSessionService;
+        _logger = logger;
+    }
+
+    public async Task HandleAsync(
+        IEntityDeletedEvent<TEntity> domainEvent)
+    {
+        var userSession = _userSessionService.GetCurrentSession();
+        var entity = domainEvent.Entity;
+
+        var properties = domainEvent.PropertyValues
+            .Select(propertyChanged => $"{propertyChanged.PropertyName}: {propertyChanged.Value}")
+            .ToList();
+
+        var description = $"Deleted {entity.GetType().Name} {entity.Id}. Properties: {string.Join(", ", properties)}";
+        dynamic data = new ExpandoObject();
+        foreach (var property in domainEvent.PropertyValues)
+        {
+            ((IDictionary<string, object>)data)[property.PropertyName] = property.Value ?? "null";
+        }
+
+        var dataSerialized = JsonSerializer.Serialize(data);
+        var qualifiedTypeName = entity.GetType().GetQualifiedName();
+
+        var activity = new CreatedActivity
+        {
+            Tenant_Id = userSession.Tenant_Id,
+            UserSession_Id = userSession.Id,
+            ActivityDate = DateTime.UtcNow,
+            Description = description,
+            CreatedData = dataSerialized,
+            QualifiedTypeName = qualifiedTypeName,
+            Entity_Id = entity.Id,
+        };
+
+        try
+        {
+            await _activityRepository.AddAsync(activity);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error on add activity for entity {EntityType} {EntityId}", entity.GetType().Name, entity.Id);
+            throw;
+        }
+
+    }
+}
---
+++ tests/AtendeLogo.Application.UnitTests/UseCases/Activities/Events/EntityDeletedEventHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿namespace AtendeLogo.Application.UnitTests.UseCases.Activities.Events;
+
+public class EntityDeletedEventHandlerTests
+    : IClassFixture<AnonymousServiceProviderMock>
+{
+    private Fixture _figure = new();
+    private IServiceProvider _serviceProvider;
+
+    public EntityDeletedEventHandlerTests(AnonymousServiceProviderMock serviceProviderMock)
+    {
+        _serviceProvider = serviceProviderMock;
+    }
+
+    [Fact]
+    public async Task EventMediator_ShouldDispatch()
+    {
+        // Arrange
+        var entity = _figure.Create<TenantUser>();
+        var eventMediator = _serviceProvider.GetRequiredService<IEventMediator>();
+        var deletedEvent = new EntityDeletedEvent<TenantUser>(entity, []);
+        var eventContext = new DomainEventContext([deletedEvent]);
+        // Act
+        await eventMediator.DispatchAsync(eventContext);
+        // Assert
+        var executedEvents = eventContext.GetExecutedEventResults(deletedEvent);
+        var handlers = executedEvents.Select(x => x.Handler).ToList();
+        handlers.Should()
+            .ContainItemsAssignableTo<EntityDeletedEventHandler<TenantUser>>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldNotThrow()
+    {
+        // Arrange
+        var entity = _figure.Create<TenantUser>();
+        var deletedEvent = new EntityDeletedEvent<TenantUser>(entity, []);
+        var activityRepository = new ActivityRepositoryMock();
+        var userSessionServiceMock = new AnonymousUserSessionServiceMock();
+        var handler = new EntityDeletedEventHandler<TenantUser>(
+            activityRepository,
+            userSessionServiceMock,
+            new LoggerServiceMock<EntityDeletedEventHandler<TenantUser>>());
+        // Act
+        Func<Task> task = async () => await handler.HandleAsync(deletedEvent);
+        // Assert
+        await task.Should().NotThrowAsync();
+    }
+}
+
+
