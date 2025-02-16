f9f05e3ac48da4dd568b3cb7cc9834bc0ad410e6
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-16 12:27:33 +0100

    Add HttpContextExtensions for retrieving request header information

---
+++ src/AtendeLogo.Infrastructure/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AtendeLogo.Common.Infos;
+using Microsoft.AspNetCore.Http;
+
+namespace AtendeLogo.Infrastructure.Extensions;
+
+public static class HttpContextExtensions
+{
+    public static RequestHeaderInfo GetRequestHeaderInfo(
+        this HttpContext context)
+    {
+        if (context == null)
+        {
+            return RequestHeaderInfo.Unknown;
+        }
+
+        var ipAddress = context.Connection.RemoteIpAddress?.ToString() ?? "Unknown";
+        var userAgent = context.Request.Headers["User-Agent"].ToString() ?? "Unknown";
+        var applicationName = context.Request.Headers["Application-Name"].ToString() ?? "Unknown";
+
+        return new RequestHeaderInfo
+        (
+            IpAddress : ipAddress,
+            UserAgent : userAgent,
+            ApplicationName :applicationName
+        );
+    }
+}
