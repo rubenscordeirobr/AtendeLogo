3dcc70681e20bfea873bbdca08f20f0c51331aae
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-16 16:42:27 +0100

    Add EntityCreatedEventHandler with tests

---
+++ src/AtendeLogo.UseCases/Activities/Events/EntityCreatedEventHandler.cs
@@ -0,0 +1,67 @@
+﻿using AtendeLogo.Domain.Entities.Activities;
+using System.Dynamic;
+using System.Text.Json;
+using AtendeLogo.Domain.Primitives;
+using AtendeLogo.Common.Extensions;
+using AtendeLogo.Application.Contracts.Persistence.Activities;
+
+namespace AtendeLogo.UseCases.Activities.Events;
+
+public sealed class EntityCreatedEventHandler<TEntity> : IEntityCreatedEventHandler<TEntity>
+    where TEntity : EntityBase
+{
+    private readonly IActivityRepository _activityRepository;
+    private readonly IUserSessionService _userSessionService;
+    private readonly ILogger<EntityCreatedEventHandler<TEntity>> _logger;
+
+    public EntityCreatedEventHandler(
+        IActivityRepository activityRepository,
+        IUserSessionService userSessionService,
+        ILogger<EntityCreatedEventHandler<TEntity>> logger)
+    {
+        _activityRepository = activityRepository;
+        _userSessionService = userSessionService;
+        _logger = logger;
+    }
+
+    public async Task HandleAsync(IEntityCreatedEvent<TEntity> domainEvent)
+    {
+        var userSession = _userSessionService.GetCurrentSession();
+        var entity = domainEvent.Entity;
+       
+        var properties = domainEvent.PropertyValues
+            .Select(propertyChanged => $"{propertyChanged.PropertyName}: {propertyChanged.Value}")
+            .ToList();
+
+        var description = $"Created {entity.GetType().Name} {entity.Id}. Properties: {string.Join(", ", properties)}";
+        dynamic data = new ExpandoObject();
+        foreach (var property in domainEvent.PropertyValues)
+        {
+            ((IDictionary<string, object>)data)[property.PropertyName] = property.Value ?? "null";
+        }
+        
+        var dataSerialized = JsonSerializer.Serialize(data);
+        var qualifiedTypeName = entity.GetType().GetQualifiedName();
+
+        var activity = new CreatedActivity
+        {
+            Tenant_Id = userSession.Tenant_Id,
+            UserSession_Id = userSession.Id,
+            ActivityDate = DateTime.UtcNow,
+            Description = description,
+            CreatedData = dataSerialized,
+            Entity_Id = entity.Id,
+            QualifiedTypeName = qualifiedTypeName,
+        };
+
+        try
+        {
+            await _activityRepository.AddAsync(activity);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error on add activity for entity {EntityType} {EntityId}", entity.GetType().Name, entity.Id);
+            throw;
+        }
+    }
+}
---
+++ tests/AtendeLogo.Application.UnitTests/UseCases/Activities/Events/EntityCreatedEventHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿namespace AtendeLogo.Application.UnitTests.UseCases.Activities.Events;
+
+public class EntityCreatedEventHandlerTests
+    : IClassFixture<AnonymousServiceProviderMock>
+{
+    private Fixture _figure = new();
+
+    private IServiceProvider _serviceProvider;
+
+    public EntityCreatedEventHandlerTests(AnonymousServiceProviderMock serviceProviderMock)
+    {
+        _serviceProvider = serviceProviderMock;
+    }
+
+    [Fact]
+    public async Task EventMediator_ShouldDispatch()
+    {
+        // Arrange
+        var entity = _figure.Create<TenantUser>();
+        var eventMediator = _serviceProvider.GetRequiredService<IEventMediator>();
+
+        var createdEvent = new EntityCreatedEvent<TenantUser>(entity, []);
+        var eventContext = new DomainEventContext([createdEvent]);
+
+        // Act
+        await eventMediator.DispatchAsync(eventContext);
+        // Assert
+
+        var executedEvents = eventContext.GetExecutedEventResults(createdEvent);
+        var handlers = executedEvents.Select(x => x.Handler).ToList();
+
+        handlers.Should()
+            .ContainItemsAssignableTo<EntityCreatedEventHandler<TenantUser>>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldNotThrow()
+    {
+        // Arrange
+        var entity = _figure.Create<TenantUser>();
+        var createdEvent = new EntityCreatedEvent<TenantUser>(entity, []);
+        var activityRepository = new ActivityRepositoryMock();
+        var userSessionServiceMock = new AnonymousUserSessionServiceMock();
+
+        var handler = new EntityCreatedEventHandler<TenantUser>(
+            activityRepository,
+            userSessionServiceMock,
+            new LoggerServiceMock<EntityCreatedEventHandler<TenantUser>>());
+
+        // Act
+        Func<Task> task = async () => await handler.HandleAsync(createdEvent);
+
+        // Assert
+        await task.Should().NotThrowAsync();
+    }
+}
