9118be73b858b84edb0a9a5cc0ed6fdfaf94be29
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-16 16:50:59 +0100

    Add EntityUpdatedEventHandler with unit tests

---
+++ src/AtendeLogo.UseCases/Activities/Events/EntityUpdatedEventHandler.cs
@@ -0,0 +1,78 @@
+﻿using System.Dynamic;
+using System.Text.Json;
+using AtendeLogo.Application.Contracts.Persistence.Activities;
+using AtendeLogo.Common.Extensions;
+using AtendeLogo.Domain.Entities.Activities;
+using AtendeLogo.Domain.Primitives;
+
+namespace AtendeLogo.UseCases.Activities.Events;
+
+public sealed class EntityUpdatedEventHandler<TEntity> : IEntityUpdatedEventHandler<TEntity>
+    where TEntity : EntityBase
+{
+    private readonly IActivityRepository _activityRepository;
+    private readonly IUserSessionService _userSessionService;
+    private readonly ILogger<EntityUpdatedEventHandler<TEntity>> _logger;
+
+    public EntityUpdatedEventHandler(
+        IActivityRepository activityRepository,
+        IUserSessionService userSessionService,
+        ILogger<EntityUpdatedEventHandler<TEntity>> logger)
+    {
+        _activityRepository = activityRepository;
+        _userSessionService = userSessionService;
+        _logger = logger;
+    }
+
+    public async Task HandleAsync(
+        IEntityUpdatedEvent<TEntity> domainEvent)
+    {
+        var userSession = _userSessionService.GetCurrentSession();
+       
+        var entity = domainEvent.Entity;
+        var properties = domainEvent.ChangedProperties
+            .Select(propertyChanged => $"{propertyChanged.PropertyName}: {propertyChanged.OldValue} -> {propertyChanged.NewValue}")
+            .ToList();
+
+        var description = $"Updated {entity.GetType().Name} {entity.Id}. Properties: {string.Join(", ", properties)}";
+        dynamic newData = new ExpandoObject();
+        
+        foreach (var property in domainEvent.ChangedProperties)
+        {
+            ((IDictionary<string, object>)newData)[property.PropertyName] = property.NewValue ?? "null";
+        }
+
+        dynamic oldData = new ExpandoObject();
+        foreach (var property in domainEvent.ChangedProperties)
+        {
+            ((IDictionary<string, object>)oldData)[property.PropertyName] = property.OldValue ?? "null";
+        }
+
+        var oldDataSerialized = JsonSerializer.Serialize(oldData);
+        var newDataSerialized = JsonSerializer.Serialize(newData);
+
+        var qualifiedTypeName = entity.GetType().GetQualifiedName();
+
+        var activity = new UpdatedActivity
+        {
+            Tenant_Id = userSession.Tenant_Id,
+            UserSession_Id = userSession.Id,
+            ActivityDate = DateTime.UtcNow,
+            Description = description,
+            OldData = oldDataSerialized,
+            NewData = newDataSerialized,
+            QualifiedTypeName = qualifiedTypeName,
+            Entity_Id = entity.Id,
+        };
+
+        try
+        {
+            await _activityRepository.AddAsync(activity);
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError(ex, "Error on add activity for entity {EntityType} {EntityId}", entity.GetType().Name, entity.Id);
+            throw;
+        }
+    }
+}
---
+++ tests/AtendeLogo.Application.UnitTests/UseCases/Activities/Events/EntityUpdatedEventHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿namespace AtendeLogo.Application.UnitTests.UseCases.Activities.Events;
+
+public class EntityUpdatedEventHandlerTests
+    : IClassFixture<AnonymousServiceProviderMock>
+{
+    private Fixture _figure = new();
+    private IServiceProvider _serviceProvider;
+    public EntityUpdatedEventHandlerTests(AnonymousServiceProviderMock serviceProviderMock)
+    {
+        _serviceProvider = serviceProviderMock;
+    }
+
+    [Fact]
+    public async Task EventMediator_ShouldDispatch()
+    {
+        // Arrange
+        var entity = _figure.Create<TenantUser>();
+        var eventMediator = _serviceProvider.GetRequiredService<IEventMediator>();
+        var updatedEvent = new EntityUpdatedEvent<TenantUser>(entity, []);
+        var eventContext = new DomainEventContext([updatedEvent]);
+        // Act
+        await eventMediator.DispatchAsync(eventContext);
+        // Assert
+        var executedEvents = eventContext.GetExecutedEventResults(updatedEvent);
+        var handlers = executedEvents.Select(x => x.Handler).ToList();
+        handlers.Should()
+            .ContainItemsAssignableTo<EntityUpdatedEventHandler<TenantUser>>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldNotThrow()
+    {
+        // Arrange
+        var entity = _figure.Create<TenantUser>();
+        var updatedEvent = new EntityUpdatedEvent<TenantUser>(entity, []);
+        var activityRepository = new ActivityRepositoryMock();
+        var userSessionServiceMock = new AnonymousUserSessionServiceMock();
+        var handler = new EntityUpdatedEventHandler<TenantUser>(
+            activityRepository,
+            userSessionServiceMock,
+            new LoggerServiceMock<EntityUpdatedEventHandler<TenantUser>>());
+       
+        // Act
+        Func<Task> task = async () => await handler.HandleAsync(updatedEvent);
+        
+        // Assert
+        await task.Should().NotThrowAsync();
+    }
+}
+
+
