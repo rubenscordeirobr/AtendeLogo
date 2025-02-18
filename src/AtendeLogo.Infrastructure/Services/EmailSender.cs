01f6633c1b93117be88c61a27f25539a3642b9ff
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 09:35:42 +0100

    Add EmailSender  and JsonStringLocalizer service as placeholders without implementation yet.

---
+++ src/AtendeLogo.Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using AtendeLogo.Application.Models;
+
+namespace AtendeLogo.Infrastructure.Services;
+
+public class EmailSender : IEmailSender
+{
+    public async Task<bool> SendEmailAsync(Email email)
+    {
+        //todo implement email sending
+        await Task.Delay(1000);
+        return true;
+    }
+}
---
+++ src/AtendeLogo.Infrastructure/Services/JsonStringLocalizer.cs
@@ -0,0 +1,9 @@
+﻿using AtendeLogo.Shared.Contracts;
+
+namespace AtendeLogo.Infrastructure.Services;
+
+public class JsonStringLocalizer<T> : IJsonStringLocalizer<T>
+{
+    public string this[string name, string defaultValue]
+        => defaultValue;
+}
