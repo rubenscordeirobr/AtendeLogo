dca7e26ccb2efb6b7f4c68bddc03c6af7e526cc0
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 10:18:39 +0100

    Add global using directives for unit tests in AtendeLogo.Application

---
+++ tests/AtendeLogo.Application.UnitTests/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿global using AtendeLogo.Application.Contracts.Mediators;
+global using AtendeLogo.Application.Contracts.Persistence.Activities;
+global using AtendeLogo.Application.Contracts.Services;
+global using AtendeLogo.Application.Events;
+global using AtendeLogo.Application.UnitTests.Common;
+global using AtendeLogo.Application.UnitTests.Mocks;
+global using AtendeLogo.Application.UnitTests.Mocks.Infrastructure;
+global using AtendeLogo.Application.UnitTests.Mocks.Repositories;
+global using AtendeLogo.Common.Enums;
+global using AtendeLogo.Domain.Entities.Identities;
+global using AtendeLogo.Domain.Primitives;
+global using AtendeLogo.Shared.Enums;
+global using AtendeLogo.Shared.ValueObjects;
+global using AtendeLogo.UseCases.Activities.Events;
+global using AutoFixture;
+global using FluentAssertions;
+global using Microsoft.Extensions.DependencyInjection;
