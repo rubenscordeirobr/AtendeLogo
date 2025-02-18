fe1205a3c40ef162b7664d8dca6dad6f3f55966c
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 07:39:55 +0100

    Add GuidExtensions with methods for zero-prefixed GUIDs and corresponding unit tests

---
+++ src/AtendeLogo.Common/Extensions/GuidExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AtendeLogo.Common.Extensions;
+
+public static class GuidExtensions
+{
+    public static bool IsZeroPrefixedGuid(this Guid guid)
+    {
+        var zero = (byte)0x0;
+
+        var bytes = MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref guid, 1));
+        return bytes[0] == zero &&
+               bytes[1] == zero &&
+               bytes[2] == zero &&
+               bytes[3] == zero &&
+               bytes[4] == zero;
+    }
+
+    public static Guid NewGuidZeroPrefixed()
+    {
+        var guid = Guid.NewGuid();
+        var zero = (byte)0x0;
+        var bytes = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref guid, 1));
+        bytes[0] = zero;
+        bytes[1] = zero;
+        bytes[2] = zero;
+        bytes[3] = zero;
+        bytes[4] = zero;
+        return MemoryMarshal.Read<Guid>(bytes);
+    }
+}
---
+++ tests/AtendeLogo.Common.UnitTests/Extensions/GuidExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿namespace AtendeLogo.Common.UnitTests.Extensions;
+
+public class GuidExtensionsTests
+{
+    [Fact]
+    public void IsZeroPrefixedGuid_ShouldReturnTrue_WhenGuidIsZeroPrefixed()
+    {
+        // Arrange
+        var guid = GuidExtensions.NewGuidZeroPrefixed();
+
+        // Act
+        var result = guid.IsZeroPrefixedGuid();
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsZeroPrefixedGuid_ShouldReturnFalse_WhenGuidIsNotZeroPrefixed()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+
+        // Act
+        var result = guid.IsZeroPrefixedGuid();
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void NewGuidZeroPrefixed_ShouldReturnGuidWithZeroPrefix()
+    {
+        // Act
+        var guid = GuidExtensions.NewGuidZeroPrefixed();
+
+        // Assert
+        var bytes = guid.ToByteArray();
+        bytes[0].Should().Be(0x0);
+        bytes[1].Should().Be(0x0);
+        bytes[2].Should().Be(0x0);
+        bytes[3].Should().Be(0x0);
+        bytes[4].Should().Be(0x0);
+    }
+}
+
