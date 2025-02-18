050aa13d911c3cf0f34c0f8d3f1cc0c7938e8b6d
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 11:36:05 +0100

    Add GlobalEventHandler for testing purposes and unit tests for event dispatching

---
+++ src/AtendeLogo.UseCases/Events/GlobalEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using AtendeLogo.Domain.Primitives.Contracts;
+
+namespace AtendeLogo.UseCases.Events;
+internal class GlobalEventHandler : IDomainEventHandler<IDomainEvent>
+{
+    private readonly ILogger<GlobalEventHandler> _logger;
+  
+    public GlobalEventHandler(ILogger<GlobalEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task HandleAsync(IDomainEvent domainEvent)
+    {
+        //tests propose
+        _logger.LogInformation("Handling event {EventName}", domainEvent.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/AtendeLogo.Application.UnitTests/UseCases/Events/GlobalEventHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using AtendeLogo.Application.UnitTests.Extensions;
+using AtendeLogo.Domain.Entities.Identities.Events;
+using AtendeLogo.UseCases.Events;
+
+namespace AtendeLogo.Application.UnitTests.UseCases.Events;
+
+public class GlobalEventHandlerTests
+     : IClassFixture<AnonymousServiceProviderMock>
+{
+    private Fixture _figure = new();
+    private IEventMediator _eventMediator;
+
+    public GlobalEventHandlerTests(AnonymousServiceProviderMock serviceProviderMock)
+    {
+        _eventMediator = serviceProviderMock.GetRequiredService<IEventMediator>();
+    }
+
+    [Fact]
+    public async Task EventMediator_ShouldDispatchForCreateEntityEvent()
+    {
+        // Arrange
+        var entity = _figure.Create<TenantUser>();
+        var createdEvent = new EntityCreatedEvent<TenantUser>(entity, []);
+        var eventContext = new DomainEventContext([createdEvent]);
+       
+        // Act
+        await _eventMediator.DispatchAsync(eventContext);
+        // Assert
+
+        eventContext
+            .ShouldHaveExecutedEvent(createdEvent)
+            .WithHandler<GlobalEventHandler>();
+    }
+
+    [Fact]
+    public async Task EventMediator_ShouldDispatchForPasswordChanged()
+    {
+        // Arrange
+        var entity = _figure.Create<TenantUser>();
+        var passwordChangedEvent = new PasswordChangedEvent(entity);
+        var eventContext = new DomainEventContext([passwordChangedEvent]);
+
+        // Act
+        await _eventMediator.DispatchAsync(eventContext);
+        // Assert
+
+        eventContext
+            .ShouldHaveExecutedEvent(passwordChangedEvent)
+            .WithHandler<GlobalEventHandler>();
+    }
+}
+
