e0753787c99ddd284d894d7d474198c66b0ccc19
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 09:46:13 +0100

    Add EnumValueValidator

---
+++ src/AtendeLogo.UseCases.Shared/Common/Validations/EnumValueValidator.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using AtendeLogo.Common.Attributes;
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace AtendeLogo.UseCases.Common.Validations;
+
+public static partial class DefaultValidationsExtensions
+{
+    [Obsolete("Use IsInEnumValue instead.")]
+    public static IRuleBuilderOptions<T, TProperty> IsInEnum<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder)
+    {
+        throw new Exception("Use IsInEnumValue instead.");
+    }
+
+    public static IRuleBuilderOptions<T, TProperty> IsInEnumValue<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder)
+    {
+        return ruleBuilder
+            .SetValidator(new EnumValueValidator<T, TProperty>());
+
+    }
+}
+
+public class EnumValueValidator<T, TProperty> : EnumValidator<T, TProperty>
+{
+    public override string Name
+        => "EnumValueValidator";
+
+    public override bool IsValid(ValidationContext<T> context, TProperty value)
+    {
+        if (!base.IsValid(context, value))
+        {
+            return false;
+        }
+
+        var member = EnumType.GetMember(value!.ToString()!)
+            .FirstOrDefault();
+
+        if (member is null)
+            return false;
+
+        return member.GetCustomAttribute<UndefinedValueAttribute>() == null;
+    }
+}
