76f814578b6d7a9a483648a0a7f94d07c2d8c1f9
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 10:25:34 +0100

    Add ResultExtensions for validation result assertions in unit tests

---
+++ tests/AtendeLogo.Application.UnitTests/Common/ResultExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+using AtendeLogo.Common;
+using AtendeLogo.UseCases.Identities.Tenants.Commands;
+
+namespace AtendeLogo.Application.UnitTests.Common;
+
+public static class ResultExtensions
+{
+    public static void ShouldBeSuccessful<TResponse>(
+        this Result<TResponse> result)
+        where TResponse : notnull
+    {
+        result.IsSuccess
+            .Should()
+            .BeTrue();
+
+        result.IsFailure
+            .Should()
+            .BeFalse();
+
+        result.Error
+            .Should()
+            .BeNull();
+    }
+
+    public static void ShouldHaveValidationErrorFor<TRequest, TResponse>(
+        this Result<TResponse> result,
+        Expression<Func<TRequest, object>> propertyExpression)
+        where TResponse : notnull
+    {
+        result.Error
+            .Should()
+            .NotBeNull();
+
+        result.IsFailure
+            .Should()
+            .BeTrue();
+
+        result.IsSuccess
+            .Should()
+            .BeFalse();
+
+        var errorCode = ErrorCodeFactory.CreateInvalidCodeFor(propertyExpression);
+        result.Error.Should()
+            .BeOfType<ValidationError>()
+            .Subject.Code
+            .Should()
+            .Be(errorCode);
+
+    }
+
+    public static void ShouldHaveValidationErrorFor(
+        this Result<CreateTenantResponse> result,
+        Expression<Func<CreateTenantCommand, object>> propertyExpression)
+    {
+        ShouldHaveValidationErrorFor<CreateTenantCommand, CreateTenantResponse>(
+            result, 
+            propertyExpression);
+    }
+}
+
