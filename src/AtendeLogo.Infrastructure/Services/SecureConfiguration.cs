b63ef6d31b41582c11a4387c366f33d167e73de1
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 09:36:00 +0100

    Add SecureConfiguration service to manage password salt retrieval

---
+++ src/AtendeLogo.Infrastructure/Services/SecureConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using AtendeLogo.Application.Contracts.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace AtendeLogo.Infrastructure.Services;
+
+public class SecureConfiguration : ISecureConfiguration
+{
+    private readonly IConfiguration _configuration;
+    private readonly IHostEnvironment _hostEnvironment;
+
+    public SecureConfiguration(
+        IConfiguration configuration,
+        IHostEnvironment hostEnvironment)
+    {
+        _configuration = configuration;
+        _hostEnvironment = hostEnvironment;
+    }
+
+    public string GetPasswordSalt()
+    {
+        if (_hostEnvironment.IsDevelopment())
+        {
+            return "dev-salt";
+        }
+
+        var salt = _configuration["AppSettings:PasswordSalt"];  
+        if (string.IsNullOrEmpty(salt))
+        {
+            salt = Environment.GetEnvironmentVariable("APP_PASSWORD_SALT");
+        }
+ 
+        if (string.IsNullOrEmpty(salt))
+        {
+            throw new Exception("Salt key is not configured properly.");
+        }
+        return salt;
+    }
+}
