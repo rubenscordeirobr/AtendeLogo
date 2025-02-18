5c416b09ec7c5ae8843feb98bd479ef0c4071d79
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 09:45:10 +0100

    Add abstract CommandValidator class for command validation

---
+++ src/AtendeLogo.UseCases.Shared/Common/Validations/CommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+namespace AtendeLogo.UseCases.Common.Validations;
+
+public abstract class CommandValidator<TCommand> : AbstractValidator<TCommand>
+{
+    protected readonly IJsonStringLocalizer<ValidationMessages> Localized;
+
+    protected CommandValidator(IJsonStringLocalizer<ValidationMessages> localizer)
+    {
+        Localized = localizer;
+        ClassLevelCascadeMode = CascadeMode.Stop;
+    }
+}
