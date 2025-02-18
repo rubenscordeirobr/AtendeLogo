0698eed5a911a768877bbe4e036f21a37942bb07
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 07:36:35 +0100

    Add ExpressionExtensions with GetMemberName method and unit tests

---
+++ src/AtendeLogo.Common/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace AtendeLogo.Common.Extensions;
+
+public static class ExpressionExtensions
+{
+    public static string GetMemberName<T>(this Expression<Func<T, object>> expression)
+    {
+        Expression body = expression.Body;
+
+        if (body is UnaryExpression unaryExpression)
+        {
+            body = unaryExpression.Operand;
+        }
+
+        if (body is MemberExpression memberExpression)
+        {
+            return memberExpression.Member.Name;
+        }
+
+        throw new ArgumentException($"The Expression '{expression}' is not a member expression", nameof(expression));
+    }
+}
+
---
+++ tests/AtendeLogo.Common.UnitTests/Extensions/ExpressionExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+
+namespace AtendeLogo.Common.UnitTests.Extensions;
+
+public class ExpressionExtensionsTests
+{
+    [Fact]
+    public void GetMemberName_ShouldReturnMemberName_WhenExpressionIsValid()
+    {
+        // Arrange
+        Expression<Func<TestClass, object>> expression = x => x.TestProperty;
+
+        // Act
+        var memberName = expression.GetMemberName();
+
+        // Assert
+        memberName.Should().Be(nameof(TestClass.TestProperty));
+    }
+
+    [Fact]
+    public void GetMemberName_ShouldThrowArgumentException_WhenExpressionIsNotMemberExpression()
+    {
+        // Arrange
+#pragma warning disable CS8603 // Possible null reference return.
+        Expression<Func<TestClass, object>> expression = x => x.ToString();
+#pragma warning restore CS8603 // Possible null reference return.
+
+        // Act
+        Action act = () => expression.GetMemberName();
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("The Expression 'x => x.ToString()' is not a member expression*");
+    }
+
+    private class TestClass
+    {
+        public string? TestProperty { get; set; }
+    }
+}
+
