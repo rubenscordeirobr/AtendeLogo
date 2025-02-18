7345cb82af1ee97bf784528b0874b3e70b161a89
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 10:05:06 +0100

    Add mock implementation for IActivityRepository in unit tests

---
+++ tests/AtendeLogo.Application.UnitTests/Mocks/Repositories/ActivityRepositoryMock.cs
@@ -0,0 +1,26 @@
+﻿using AtendeLogo.Domain.Entities.Activities;
+
+namespace AtendeLogo.Application.UnitTests.Mocks.Repositories;
+
+internal class ActivityRepositoryMock : IActivityRepository
+{
+    public Task<IEnumerable<ActivityBase>> GetAllAsync()
+    {
+        return Task.FromResult<IEnumerable<ActivityBase>>([]);
+    }
+
+    public Task<ActivityBase?> GetByIdAsync(string id)
+    {
+        return Task.FromResult<ActivityBase?>(null);
+    }
+
+    public Task AddAsync(ActivityBase activity)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(string id)
+    {
+        return Task.CompletedTask;
+    }
+}
