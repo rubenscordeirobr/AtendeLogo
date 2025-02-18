35476b4173d77be987dffa10efff852da360862a
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-18 10:19:16 +0100

    Add global using directives and update project references in UseCases

---
+++ src/AtendeLogo.UseCases/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using AtendeLogo.Application.Contracts.Events;
+global using AtendeLogo.Application.Contracts.Services;
+global using AtendeLogo.Application.Queries;
+global using AtendeLogo.Domain.Entities.Identities;
+global using AtendeLogo.Shared.Enums;
+global using AtendeLogo.Shared.ValueObjects;
+global using Microsoft.Extensions.Logging;
