4f375b45326d8167953874016b0237d574868290
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-19 11:37:15 +0100

    Add UserResponse record for user identity management

---
+++ src/AtendeLogo.UseCases.Shared/Identities/Users/Shared/UserResponse.cs
@@ -0,0 +1,13 @@
+﻿using AtendeLogo.UseCases.Common;
+
+namespace AtendeLogo.UseCases.Identities.Users.Shared;
+
+public abstract record UserResponse : ResponseBase, IUser
+{
+    public required Guid Id { get; init; }
+    public required string Name { get; init; }
+    public required string Email { get; init; }
+    public required PhoneNumber PhoneNumber { get; init; }
+    public required UserState UserState { get; init; }
+    public required UserStatus UserStatus { get; init; }
+}
