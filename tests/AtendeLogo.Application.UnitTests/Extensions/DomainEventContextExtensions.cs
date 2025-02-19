ff523bed7457251385dc4aef98d16e296b4ffc63
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-19 11:27:50 +0100

    Add DomainEventContextExtensions for event execution assertions in unit tests

---
+++ tests/AtendeLogo.Application.UnitTests/Extensions/DomainEventContextExtensions.cs
@@ -0,0 +1,42 @@
+﻿using AtendeLogo.Application.Contracts.Handlers;
+using AtendeLogo.Domain.Primitives.Contracts;
+
+namespace AtendeLogo.Application.UnitTests.Extensions;
+
+public static class DomainEventContextExtensions
+{
+    public static ExecutedEventContext<TEvent> ShouldHaveExecutedEvent<TEvent>(
+        this DomainEventContext context,
+        TEvent domainEvent)
+        where TEvent : IDomainEvent
+    {
+        var executedEvents = context.GetExecutedEventResults(domainEvent);
+        executedEvents.Should()
+            .Contain(x => x.DomainEvent.Equals(domainEvent));
+
+        return new ExecutedEventContext<TEvent>(executedEvents);
+    }
+}
+
+public class ExecutedEventContext<TEvent>
+    where TEvent : IDomainEvent
+{
+    private readonly IReadOnlyList<ExecutedDomainEventResult> _executedEvents;
+  
+    public ExecutedEventContext(
+        IReadOnlyList<ExecutedDomainEventResult> executedEvents)
+    {
+        _executedEvents = executedEvents;
+    }
+
+    public ExecutedEventContext<TEvent> WithHandler<THandler>()
+        where THandler : IApplicationHandler
+    {
+        var handlerType = typeof(THandler);
+
+        _executedEvents.Should()
+            .ContainSingle(x => x.HandlerType == handlerType 
+                            || x.ImplementationHandlerType == handlerType);
+        return this;
+    }
+}
