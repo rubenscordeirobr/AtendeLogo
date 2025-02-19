3c41e93022e300bfbcab5b21842c66d248a1bd8c
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-19 17:08:52 +0100

    Update IUserRepository methods to use default CancellationToken

--- src/AtendeLogo.Application/Contracts/Persistence/Identity/IUserRepository.cs
+++ src/AtendeLogo.Application/Contracts/Persistence/Identity/IUserRepository.cs
@@ -3,10 +3,10 @@
 public interface IUserRepository<TUserEntity> : IRepositoryBase<TUserEntity>
     where TUserEntity : User
 {
-    Task<bool> EmailExistsAsync(string email, CancellationToken cancellationToken);
-    Task<bool> EmailExistsAsync(string email, Guid userId, CancellationToken cancellationToken);
-    Task<bool> PhoneNumberExistsAsync(string phoneNumber, CancellationToken cancellationToken);
-    Task<bool> PhoneNumberExistsAsync(string phoneNumber, Guid userId, CancellationToken cancellationToken);
-    Task<TUserEntity?> GetByEmailAsync(string email, CancellationToken cancellationToken);
-    Task<TUserEntity?> GetByPhoneNumberAsync(string phoneNumber, CancellationToken cancellationToken);
+    Task<bool> EmailExistsAsync(string email, CancellationToken cancellationToken = default);
+    Task<bool> EmailExistsAsync(string email, Guid userId, CancellationToken cancellationToken = default);
+    Task<bool> PhoneNumberExistsAsync(string phoneNumber, CancellationToken cancellationToken = default);
+    Task<bool> PhoneNumberExistsAsync(string phoneNumber, Guid userId, CancellationToken cancellationToken = default);
+    Task<TUserEntity?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task<TUserEntity?> GetByPhoneNumberAsync(string phoneNumber, CancellationToken cancellationToken = default);
 }
