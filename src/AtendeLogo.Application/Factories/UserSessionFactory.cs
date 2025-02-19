e536f2b8bdb3b03c6cb4187ff87f70b388a37f94
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-19 11:11:43 +0100

    Add UserSessionFactory and AnonymousUserSession for anonymous session handling

---
+++ src/AtendeLogo.Application/Factories/UserSessionFactory.cs
@@ -0,0 +1,18 @@
+﻿using AtendeLogo.Application.Models.Identities;
+using AtendeLogo.Common.Infos;
+
+namespace AtendeLogo.Application.Factories;
+
+public static class UserSessionFactory
+{
+    public static IUserSession CreateAnonymousSession(
+        RequestHeaderInfo headerInfo)
+    {
+        return new AnonymousUserSession
+        {
+            ApplicationName = headerInfo.ApplicationName,
+            IpAddress = headerInfo.IpAddress,
+            UserAgent = headerInfo.UserAgent
+        };
+    }
+}
