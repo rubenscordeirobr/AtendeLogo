9613aff4287a7a6e3f5e8c5852269a3f44b33116
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-19 11:35:44 +0100

    Refactor EntityTests to improve readability and add detailed assertions for entity properties

--- tests/AtendeLogo.ArchitectureTests/EntityTests.cs
+++ tests/AtendeLogo.ArchitectureTests/EntityTests.cs
@@ -11,41 +11,46 @@ public class EntityTests
     [Fact]
     public void Entities_ShouldBeSealed()
     {
+        // Arrange
         var assembly = typeof(EntityBase).Assembly;
 
-        Types.InAssembly(assembly)
-            .That()
-            .Inherit(typeof(EntityBase))
-            .And()
-            .AreNotAbstract()
-            .Should()
-            .BeSealed()
-            .GetResult()
-            .IsSuccessful
-                .Should()
-                .BeTrue();
+        //
+        var result = Types.InAssembly(assembly)
+             .That()
+             .Inherit(typeof(EntityBase))
+             .And()
+             .AreNotAbstract()
+             .Should()
+             .BeSealed()
+             .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue(
+            because: "Entities should be sealed or abstract to prevent modification." +
+                     $"Entities fail :{result.GetFailingTypeNames()}");
     }
 
     [Fact]
     public void Entities_PublicPropertiesShouldHavePrivateOrProtectedSetters()
     {
+        // Arrange
         var assembly = typeof(EntityBase).Assembly;
 
-        var publicProperties = Types.InAssembly(assembly)
+        //Act
+        var publicPropertiesWithPublicSetters = Types.InAssembly(assembly)
               .That()
               .Inherit(typeof(EntityBase))
               .GetTypes()
               .SelectMany(type => type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
-              .Where(prop => prop.GetMethod?.IsPublic == true && prop.SetMethod != null);
-
-        var propertiesWithPublicSetters = publicProperties
-            .Where(prop => prop.SetMethod?.IsPublic == true);
-
-        string getBecauseMessage() 
-            => $"These properties should have private or protected setters:{Environment.NewLine}" +
-               $"{string.Join(Environment.NewLine, propertiesWithPublicSetters.Select(p => p.GetPropertyPath()))}";
-
-        propertiesWithPublicSetters.Should()
+              .Where(prop => prop.GetMethod?.IsPublic == true && prop.SetMethod != null)
+              .Where(prop => prop.SetMethod?.IsPublic == true);
+         
+        // Assert
+        publicPropertiesWithPublicSetters.Should()
             .BeEmpty(getBecauseMessage());
+
+        string getBecauseMessage()
+            => $"These properties should have private or protected setters:{Environment.NewLine}" +
+               $"{string.Join(Environment.NewLine, publicPropertiesWithPublicSetters.Select(p => p.GetPropertyPath()))}";
     }
 }
