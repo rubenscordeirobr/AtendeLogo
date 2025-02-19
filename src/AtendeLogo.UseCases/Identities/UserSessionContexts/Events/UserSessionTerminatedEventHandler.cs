4d6cbefa2dc5f3e6f65f657bdff7f17cbac86d5d
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-19 11:26:01 +0100

    Add UserSessionTerminatedEventHandler and corresponding unit tests

---
+++ src/AtendeLogo.UseCases/Identities/UserSessionContexts/Events/UserSessionTerminatedEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using AtendeLogo.Domain.Entities.Identities.Events;
+
+namespace AtendeLogo.UseCases.Identities.UserSessionContexts.Events;
+
+internal class UserSessionTerminatedEventHandler : IDomainEventHandler<UserSessionTerminatedEvent>
+{
+    private readonly ILogger<UserSessionTerminatedEventHandler> _logger;
+
+    public UserSessionTerminatedEventHandler(ILogger<UserSessionTerminatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task HandleAsync(
+        UserSessionTerminatedEvent domainEvent )
+    {
+        //TODO: disconnect user session
+        _logger.LogInformation(
+            "User session {UserSessionId} terminated. Reason: {Reason}",
+            domainEvent.UserSession.Id,
+            domainEvent.Reason);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/AtendeLogo.Application.UnitTests/UseCases/Identities/UserSessionContexts/UserSessionTerminatedEventHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using AtendeLogo.Domain.Entities.Identities.Events;
+using AtendeLogo.UseCases.Identities.UserSessionContexts.Events;
+
+namespace AtendeLogo.Application.UnitTests.UseCases.Identities.UserSessionContexts;
+
+public class UserSessionTerminatedEventHandlerTests
+     : IClassFixture<AnonymousServiceProviderMock>
+{
+    private Fixture _figure = new();
+    private IEventMediator _eventMediator;
+
+    public UserSessionTerminatedEventHandlerTests(AnonymousServiceProviderMock serviceProviderMock)
+    {
+        _eventMediator = serviceProviderMock.GetRequiredService<IEventMediator>();
+    }
+
+    [Fact]
+    public async Task EventMediator_ShouldDispatchForUserSessionTerminatedEvent()
+    {
+        // Arrange
+        var userSession = _figure.Create<UserSession>();
+        var createdEvent = new UserSessionTerminatedEvent(userSession, SessionTerminationReason.SessionTimeout);
+        var eventContext = new DomainEventContext([createdEvent]);
+
+        // Act
+        await _eventMediator.DispatchAsync(eventContext);
+        // Assert
+
+        eventContext
+            .ShouldHaveExecutedEvent(createdEvent)
+            .WithHandler<UserSessionTerminatedEventHandler>();
+    }
+}
+
