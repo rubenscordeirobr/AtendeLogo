94a4fdfd2e09d53d167186dedc06cf970cd61efd
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-19 11:29:43 +0100

    Refactor global usings to improve organization and remove unused references

--- src/AtendeLogo.Infrastructure/GlobalUsing.cs
+++ src/AtendeLogo.Infrastructure/GlobalUsing.cs
@@ -1,6 +1,3 @@
 ﻿global using AtendeLogo.Application.Contracts.Services;
-global using AtendeLogo.Common.Enums;
 global using AtendeLogo.Common.Helpers;
 global using AtendeLogo.Domain.Entities.Identities;
-global using AtendeLogo.Shared.Contantes;
-global using AtendeLogo.Shared.Enums;
--- src/AtendeLogo.Persistence.Common/GlobalUsing.cs
+++ src/AtendeLogo.Persistence.Common/GlobalUsing.cs
@@ -1,12 +1,9 @@
 ﻿global using AtendeLogo.Application.Contracts.Services;
-global using AtendeLogo.Common.Enums;
 global using AtendeLogo.Common.Extensions;
-global using AtendeLogo.Domain.Entities.Identities;
 global using AtendeLogo.Domain.Extensions;
 global using AtendeLogo.Domain.Primitives;
 global using AtendeLogo.Persistence.Common.Extensions;
 global using AtendeLogo.Shared.Contantes;
-global using AtendeLogo.Shared.Enums;
 global using AtendeLogo.Shared.Interfaces.Commom;
 global using AtendeLogo.Shared.Interfaces.Identities;
 global using Microsoft.EntityFrameworkCore;
