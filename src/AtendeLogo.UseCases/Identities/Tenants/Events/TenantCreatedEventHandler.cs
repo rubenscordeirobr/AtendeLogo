1cc6dee8695aa0ceeb9b9abe3ffb4c61efd6d2bb
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-19 11:21:24 +0100

    Add TenantCreatedEventHandler and corresponding unit tests

---
+++ src/AtendeLogo.UseCases/Identities/Tenants/Events/TenantCreatedEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using AtendeLogo.Domain.Entities.Identities.Events;
+
+namespace AtendeLogo.UseCases.Identities.Tenants.Events;
+
+internal class TenantCreatedEventHandler : IDomainEventHandler<TenantCreatedEvent>
+{
+    private ILogger<TenantCreatedEventHandler> _logger;
+ 
+    public TenantCreatedEventHandler(
+        ILogger<TenantCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task HandleAsync(TenantCreatedEvent domainEvent)
+    {
+        _logger.LogInformation("Tenant created: {Tenant}", domainEvent.Tenant);
+        //TODO : Send email to owner
+        //TODO : Create user session for owner to avoid login again
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/AtendeLogo.Application.UnitTests/UseCases/Identities/Tenants/Events/TenantCreatedEventHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using AtendeLogo.Domain.Entities.Identities.Events;
+using AtendeLogo.UseCases.Identities.Tenants.Events;
+
+namespace AtendeLogo.Application.UnitTests.UseCases.Identities.Tenants.Events;
+
+public class TenantCreatedEventHandlerTests
+     : IClassFixture<AnonymousServiceProviderMock>
+{
+    private Fixture _figure = new();
+    private IEventMediator _eventMediator;
+
+    public TenantCreatedEventHandlerTests(AnonymousServiceProviderMock serviceProviderMock)
+    {
+        _eventMediator = serviceProviderMock.GetRequiredService<IEventMediator>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldLogInformation_WhenTenantCreatedEventIsHandled()
+    {
+        // Arrange
+        var tenant = _figure.Create<Tenant>();
+        var tenantUser = _figure.Create<TenantUser>();
+        var createdEvent = new TenantCreatedEvent(tenant, tenantUser);
+        var eventContext = new DomainEventContext([createdEvent]);
+
+        // Act
+        await _eventMediator.DispatchAsync(eventContext);
+
+        // Assert
+        eventContext
+            .ShouldHaveExecutedEvent(createdEvent)
+            .WithHandler<TenantCreatedEventHandler>();
+    }
+}
