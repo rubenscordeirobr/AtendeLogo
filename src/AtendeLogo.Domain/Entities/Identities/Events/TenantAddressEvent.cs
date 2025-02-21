0ce8d6ffaef5d5bb37cf0bca3dc43fb7c52fdc0f
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-21 12:02:08 +0100

    Add TenantAddress entity and related events with configuration

---
+++ src/AtendeLogo.Domain/Entities/Identities/Events/TenantAddressEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace AtendeLogo.Domain.Entities.Identities.Events;
+
+public record TenantAddressAddedEvent(
+    Tenant tenant,
+    TenantAddress newAddress) : IDomainEvent;
+
+public record TenantDefaultAddressUpdatedEvent(
+    Tenant tenant,
+    TenantAddress? previousAddress,
+    TenantAddress address) : IDomainEvent;
+
+public record TenantAddressRemovedEvent(
+    Tenant tenant,
+    TenantAddress address) : IDomainEvent;
---
+++ src/AtendeLogo.Persistence.Identity/Configurations/TenantAddressConfiguration.cs
@@ -0,0 +1,40 @@
+﻿namespace AtendeLogo.Persistence.Identity.Configurations;
+
+public class TenantAddressConfiguration : IEntityTypeConfiguration<TenantAddress>
+{
+    public void Configure(EntityTypeBuilder<TenantAddress> builder)
+    {
+        builder.Property(x => x.AddressName)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.AddressNameMaxLength);
+
+        builder.Property(x => x.Street)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.StreetMaxLength);
+
+        builder.Property(x => x.Number)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.AddressNumberMaxLength);
+
+        builder.Property(x => x.Complement)
+            .IsRequired(false)
+            .HasMaxLength(ValidationConstants.AddressComplementMaxLength);
+
+        builder.Property(x => x.Neighborhood)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.NeighborhoodMaxLength);
+
+        builder.Property(x => x.City)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.CityMaxLength);
+
+        builder.Property(x => x.State)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.AddressStateMaxLength);
+  
+        builder.Property(x => x.ZipCode)
+
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.ZipCodeMaxLength);
+    }
+}
