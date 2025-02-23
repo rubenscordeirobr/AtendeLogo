a086ef4ca3ff9d00d7e073c7d20bf00eaf49e371
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-23 10:23:33 +0100

    Add system tenant seeding logic to IdentityDbSeedExtensions

--- src/AtendeLogo.Persistence.Identity/Seed/IdentityDbSeedExtensions.cs
+++ src/AtendeLogo.Persistence.Identity/Seed/IdentityDbSeedExtensions.cs
@@ -1,6 +1,6 @@
-﻿using AtendeLogo.Application.Models.Identities;
-using AtendeLogo.Common;
+﻿using AtendeLogo.Common;
 using AtendeLogo.Shared.ValueObjects;
+using AtendeLogo.Persistence.Identity.Extensions;
 
 namespace AtendeLogo.Persistence.Identity.Seed;
 
@@ -29,6 +29,7 @@ internal static class IdentityDbSeedExtensions
 
         dbContext.AddAnonymousUserAndSessionAsync();
         dbContext.AddSuperAdminUser();
+        dbContext.AddSystemTenant();
 
         if (!(dbContext is IDbSeedAsync dbSeed))
         {
@@ -56,6 +57,7 @@ internal static class IdentityDbSeedExtensions
             phoneNumber: phoneNumber,
             password);
 
+        
         anonymousUser.SetAnonymousId();
 
         var clientToken = HashHelper.CreateSha256Hash(AnonymousConstants.AnonymousSystemSession_Id);
@@ -76,8 +78,7 @@ internal static class IdentityDbSeedExtensions
         anonymousUserSession.SetAnonymousSystemSessionId();
 
         dbContext.Add(anonymousUser);
-        dbContext.Add(anonymousUserSession); 
-
+        dbContext.Add(anonymousUserSession);
     }
 
     private static void AddSuperAdminUser(this IdentityDbContext dbContext)
@@ -87,6 +88,7 @@ internal static class IdentityDbSeedExtensions
 
         var password = Password.Create(strongPassword, "ANONYMOUS").GetValue();
         var anonymousSession_Id = AnonymousConstants.AnonymousSystemSession_Id;
+
         var anonymousUser = new AdminUser(
             name: "Super Admin",
             email: "superadmin@atendelogo.com.br",
@@ -95,7 +97,53 @@ internal static class IdentityDbSeedExtensions
             adminUserRole: AdminUserRole.SuperAdmin,
             phoneNumber: phoneNumber,
             password);
+
         anonymousUser.SetCreateSession(AnonymousConstants.AnonymousSystemSession_Id);
         dbContext.Add(anonymousUser);
+    }
+
+    internal static void AddSystemTenant(this IdentityDbContext dbContext)
+    {
+        var strongPassword = "TenantAdmin@Teste%#";
+        var password = Password.Create(strongPassword, "SYSTEM")
+            .GetValue();
+
+        var phoneNumber = PhoneNumber.Create(SystemTenantConstants.PhoneNumber)
+            .GetValue();
+
+        var systemTenant = new Tenant(
+            name: SystemTenantConstants.TenantSystemName,
+            fiscalCode: SystemTenantConstants.FiscalCode,
+            email: SystemTenantConstants.TenantSystemName,
+            businessType: BusinessType.System,
+            country: Country.Brazil,
+            currency: Currency.BRL,
+            language: Language.English,
+            tenantState: TenantState.System,
+            tenantStatus: TenantStatus.Active,
+            tenantType: TenantType.System,
+            phoneNumber: phoneNumber,
+            timeZoneOffset: TimeZoneOffset.Default
+        );
+
+        var tenantUser = systemTenant.AddUser(
+             name: SystemTenantConstants.TenantSystemName,
+             email: SystemTenantConstants.TenantSystemEmail,
+             userState: UserState.Active,
+             userStatus: UserStatus.System,
+             tenantUserRole: TenantUserRole.Admin,
+             phoneNumber: phoneNumber,
+             password: password
+         );
+
+        systemTenant.SetCreateSession(AnonymousConstants.AnonymousSystemSession_Id);
+        tenantUser.SetCreateSession(AnonymousConstants.AnonymousSystemSession_Id);
+        
+        tenantUser.SetPropertyValue(x => x.Id, SystemTenantConstants.TenantSystemOwnerUser_Id);
+        systemTenant.SetPropertyValue(x => x.Id, SystemTenantConstants.TenantSystem_Id);
+
+        dbContext.Add(systemTenant);
+
+        
     }
 }
---
+++ src/AtendeLogo.SharedKernel/Contantes/SystemTenantConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace AtendeLogo.Shared.Contantes;
+
+public static class SystemTenantConstants
+{
+    public const string TenantSystemEmail = "systemtenant@atendelogo.com.br";
+    public const string TenantSystemName = "System Tenant";
+
+    public static readonly Guid TenantSystem_Id = new Guid("F4D70ACF-1232-49AD-1FD6-038540DF9F0F");
+    public static readonly Guid TenantSystemOwnerUser_Id = new Guid("F4D70ACF-1232-49AD-1FD6-038540DF9F0F");
+
+    public const string FiscalCode = "48302193000161";
+    public const string PhoneNumber = "+554299997406";
+}
