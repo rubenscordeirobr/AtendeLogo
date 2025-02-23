d5d2e2aa92aeb2030b87e48b4f23e1eff6f706ac
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-23 10:16:24 +0100

    Add InternalsVisibleTo entries for UseCases.UnitTests and ArchitectureTests; include Domain.Exceptions in global usings

--- src/AtendeLogo.Application/GlobalUsings.cs
+++ src/AtendeLogo.Application/GlobalUsings.cs
@@ -5,6 +5,7 @@ global using AtendeLogo.Application.Contracts.Mediators;
 global using AtendeLogo.Common;
 global using AtendeLogo.Common.Enums;
 global using AtendeLogo.Domain.Entities.Identities;
+global using AtendeLogo.Domain.Exceptions;
 global using AtendeLogo.Shared.Contantes;
 global using AtendeLogo.Shared.Contracts;
 global using AtendeLogo.Shared.Enums;
