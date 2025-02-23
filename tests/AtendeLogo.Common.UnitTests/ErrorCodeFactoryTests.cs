22f8b3209486e479ca1ae4ef0e83f10962ff979e
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-23 10:45:11 +0100

    Add unit tests for ErrorCodeFactory to validate code generation logic

---
+++ tests/AtendeLogo.Common.UnitTests/ErrorCodeFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+
+namespace AtendeLogo.Common.UnitTests;
+
+public class ErrorCodeFactoryTests
+{
+    [Theory]
+    [InlineData(typeof(SampleClass), "Property1", "SampleClass.Property1Invalid")]
+    [InlineData(typeof(SampleClass), "Property2", "SampleClass.Property2Invalid")]
+    public void CreateInvalidCodeFor_ShouldReturnExpectedCode(Type type, string propertyName, string expected)
+    {
+        var result = ErrorCodeFactory.CreateInvalidCodeFor(type, propertyName);
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CreateInvalidCodeFor_WithExpression_ShouldReturnExpectedCode()
+    {
+        Expression<Func<SampleClass, object>> expression = x => x.Property1!;
+        var result = ErrorCodeFactory.CreateInvalidCodeFor(expression);
+        result.Should().Be("SampleClass.Property1Invalid");
+    }
+     
+    private class SampleClass
+    {
+        public string? Property1 { get; set; }
+        public string? Property2 { get; set; }
+    }
+}
+
