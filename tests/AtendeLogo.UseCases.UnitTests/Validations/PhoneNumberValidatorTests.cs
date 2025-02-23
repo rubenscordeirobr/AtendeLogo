317c1e03545e657d279ed39e35141c96c770b91e
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-23 10:40:10 +0100

    Add PhoneNumberValidatorTests

---
+++ tests/AtendeLogo.UseCases.UnitTests/Validations/PhoneNumberValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentValidation.TestHelper;
+
+namespace AtendeLogo.UseCases.UnitTests.Validations;
+
+public class PhoneNumberValidatorTests : IClassFixture<AnonymousServiceProviderMock>
+{
+    private readonly IValidator<PhoneNumber> _validator;
+
+    public PhoneNumberValidatorTests(AnonymousServiceProviderMock serviceProviderMock)
+    {
+        _validator = serviceProviderMock.GetRequiredService<IValidator<PhoneNumber>>();
+    }
+
+    [Fact]
+    public void Validator_ShouldBePhoneNumberValidatorType()
+    {
+        _validator.Should().BeOfType<PhoneNumberValidator>();
+    }
+
+        [Fact]
+    public void When_PhoneNumber_IsEmpty_ValidationFails()
+    {
+        // Arrange
+        var phone = new PhoneNumber(string.Empty);
+
+        // Act
+        var result = _validator.TestValidate(phone);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(p => p.Number);
+    }
+
+    [Fact]
+    public void When_PhoneNumber_Exceeds_MaximumLength_ValidationFails()
+    {
+        // Arrange
+        int maxLength = ValidationConstants.PhoneNumberMaxLength;
+        var tooLongNumber = new string('1', maxLength + 1);
+        var phone = new PhoneNumber(tooLongNumber);
+
+        // Act
+        var result = _validator.TestValidate(phone);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(p => p.Number);
+    }
+
+    [Fact]
+    public void When_PhoneNumber_StartWithPlus_ValidationFails()
+    {
+        // Arrange
+        var validNumber = "11234567890";
+        var phone = new PhoneNumber(validNumber);
+
+        // Act
+        var result = _validator.TestValidate(phone);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(p => p.Number);
+    }
+
+    [Fact]
+    public void When_PhoneNumber_IsValid_ValidationSucceeds()
+    {
+        // Arrange
+        var validNumber = "+11234567890";
+        var phone = new PhoneNumber(validNumber);
+
+        // Act
+        var result = _validator.Validate(phone);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+}
+
