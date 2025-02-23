5e97f955c49160fb5f3fed0d69bfd9721fe10973
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-23 10:38:43 +0100

    Add AddressDto and AddressValidator with unit tests for validation

---
+++ src/AtendeLogo.UseCases.Shared/Shared/AddressDto.cs
@@ -0,0 +1,15 @@
+﻿using AtendeLogo.Shared.Interfaces.Shared;
+
+namespace AtendeLogo.UseCases.Shared;
+ 
+public sealed record AddressDto : IAddress
+{
+    public required string Street { get; init; }
+    public required string Number { get; init; }
+    public string? Complement { get; init; }
+    public required string Neighborhood { get; init; }
+    public required string City { get; init; }
+    public required string State { get; init; }
+    public required string ZipCode { get; init; }
+    public required Country Country { get; init; }
+}
