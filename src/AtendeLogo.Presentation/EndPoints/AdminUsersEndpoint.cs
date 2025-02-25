19531eb9f677a5c0de8ca4032ab2d0072be02cdf
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-25 10:33:25 +0100

    Add AdminUsersEndpoint and IAdminUserService for admin user

---
+++ src/AtendeLogo.Presentation/EndPoints/AdminUsersEndpoint.cs
@@ -0,0 +1,66 @@
+﻿using AtendeLogo.Application.Contracts.Mediators;
+using AtendeLogo.Presentation.Common;
+using AtendeLogo.UseCases.Identities.Users.AdminUsers.Queries;
+using AtendeLogo.UseCases.Identities.Users.AdminUsers.Services;
+
+namespace AtendeLogo.Presentation.EndPoints;
+
+[EndPoint("api/admin-users")]
+public class AdminUsersEndpoint : ApiEndpointBase, IAdminUserService
+{
+    private readonly IRequestMediator _mediator;
+    public AdminUsersEndpoint(IRequestMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("{id}")]
+    public Task<Result<AdminUserResponse>> GetAdminUserByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        return _mediator.GetSingleAsync(new GetAdminUserByIdQuery(id), cancellationToken);
+    }
+
+    [HttpGet("email={email}")]
+    public Task<Result<AdminUserResponse>> GetAdminUserByEmailAsync(
+        string email,
+        CancellationToken cancellationToken = default)
+    {
+        return _mediator.GetSingleAsync(new GetAdminUserByEmailQuery(email), cancellationToken);
+    }
+
+    [HttpGet("phone-number={phoneNumber}")]
+    public Task<Result<AdminUserResponse>> GetAdminUserByPhoneNumberAsync(
+        string phoneNumber,
+        CancellationToken cancellationToken = default)
+    {
+        return _mediator.GetSingleAsync(new GetAdminUserByPhoneNumberQuery(phoneNumber), cancellationToken);
+    }
+
+    [HttpGet("/", "email={email}")]
+    public Task<Result<AdminUserResponse>> GetAdminUserByEmailQueryAsync(
+      string email,
+      CancellationToken cancellationToken = default)
+    {
+        return _mediator.GetSingleAsync(new GetAdminUserByEmailQuery(email), cancellationToken);
+    }
+
+    [HttpGet("/", "phone-number={phoneNumber}")]
+    public Task<Result<AdminUserResponse>> GetAdminUserByPhoneNumberQueryAsync(
+        string phoneNumber,
+        CancellationToken cancellationToken = default)
+    {
+        return _mediator.GetSingleAsync(new GetAdminUserByPhoneNumberQuery(phoneNumber), cancellationToken);
+    }
+
+    [HttpGet("/", "email={email}&phoneNumber={phoneNumber}")]
+    public Task<Result<AdminUserResponse>> GetAdminUserByEmailOrPhoneNumberQueryAsync(
+       string email,
+       string phoneNumber,
+       CancellationToken cancellationToken = default)
+    {
+        return _mediator.GetSingleAsync(new GetAdminUserByEmailOrPhoneNumberQuery(email, phoneNumber), cancellationToken);
+    }
+}
+
---
+++ src/AtendeLogo.UseCases.Shared/Identities/Users/AdminUsers/Services/IAdminUserService.cs
@@ -0,0 +1,11 @@
+﻿using AtendeLogo.Common;
+using AtendeLogo.UseCases.Identities.Users.AdminUsers.Queries;
+
+namespace AtendeLogo.UseCases.Identities.Users.AdminUsers.Services;
+
+public interface IAdminUserService
+{
+    Task<Result<AdminUserResponse>> GetAdminUserByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+}
