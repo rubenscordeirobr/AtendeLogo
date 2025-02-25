5de0aca4365bdbe3a12ea3bbf54abe0cd2cbb49b
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-25 07:31:36 +0100

    Add TaskExtensions with GetResult method with tests

---
+++ src/AtendeLogo.Common/Extensions/TaskExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AtendeLogo.Common.Extensions;
+
+public static class TaskExtensions
+{
+    public static object? GetResult(this Task task)
+    {
+        var taskType = task.GetType();
+        if (taskType.IsGenericType)
+        {
+            var resultProperty = taskType.GetProperty("Result");
+            return resultProperty?.GetValue(task);
+        }
+        return null;
+    }
+}
