8b5be41b41c619286408969c02e3f42640a4764f
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-25 10:34:47 +0100

    Add preview language version and framework reference; update global usings

--- src/AtendeLogo.Presentation/GlobalUsings.cs
+++ src/AtendeLogo.Presentation/GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using AtendeLogo.Common;
 global using AtendeLogo.Common.Infos;
 global using AtendeLogo.Presentation.Extensions;
+global using AtendeLogo.Common.Extensions;
+global using AtendeLogo.Presentation.Common.Attributes; 
