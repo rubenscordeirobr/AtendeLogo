a1de74ee1dc88f24046342dcce18c83750815dcf
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-09 14:20:18 +0100

    Update project references and update global usings

--- src/AtendeLogo.UseCases.Shared/GlobalUsings.cs
+++ src/AtendeLogo.UseCases.Shared/GlobalUsings.cs
@@ -1,8 +1,13 @@
-﻿global using AtendeLogo.Common.Enums;
+﻿global using AtendeLogo.Common;
+global using AtendeLogo.Common.Enums;
 global using AtendeLogo.Shared.Contantes;
 global using AtendeLogo.Shared.Contracts;
 global using AtendeLogo.Shared.Enums;
 global using AtendeLogo.Shared.Interfaces.Identities;
 global using AtendeLogo.Shared.ValueObjects;
-global using AtendeLogo.UseCases.Common.Validations;
 global using AtendeLogo.UseCases.Common;
+global using AtendeLogo.UseCases.Common.Services;
+global using AtendeLogo.UseCases.Common.Validations;
+global using AtendeLogo.UseCases.Identities.Tenants.Commands;
+global using AtendeLogo.UseCases.Contracts.Identities;
+global using AtendeLogo.UseCases.Shared;
