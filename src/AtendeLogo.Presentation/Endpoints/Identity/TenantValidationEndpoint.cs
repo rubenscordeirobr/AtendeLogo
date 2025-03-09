e5a532911e37f6e820467915d66e0552b5125306
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-09 14:18:56 +0100

    Add TenantValidationEndpoint for email and fiscal code uniqueness checks

---
+++ src/AtendeLogo.Presentation/Endpoints/Identity/TenantValidationEndpoint.cs
@@ -0,0 +1,57 @@
+﻿using AtendeLogo.Presentation.Common;
+using AtendeLogo.UseCases.Contracts.Identities;
+
+namespace AtendeLogo.Presentation.Endpoints.Identity;
+
+[EndPoint("api/tenant-validation")]
+public class TenantValidationEndpoint : ApiEndpointBase, ITenantValidationService
+{
+    public override bool IsAllowAnonymous
+        => true;
+
+    private readonly ITenantValidationService _tenantValidationService;
+
+    public TenantValidationEndpoint(ITenantValidationService tenantValidationService)
+    {
+        _tenantValidationService = tenantValidationService;
+    }
+
+    [HttpPost]
+    public Task<bool> IsEmailUniqueAsync(string email, CancellationToken token)
+    {
+        return _tenantValidationService.IsEmailUniqueAsync(email, token);
+
+    }
+
+    [HttpPost]
+    public Task<bool> IsEmailUniqueAsync(
+        Guid currentTenant_Id,
+        Guid currentTenantOwner_Id,
+        string email,
+        CancellationToken token)
+    {
+        return _tenantValidationService.IsEmailUniqueAsync(
+            currentTenant_Id,
+            currentTenantOwner_Id,
+            email,
+            token);
+    }
+
+    [HttpPost]
+    public Task<bool> IsFiscalCodeUniqueAsync(string fiscalCode, CancellationToken token)
+    {
+        return _tenantValidationService.IsFiscalCodeUniqueAsync(fiscalCode, token);
+    }
+
+    [HttpPost]
+    public Task<bool> IsFiscalCodeUniqueAsync(
+        Guid currentTenant_Id,
+        string fiscalCode,
+        CancellationToken token)
+    {
+        return _tenantValidationService.IsFiscalCodeUniqueAsync(
+            currentTenant_Id,
+            fiscalCode, token);
+    }
+}
+
