6e7c1be9437a2e479e4b116a77e538a55b31eae1
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-15 19:07:05 +0100

    Add HttpErrorMapper for mapping errors to HTTP status codes and vice versa

---
+++ src/AtendeLogo.Common/HttpErrorMapper.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using AtendeLogo.Common.Enums;
+
+namespace AtendeLogo.Common;
+
+public static class HttpErrorMapper
+{
+    public static HttpStatusCode GetHttpStatusCode(Error error)
+    {
+        return error switch
+        {
+            BadRequestError => HttpStatusCode.BadRequest,
+            UnauthorizedError => HttpStatusCode.Unauthorized,
+            ValidationError => HttpStatusCode.UnprocessableContent,
+            NotFoundError => HttpStatusCode.NotFound,
+            DomainEventError => HttpStatusCode.Conflict,
+            NotImplementedError => HttpStatusCode.NotImplemented,
+            AbortedError => (HttpStatusCode)499,
+            _ => HttpStatusCode.InternalServerError
+        };
+    }
+
+    public static Error GetErrorFromStatus(
+        HttpStatusCode statusCode,
+        string code,
+        string message)
+    {
+        return statusCode switch
+        {
+            HttpStatusCode.InternalServerError
+                => new InternalServerError(null!, code, message),
+            HttpStatusCode.BadRequest
+                => new BadRequestError(code, message),
+            HttpStatusCode.NotFound
+                => new NotFoundError(code, message),
+            HttpStatusCode.UnprocessableContent
+                => new ValidationError(code, message),
+            HttpStatusCode.Conflict
+                => new DomainEventError(code, message),
+            _ => ResolverExtendedStatus(statusCode, code, message)
+        };
+    }
+
+    private static Error ResolverExtendedStatus(
+        HttpStatusCode statusCode,
+        string code,
+        string message)
+    {
+        var extendStatus = (ExtendedHttpStatusCode)statusCode;
+        if (Enum.IsDefined(extendStatus))
+        {
+            return extendStatus switch
+            {
+                ExtendedHttpStatusCode.RequestAborted => new AbortedError(code, message),
+                _ => new NotImplementedError($"ExtendedHttpStatusCode.NotImplemented.{code}", message)
+            };
+        }
+
+        return new NotImplementedError($"HttpStatusCode.NotImplemented.{code}", message);
+    }
+}
+
---
+++ tests/AtendeLogo.Common.UnitTests/HttpErrorMapperTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+
+namespace AtendeLogo.Common.UnitTests;
+
+public class HttpErrorMapperTests
+{
+    [Theory]
+    [MemberData(nameof(GetHttpStatusCodeTestData))]
+    public void GetHttpStatusCode_Should_Return_ExpectedStatus(Error error, HttpStatusCode expectedStatus)
+    {
+        // Act
+        var status = HttpErrorMapper.GetHttpStatusCode(error);
+        // Assert
+        status.Should().Be(expectedStatus);
+    }
+
+    public static IEnumerable<object[]> GetHttpStatusCodeTestData()
+    {
+        yield return new object[] { new BadRequestError("BR001", "Bad request"), HttpStatusCode.BadRequest };
+        yield return new object[] { new UnauthorizedError("UA001", "Unauthorized"), HttpStatusCode.Unauthorized };
+        yield return new object[] { new ValidationError("VE001", "Validation failed"), HttpStatusCode.UnprocessableContent };
+        yield return new object[] { new NotFoundError("NF001", "Not found"), HttpStatusCode.NotFound };
+        yield return new object[] { new DomainEventError("DE001", "Domain event conflict"), HttpStatusCode.Conflict };
+        yield return new object[] { new NotImplementedError("NI001", "Not implemented"), HttpStatusCode.NotImplemented };
+        yield return new object[] { new AbortedError("AB001", "Request aborted"), (HttpStatusCode)499 };
+        yield return new object[] { new InternalServerError(new Exception("dummy"), "ISE001", "Internal server error"), HttpStatusCode.InternalServerError };
+    }
+
+    [Theory]
+    [InlineData(HttpStatusCode.InternalServerError, "ISE001", "Internal server error", "InternalServerError")]
+    [InlineData(HttpStatusCode.BadRequest, "BR001", "Bad request", "BadRequestError")]
+    [InlineData(HttpStatusCode.NotFound, "NF001", "Not found", "NotFoundError")]
+    [InlineData(HttpStatusCode.UnprocessableContent, "VE001", "Validation failed", "ValidationError")]
+    [InlineData(HttpStatusCode.Conflict, "DE001", "Domain event conflict", "DomainEventError")]
+    public void GetErrorFromStatus_Should_Return_CorrectError_Type(
+        HttpStatusCode statusCode, 
+        string code, 
+        string message,
+        string expectedTypeName)
+    {
+        // Act
+        var error = HttpErrorMapper.GetErrorFromStatus(statusCode, code, message);
+        // Assert
+        error.GetType().Name.Should().Be(expectedTypeName);
+        error.Code.Should().Be(code);
+        error.Message.Should().Be(message);
+    }
+
+    [Fact]
+    public void GetErrorFromStatus_Should_Return_AbortedError_For_ExtendedRequestAbortedStatus()
+    {
+        // Arrange
+        var statusCode = (HttpStatusCode)ExtendedHttpStatusCode.RequestAborted;
+        var code = "AB001";
+        var message = "Request aborted";
+        // Act
+        var error = HttpErrorMapper.GetErrorFromStatus(statusCode, code, message);
+        // Assert
+        error.Should().BeOfType<AbortedError>();
+        error.Code.Should().Be(code);
+        error.Message.Should().Be(message);
+    }
+
+    [Fact]
+    public void GetErrorFromStatus_Should_Return_NotImplementedError_For_UnknownExtendedStatus()
+    {
+        // Arrange
+        // Assuming that 600 is not defined in ExtendedHttpStatusCode.
+        var statusCode = (HttpStatusCode)600;
+        var code = "NI001";
+        var message = "Not implemented extended";
+        // Act
+        var error = HttpErrorMapper.GetErrorFromStatus(statusCode, code, message);
+        // Assert
+        error.Should().BeOfType<NotImplementedError>();
+        error.Code.Should().StartWith("HttpStatusCode.NotImplemented.");
+        error.Message.Should().Be(message);
+    }
+}
+
