2f44c876e9ab7f33c898e6841c1b6cd20e3adc44
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-18 08:19:19 +0100

    Add JsonSerializerOptionsHelper for enabling indentation in development with tests

---
+++ src/AtendeLogo.Common/Helpers/JsonSerializerOptionsHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace AtendeLogo.Common.Helpers;
+
+public class JsonSerializerOptionsHelper
+{
+    public static void EnableIndentationInDevelopment(JsonSerializerOptions jsonSerializerOptions)
+    {
+        Guard.NotNull(jsonSerializerOptions);
+
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Development")
+        {
+            return;
+        }
+
+        if (jsonSerializerOptions.WriteIndented)
+            return;
+
+        jsonSerializerOptions.WriteIndented = true;
+    }
+}
