10a570b49dfc51d7af9ab1a16a43744855f8320b
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-19 09:38:00 +0100

    Add UpdateTenantCommand, handler, validator, and unit tests

---
+++ src/AtendeLogo.UseCases/Identities/Tenants/Commands/UpdateTenantCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace AtendeLogo.UseCases.Identities.Tenants.Commands;
+
+public class UpdateTenantCommandHandler
+     : CommandHandler<UpdateTenantCommand, OperationResponse>
+{
+    private readonly IIdentityUnitOfWork _unitOfWork;
+
+    public UpdateTenantCommandHandler(IIdentityUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    protected override async Task<Result<OperationResponse>> HandleAsync(
+        UpdateTenantCommand command,
+        CancellationToken cancellationToken)
+    {
+        var tenant = await _unitOfWork.Tenants.GetByIdAsync(command.Tenant_Id, cancellationToken);
+        if(tenant is null)
+        {
+            return Result.NotFoundFailure<OperationResponse>(
+                "Tenant.NotFound",
+                 $"Tenant with id {command.Tenant_Id} not found.");
+        }
+
+        tenant.Update(
+            name: command.Name,
+            country: command.Country,
+            language: command.Language,
+            currency: command.Currency,
+            businessType: command.BusinessType,
+            tenantType: command.TenantType,
+            fiscalCode: command.FiscalCode);
+
+        _unitOfWork.Update(tenant);
+
+        var result = await _unitOfWork.SaveChangesAsync(silent: true, cancellationToken);
+        if (result.IsSuccess)
+        {
+            return Result.Success(new OperationResponse());
+        }
+        return Result.Failure<OperationResponse>(result.Error);
+    }
+}
+
---
+++ tests/AtendeLogo.UseCases.UnitTests/Identities/Tenants/Commands/UpdateTenantCommandHandlerTests.cs
@@ -0,0 +1,72 @@
+﻿using AtendeLogo.UseCases.Identities.Tenants.Commands;
+
+namespace AtendeLogo.UseCases.UnitTests.Identities.Tenants.Commands;
+
+public class UpdateTenantCommandHandlerTests : IClassFixture<TenantOwnerUserServiceProviderMock>
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly UpdateTenantCommand _validadeCommand;
+
+    public UpdateTenantCommandHandlerTests(TenantOwnerUserServiceProviderMock serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        _validadeCommand = new UpdateTenantCommand
+        {
+            Name = "Tenant name",
+            FiscalCode = new FiscalCode("04866748940"),
+            Country = Country.Brazil,
+            Language = Language.Portuguese,
+            Currency = Currency.BRL,
+            BusinessType = BusinessType.CivilRegistryOffice,
+            TenantType = TenantType.Company,
+            Tenant_Id = SystemTenantConstants.Tenant_Id
+        };
+    }
+
+    [Fact]
+    public void Handler_ShouldBe_UpdateTenantCommandHandler()
+    {
+        // Arrange
+        var mediator = _serviceProvider.GetRequiredService<IRequestMediator>() as IRequestMediatorTest;
+
+        // Act
+        var handlerType = mediator!.GetRequestHandler(_validadeCommand);
+
+        // Assert
+        handlerType.Should().BeOfType<UpdateTenantCommandHandler>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_ReturnsSuccess()
+    {
+        // Arrange
+        var mediator = _serviceProvider.GetRequiredService<IRequestMediator>();
+        var command = _validadeCommand;
+
+        // Act
+        var result = await mediator.RunAsync(command, CancellationToken.None);
+
+        // Assert
+        result.ShouldBeSuccessful();
+    }
+
+    [Fact]
+    public async Task HandleAsync_ReturnsNotFound_WhenTenantDoesNotExist()
+    {
+        // Arrange
+        var mediator = _serviceProvider.GetRequiredService<IRequestMediator>();
+        var command = _validadeCommand with
+        {
+            // Use a non-existent Tenant_Id
+            Tenant_Id = Guid.NewGuid()
+        };
+
+        // Act
+        var result = await mediator.RunAsync(command, CancellationToken.None);
+
+        // Assert
+        result.ShouldBeFailure<NotFoundError>();
+
+    }
+}
+
