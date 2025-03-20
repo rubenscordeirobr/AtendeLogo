fcbe601e57b1a81a03673df4e3cadbed7534ad25
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-20 07:56:21 +0100

    Add ITenantUserValidationService and implementation for email and phone number uniqueness checks with unit tests

---
+++ src/AtendeLogo.UseCases.Shared/Contracts/Identities/ITenantUserValidationService.cs
@@ -0,0 +1,22 @@
+﻿namespace AtendeLogo.UseCases.Contracts.Identities;
+
+public interface ITenantUserValidationService : IValidationService
+{
+    Task<bool> IsEmailUniqueAsync(
+       string email,
+       CancellationToken token = default);
+
+    Task<bool> IsEmailUniqueAsync(
+        Guid currentUser_Id,
+        string email,
+        CancellationToken token = default);
+
+    Task<bool> IsPhoneNumberUniqueAsync(
+        string phoneNumber, 
+        CancellationToken token = default);
+
+    Task<bool> IsPhoneNumberUniqueAsync(
+        Guid currentUser_Id,
+        string number,
+        CancellationToken token = default);
+}
---
+++ src/AtendeLogo.UseCases/Identities/Tenants/Services/TenantUserValidationService.cs
@@ -0,0 +1,42 @@
+﻿using AtendeLogo.UseCases.Contracts.Identities;
+
+namespace AtendeLogo.UseCases.Identities.Tenants.Services;
+
+internal class TenantUserValidationService : ITenantUserValidationService
+{
+    private readonly ITenantUserRepository _tenantUserRepository;
+
+    public TenantUserValidationService(
+        ITenantUserRepository tenantUserRepository)
+    {
+        _tenantUserRepository = tenantUserRepository;
+    }
+
+    public async Task<bool> IsEmailUniqueAsync(string email, CancellationToken token)
+    {
+        return await _tenantUserRepository.EmailExistsAsync(email, token) == false;
+    }
+
+    public async Task<bool> IsEmailUniqueAsync(
+        Guid currentUser_Id, 
+        string email,
+        CancellationToken token)
+    {
+        return await _tenantUserRepository.EmailExistsAsync(currentUser_Id, email, token) == false;
+    }
+
+    public async Task<bool> IsPhoneNumberUniqueAsync(
+        string phoneNumber,
+        CancellationToken token)
+    {
+        return await _tenantUserRepository.PhoneNumberExitsAsync(phoneNumber, token) == false;
+    }
+
+    public async Task<bool> IsPhoneNumberUniqueAsync(
+        Guid currentUser_Id, 
+        string number, 
+        CancellationToken token)
+    {
+        return await _tenantUserRepository.PhoneNumberExitsAsync(currentUser_Id, number, token) == false;
+    }
+}
