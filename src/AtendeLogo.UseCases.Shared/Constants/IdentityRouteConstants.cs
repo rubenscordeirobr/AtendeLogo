608e074dad448c1fac4b332e05b2c11fff96f924
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-20 08:57:32 +0100

    Add IdentityRouteConstants and RouteConstants for API routing

---
+++ src/AtendeLogo.UseCases.Shared/Constants/IdentityRouteConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace AtendeLogo.UseCases.Constants;
+
+public static class IdentityRouteConstants
+{
+    private const string IdentityBase = $"{RouteConstants.ApiBase}/identity";
+     
+    public const string Tenants = $"{IdentityBase}/tenants";
+    public const string TenantValidation = $"{IdentityBase}/tenant-validation";
+    public const string TenantUserValidation = $"{IdentityBase}/tenant-users-validation";
+    public const string TenantAuthentication = $"{IdentityBase}/tenant-authentication";
+    public const string TenantAuthenticationValidation = $"{IdentityBase}/tenant-authentication-validation";
+    public const string TenantUpdateDefaultAddress = "update-default-address";
+
+    public const string TenantUsersEndpoint = $"{IdentityBase}/tenant-users";
+    public const string AdminUsersRoute = $"{IdentityBase}/admin-users";
+
+    public const string Logout = "logout";
+}
---
+++ src/AtendeLogo.UseCases.Shared/Constants/RouteConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace AtendeLogo.UseCases.Constants;
+
+public static class RouteConstants
+{
+    public const string RouteId = "{id}";
+    public const string ApiBase = "api";
+}
