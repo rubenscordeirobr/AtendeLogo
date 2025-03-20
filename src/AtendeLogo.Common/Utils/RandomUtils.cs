12f902e8e462ce99c8d775a348b49b50b09c28c5
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-20 08:48:34 +0100

    Add RandomUtils with tests

---
+++ src/AtendeLogo.Common/Utils/RandomUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace AtendeLogo.Common.Utils;
+
+public static class RandomUtils
+{
+    private static readonly Random _random = new();
+
+    public static string GenerateRandomNumber(int length)
+    {
+        var digits = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            digits[i] = (char)('0' + _random.Next(0, 10));
+        }
+        return new string(digits);
+    }
+}
