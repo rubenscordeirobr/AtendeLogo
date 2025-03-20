931003794d0b5ff898a1b053674e5d3bafdaccfd
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-20 08:53:45 +0100

    Rename JsonStringLocalizer to JsonStringLocalizerMock

--- tests/AtendeLogo.Application.UnitTests/Mocks/Infrastructure/JsonStringLocalizerMock.cs
+++ tests/AtendeLogo.Application.UnitTests/Mocks/Infrastructure/JsonStringLocalizerMock.cs
@@ -3,7 +3,7 @@ using AtendeLogo.Shared.Contracts;
 
 namespace AtendeLogo.Application.UnitTests.Mocks.Infrastructure;
 
-internal class JsonStringLocalizer<T> : IJsonStringLocalizer<T>
+public class JsonStringLocalizerMock<T> : IJsonStringLocalizer<T>
 {
     public string this[string name, string defaultValue, params object[] args]
         => StringFormatUtils.Format(defaultValue, args);
---
+++ tests/AtendeLogo.ArchitectureTests/TestSupport/ClientGatewayServiceProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AtendeLogo.ArchitectureTests.TestSupport;
+
+public class ClientGatewayServiceProvider : IServiceProvider
+{
+    private readonly IServiceProvider _serviceProvider;
+    public IServiceCollection Services { get; }
+
+    public ClientGatewayServiceProvider()
+    {
+        Services = new ClientGatewayServiceCollection();
+        _serviceProvider = Services.BuildServiceProvider();
+    }
+
+    public object? GetService(Type serviceType)
+    {
+        return _serviceProvider.GetService(serviceType);
+    }
+}
