7cda1dd55d097c444273981b8a5cd4859d86a005
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-21 09:25:43 +0100

    Add ErrorLogLevelMapper for mapping error types to log levels

---
+++ src/AtendeLogo.Common/Mappers/ErrorLogLevelMapper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AtendeLogo.Common.Mappers;
+
+public static class ErrorLogLevelMapper
+{
+    public static LogLevel ResolveErrorLevel(Error error)
+    {
+        return error switch
+        {
+            NotFoundError => LogLevel.Information,
+            OperationCanceledError => LogLevel.Information,
+            // Warnings
+            ValidationError => LogLevel.Warning,
+            UnauthorizedError => LogLevel.Warning,
+            AbortedError => LogLevel.Warning,
+            TaskTimeoutError => LogLevel.Warning,
+            // Errors
+            BadRequestError => LogLevel.Error,
+            InvalidOperationError => LogLevel.Error,
+            DomainEventError => LogLevel.Error,
+            RequestError => LogLevel.Error,
+            DeserializationError => LogLevel.Error,
+            CreateHttpRequestMessageError => LogLevel.Error,
+            UnknownError => LogLevel.Error,
+            //Critical
+            DatabaseError => LogLevel.Critical,
+            CommandValidatorNotFoundError => LogLevel.Critical,
+            InternalServerError => LogLevel.Critical,
+            NotImplementedError => LogLevel.Critical,
+            _ => LogLevel.Error
+        };
+    }
+}
