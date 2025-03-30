be5bd36a57142576beef98c4227d66903f2411bb
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-30 15:51:59 +0100

    feat: implement TenantUserValidationService and corresponding endpoint with methods for validating unique email and phone number, and add functional tests

---
+++ src/AtendeLogo.Presentation/Endpoints/Identity/TenantUserValidationEndpoint.cs
@@ -0,0 +1,56 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+
+namespace AtendeLogo.Presentation.Endpoints.Identity;
+
+[AllowAnonymous]
+[ServiceRole(ServiceRole.Authentication)]
+[EndPoint(IdentityRouteConstants.TenantUserValidation)]
+public class TenantUserValidationEndpoint : ApiEndpointBase, ITenantUserValidationService
+{
+    private readonly ITenantUserValidationService _validationService;
+     
+    public TenantUserValidationEndpoint(ITenantUserValidationService validationService)
+    {
+        _validationService = validationService;
+    }
+
+    [HttpFormValidation]
+    public Task<bool> IsEmailUniqueAsync(
+        string email,
+        CancellationToken cancellationToken = default)
+    {
+        return _validationService.IsEmailUniqueAsync(email, cancellationToken);
+    }
+
+    [HttpFormValidation]
+    public Task<bool> IsEmailUniqueAsync(
+        Guid currentUser_Id,
+        string email,
+        CancellationToken cancellationToken = default)
+    {
+        return _validationService.IsEmailUniqueAsync(currentUser_Id, email, cancellationToken);
+    }
+
+    [HttpFormValidation]
+    public Task<bool> IsPhoneNumberUniqueAsync(
+        string phoneNumber,
+        CancellationToken cancellationToken = default)
+    {
+        return _validationService.IsPhoneNumberUniqueAsync(phoneNumber, cancellationToken);
+    }
+
+    [HttpFormValidation]
+    public Task<bool> IsPhoneNumberUniqueAsync(
+        Guid currentUser_Id,
+        string number,
+        CancellationToken cancellationToken = default)
+    {
+        return _validationService.IsPhoneNumberUniqueAsync(currentUser_Id, number, cancellationToken);
+    }
+
+    #region IEndpointService
+ 
+  
+    #endregion
+}
