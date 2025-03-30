40355cc1768293b40a0420def09058aaea3addb0
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-30 15:39:49 +0100

    feat: implement AdminUserAuthenticationValidation service and endpoint with validation methods

---
+++ src/AtendeLogo.Presentation/Endpoints/Identity/AdminUserAuthenticationValidationEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AtendeLogo.Presentation.Endpoints.Identity;
+
+[AllowAnonymous]
+[ServiceRole(ServiceRole.Authentication)]
+[EndPoint(IdentityRouteConstants.AdminUserAuthenticationValidation)]
+public class AdminUserAuthenticationValidationEndpoint : ApiEndpointBase, IAdminUserAuthenticationValidationService
+{
+    private readonly IAdminUserAuthenticationValidationService _validationService;
+ 
+    public AdminUserAuthenticationValidationEndpoint(
+        IAdminUserAuthenticationValidationService validationService)
+    {
+        _validationService = validationService;
+    }
+
+    [HttpFormValidation]
+    public Task<bool> VerifyAdminUserCredentialsAsync(
+        string emailOrPhoneNumber,
+        string password,
+        CancellationToken cancellationToken = default)
+    {
+        return _validationService
+            .VerifyAdminUserCredentialsAsync(emailOrPhoneNumber, password, cancellationToken);
+    }
+
+    [HttpFormValidation]
+    public Task<bool> EmailOrPhoneNumberExitsAsync(
+        string emailOrPhoneNumber,
+        CancellationToken cancellationToken = default)
+    {
+        return _validationService
+            .EmailOrPhoneNumberExitsAsync(emailOrPhoneNumber, cancellationToken);
+    }
+}
+
+
---
+++ src/AtendeLogo.UseCases.Shared/Contracts/Identities/IAdminUserAuthenticationValidationService.cs
@@ -0,0 +1,13 @@
+﻿namespace AtendeLogo.UseCases.Contracts.Identities;
+
+public interface IAdminUserAuthenticationValidationService : IValidationService
+{
+    Task<bool> EmailOrPhoneNumberExitsAsync(
+        string emailOrPhoneNumber,
+        CancellationToken cancellationToken = default);
+
+    Task<bool> VerifyAdminUserCredentialsAsync(
+        string emailOrPhoneNumber,
+        string password,
+        CancellationToken cancellationToken = default);
+}
