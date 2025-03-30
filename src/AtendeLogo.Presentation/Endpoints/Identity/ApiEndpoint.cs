4be8958bfbc082d40fa6775616cbd72321a8642c
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-03-30 16:34:39 +0100

    add ApiEndpoint and TenantAddressService

---
+++ src/AtendeLogo.Presentation/Endpoints/Identity/ApiEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using AtendeLogo.UseCases.Contracts;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AtendeLogo.Presentation.Endpoints.Identity;
+
+[AllowAnonymous]
+[EndPoint(RouteConstants.Api)]
+public class ApiEndpoint : ApiEndpointBase, IApiService
+{
+    [HttpGet(routeTemplate: RouteConstants.Version)]
+    public string GetVersion()
+    {
+        return "0.0.1";
+    }
+}
+
