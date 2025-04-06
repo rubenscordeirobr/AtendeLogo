fbd49c24b513df4e93c51dbb84998a6e83c164e1
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-04-06 19:39:01 +0200

    Add LanguageExtensions and LanguageMapper for language handling functionality

---
+++ src/AtendeLogo.Common/Extensions/LanguageExtensions.cs
@@ -0,0 +1,17 @@
+﻿using AtendeLogo.Common.Enums;
+using AtendeLogo.Common.Mappers;
+
+namespace AtendeLogo.Common.Extensions;
+
+public static class LanguageExtensions
+{
+    public static bool IsDefaultLanguage(this Language language)
+    {
+        return language == Language.Default || language == Language.English;
+    }
+
+    public static string GetLanguageTag(this Language language)
+    {
+        return LanguageMapper.MapTag(language);
+    }
+}
