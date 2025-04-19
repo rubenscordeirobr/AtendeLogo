1cb27d67ac775c226f0f3a08cbf10ad6e41b96ce
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-04-19 13:56:17 +0200

    Add global usings and suppression

---
+++ src/AtendeLogo.UI/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design",
+    "CA1003:Use generic event handler instances",
+    Justification = "In Blazor, the event handler is not generic, so we use a non-generic delegate.",
+    Scope = "module")]
