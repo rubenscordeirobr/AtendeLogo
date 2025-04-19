ad6240c88e0544dce9a21981ff6c09904b0234ef
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-04-19 13:51:35 +0200

    Add LanguageProvider for managing application language settings

---
+++ src/AtendeLogo.UI/Services/LanguageProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace AtendeLogo.UI.Services;
+
+public class LanguageProvider : ILanguageProvider
+{
+    private Language _language = LanguageHelper.DefaultLanguage;
+
+    private readonly IJsonStringLocalizerCache _stringLocalizerCache;
+
+    public LanguageProvider(IJsonStringLocalizerCache stringLocalizerCache)
+    {
+        _stringLocalizerCache = stringLocalizerCache;
+    }
+
+    public Language Language
+        => _language;
+   
+    public async Task SetLanguageAsync(string language)
+    {
+        _language = LanguageHelper.GetLanguageEnum(language);
+        await _stringLocalizerCache.LoadLanguageAsync(language);
+    }
+}
