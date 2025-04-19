704c66ee88a8cec470b47707cad32829647f9777
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-04-19 13:56:26 +0200

    Add UIServiceConfiguration for service registration and initialization

---
+++ src/AtendeLogo.UI/UIServiceConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using AtendeLogo.ClientGateway;
+using AtendeLogo.ClientGateway.Common.Abstractions;
+using AtendeLogo.UI.Services;
+using Blazored.LocalStorage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AtendeLogo.UI;
+
+public static class UIServiceConfiguration
+{
+    public static IServiceCollection AddAtendeLogoUIServices(
+       this IServiceCollection services)
+    {
+        return services.AddCascadingAuthenticationState()
+            .AddFluentUIComponents()
+            .AddBlazoredLocalStorage()
+            .AddClientGatewayServices()
+            .AddScoped<IInternetStatusService, InternetStatusService>()
+            .AddScoped<IConnectionStatusNotifier, ConnectionStatusNotifier>()
+            .AddScoped<IRequestErrorNotifier, RequestErrorNotifier>()
+            .AddScoped<ILanguageProvider, LanguageProvider>()
+            .AddScoped<IClientAuthorizationTokenManager, ClientAuthorizationTokenManager>()
+            .AddScoped<IBusyIndicatorService, BusyIndicatorService>()
+            .AddScoped<IThemeService, ThemeService>();
+    }
+
+    public static async Task InitializeUIServiceAsync(
+       this IServiceProvider serviceProvider)
+    {
+        await serviceProvider.InitializeClientServiceAsync();
+    }
+        
+}
