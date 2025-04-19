c71a265191b70ce84992a3d20cea1448523b89f9
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-04-19 13:55:30 +0200

    Add ThemeServiceExtensions for toggling theme modes

---
+++ src/AtendeLogo.UI/Extensions/ThemeServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace AtendeLogo.UI.Extensions;
+
+public static class ThemeServiceExtensions
+{
+    public static void ToggleThemeMode(this IThemeService themeService)
+    {
+        Guard.NotNull(themeService);
+
+        var nextThemeState = themeService.ThemeMode == DesignThemeModes.Light
+            ? DesignThemeModes.Dark
+            : DesignThemeModes.Light;
+
+        themeService.SetThemeMode(nextThemeState);
+    }
+}
+
