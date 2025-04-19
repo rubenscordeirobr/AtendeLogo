3dce149c73ff76df75a3c3dae394586cc3346efb
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-04-19 13:50:33 +0200

    Add AnonymousPage and AuthenticatedPage classes for authorization management

---
+++ src/AtendeLogo.UI/Components/Pages/AnonymousPage.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AtendeLogo.UI.Components.Pages;
+
+[AllowAnonymous]
+public abstract partial class AnonymousPage : PageBase
+{
+}
+
---
+++ src/AtendeLogo.UI/Components/Pages/AuthenticatedPage.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AtendeLogo.UI.Components.Pages;
+
+[Authorize]
+public abstract partial class AuthenticatedPage : PageBase
+{
+}
+
