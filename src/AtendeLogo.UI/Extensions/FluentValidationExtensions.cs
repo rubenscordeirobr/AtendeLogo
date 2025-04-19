6ce485785457c30b7aec4ac12ccbc419197b924e
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-04-19 13:54:58 +0200

    Add FluentValidationExtensions for custom validation logic with async support

---
+++ src/AtendeLogo.UI/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace AtendeLogo.UI.Extensions;
+
+public static class FluentValidationExtensions
+{
+    public static void ValidateWithCommand<T, TCommand>(
+        this IRuleBuilder<T, T> ruleBuilder,
+        Func<T, TCommand> commandFactory,
+        IValidator<TCommand> validator)
+    {
+        ruleBuilder.CustomAsync(async (model, context, cancellation) =>
+      {
+          var result = await validator.ValidateAsync(commandFactory(model), cancellation);
+          if (!result.IsValid)
+          {
+              foreach (var error in result.Errors)
+              {
+                  context.AddFailure(error.PropertyName, error.ErrorMessage);
+              }
+          }
+      });
+    }
+}
