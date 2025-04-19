032101bf6591e204e558d4a15f8810f4bd4d1293
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-04-19 13:55:05 +0200

    Add LanguageProviderExtensions for asynchronous language setting support

---
+++ src/AtendeLogo.UI/Extensions/LanguageProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using AtendeLogo.UI.Services;
+
+namespace AtendeLogo.UI.Extensions;
+
+public static class LanguageProviderExtensions
+{
+    public static Task SetLanguageAsync(
+        this ILanguageProvider languageProvider, 
+        string language)
+    {
+        if (languageProvider is not LanguageProvider concreteProvider)
+        {
+            throw new InvalidOperationException(
+                $"The language provider must be of type {nameof(LanguageProvider)}.");
+        }
+        return concreteProvider.SetLanguageAsync(language);
+    }
+}
+
