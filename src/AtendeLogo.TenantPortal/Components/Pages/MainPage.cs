359e4dfda4b1d1741b6349b5b1c45e5c6728d37c
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-05-24 19:05:18 -0300

    Add Home and MainPage placeholder components for tenant portal with authorization handling

---
+++ src/AtendeLogo.TenantPortal/Components/Pages/MainPage.cs
@@ -0,0 +1,11 @@
+﻿using AtendeLogo.TenantPortal.Components.Layout;
+using AtendeLogo.UI.Components.Pages;
+using Microsoft.AspNetCore.Components;
+
+namespace AtendeLogo.TenantPortal.Components.Pages;
+
+[Layout(typeof(MainLayout))]
+public abstract class MainPage : AuthenticatedPage
+{
+}
+
