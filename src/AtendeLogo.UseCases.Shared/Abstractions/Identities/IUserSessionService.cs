a815e54b8971ca33a662f7ab532e51634dd94870
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-05-24 16:01:39 -0300

    Add IUserSessionService interface for managing user session language changes

---
+++ src/AtendeLogo.UseCases.Shared/Abstractions/Identities/IUserSessionService.cs
@@ -0,0 +1,10 @@
+﻿using AtendeLogo.UseCases.Identities.UserSessions;
+
+namespace AtendeLogo.UseCases.Abstractions.Identities;
+
+public interface IUserSessionService
+{
+    Task ChangeLanguageAsync(
+        ChangeUserSessionLanguageCommand command, 
+        CancellationToken cancellationToken = default);
+}
