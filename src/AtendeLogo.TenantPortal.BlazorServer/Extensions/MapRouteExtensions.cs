ac11e015c1ed9474c776754330ac5755cdfaf739
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-05-25 09:42:22 -0300

    Add endpoint mapping for tenant portal including NotFound and SignIn endpoints

---
+++ src/AtendeLogo.TenantPortal.BlazorServer/Extensions/MapRouteExtensions.cs
@@ -0,0 +1,55 @@
+﻿using AtendeLogo.TenantPortal.BlazorServer.Services;
+
+namespace AtendeLogo.TenantPortal.BlazorServer.Extensions;
+
+internal static class MapRouteExtensions
+{
+    public static void MapTenantPortalBlazorServerEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapNotFoundEndpoint();
+        endpoints.MapSingInEndpoint();
+    }
+
+    public static void MapNotFoundEndpoint(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapFallback(async context =>
+        {
+            var cultureCode = LocalizedUriUtils.ExtractCultureCodeFromUri(context.Request.Path);
+            if (cultureCode is null)
+            {
+                var defaultCulturePath = LocalizedUriUtils.BuildDefaultCultureUri(
+                    context.Request.Path,
+                    context.Request.QueryString.ToString());
+
+                context.Response.Redirect(defaultCulturePath);
+                return;
+            }
+
+            var localizerCache = context.RequestServices.GetRequiredService<IJsonStringLocalizerCache>();
+            var culture = CultureHelper.GetCulture(cultureCode);
+            await localizerCache.EnsureLanguageLoadedAsync(culture);
+
+            var route = StringFormatUtils.Format(RouteBlazorServerConstants.NotFoundRoute,
+                cultureCode,
+                context.Request.Path);
+
+            context.Response.Redirect(route);
+            await context.Response.CompleteAsync();
+        });
+    }
+
+    public static void MapSingInEndpoint(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost(RouteBlazorServerConstants.BlazorServerLoginRoute, async context =>
+        {
+            var singInService = context.RequestServices.GetRequiredService<HttpContextSignInService>();
+            await singInService.LoginAsync();
+        });
+
+        endpoints.MapPost(RouteBlazorServerConstants.BlazorServerLogoutRoute, async context =>
+        {
+            var singInService = context.RequestServices.GetRequiredService<HttpContextSignInService>();
+            await singInService.LogoutAsync();
+        });
+    }
+}
---
+++ src/AtendeLogo.TenantPortal.BlazorServer/Extensions/RouteBlazorServerConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace AtendeLogo.TenantPortal.BlazorServer.Extensions;
+
+internal static class RouteBlazorServerConstants
+{
+    public const string NotFoundRoute = "/{0}/not-found?url={1}";
+    public const string BlazorServerLoginRoute = "/api/login";
+    public const string BlazorServerLogoutRoute = "/api/logout";
+}
