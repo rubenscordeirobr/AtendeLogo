80047c693784490fae5743eaa5f52d8b29783f95
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-05-25 10:08:30 -0300

    Add initial project files for AtendeLogo.AppHost (Aspire.NET) with default configuration and settings

---
+++ src/AtendeLogo.AppHost/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var identityApi = builder.AddProject<Projects.AtendeLogo_IdentityApi>("identityapi");
+
+builder.AddProject<Projects.AtendeLogo_TenantPortal_BlazorServer>("tenant-portal-blazor-server")
+    .WithExternalHttpEndpoints()
+    .WithReference(identityApi)
+    .WaitFor(identityApi);
+ 
+await builder.Build().RunAsync();
